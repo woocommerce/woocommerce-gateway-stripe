47a1afacba4a2a5d1b53fb48de9cb9c96a54efc2
roykho <roykho77@gmail.com>
2016-12-09 07:05:25 -0800

    Check to ensure WC class exists before using closes #126

--- woocommerce-gateway-stripe.php
+++ woocommerce-gateway-stripe.php
@@ -224,7 +224,7 @@ class WC_Stripe {
 	 * @return string Setting link
 	 */
 	public function get_setting_link() {
-		$use_id_as_section = version_compare( WC()->version, '2.6', '>=' );
+		$use_id_as_section = class_exists( 'WC' ) ? version_compare( WC()->version, '2.6', '>=' ) : false;
 
 		$section_slug = $use_id_as_section ? 'stripe' : strtolower( 'WC_Gateway_Stripe' );
 
