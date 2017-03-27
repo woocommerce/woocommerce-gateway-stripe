bd180dec57c40358291dccdcab60c315c13a8f1a
roykho <roykho77@gmail.com>
2017-03-27 06:54:49 -0700

    Stripe Checkout email validation allow + symbols

--- assets/js/stripe-checkout.js
+++ assets/js/stripe-checkout.js
@@ -69,7 +69,7 @@ jQuery( function( $ ) {
 					var emailField = $( this ).find( '#billing_email' );
 
 					if ( emailField.length ) {
-						var re = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+						var re = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
 						if ( ! re.test( emailField.val() ) ) {
 							required_error = true;
