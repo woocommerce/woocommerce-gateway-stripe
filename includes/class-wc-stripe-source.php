c13ebcb57fe74625f93516046dec3555fe8e57b3
roykho <roykho77@gmail.com>
2017-07-14 18:26:17 -0700

    Add source class file and update Stripe API version

---
+++ includes/class-wc-stripe-source.php
@@ -0,0 +1,25 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * WC_Gateway_Stripe_Source class.
+ *
+ * Represents a Stripe Source object.
+ *
+ * @since 4.0.0
+ */
+class WC_Gateway_Stripe_Source {
+	public function create_source() {
+
+	}
+
+	public function get_source() {
+
+	}
+
+	public function update_source() {
+
+	}
+}
