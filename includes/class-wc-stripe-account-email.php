a75d4bf101c567b3a09c4681b348271ba36b10a1
roykho <roykho77@gmail.com>
2017-08-18 21:50:31 -0700

    Leverage WC account creation process for Apple Pay

--- includes/class-wc-stripe-account-email.php
+++ includes/class-wc-stripe-account-email.php
@@ -72,11 +72,8 @@ class WC_Stripe_Account_Email {
 	public function create_account() {
 		$user_login   = WC_Stripe_Helper::is_pre_30() ? $this->order->billing_first_name : $this->order->get_billing_first_name();
 		$user_email   = WC_Stripe_Helper::is_pre_30() ? $this->order->billing_email : $this->order->get_billing_email();
-		$first_name   = WC_Stripe_Helper::is_pre_30() ? $this->order->billing_first_name : $this->order->get_billing_first_name();
-		$last_name    = WC_Stripe_Helper::is_pre_30() ? $this->order->billing_last_name : $this->order->get_billing_last_name();
-		$display_name = WC_Stripe_Helper::is_pre_30() ? $this->order->billing_first_name : $this->order->get_billing_first_name();
-
-		$username = sanitize_user( current( explode( '@', $user_email ) ), true );
+		$password     = wp_generate_password();
+		$username     = sanitize_user( current( explode( '@', $user_email ) ), true );
 
 		// Ensure username is unique.
 		$append     = 1;
@@ -86,42 +83,20 @@ class WC_Stripe_Account_Email {
 			$append++;
 		}
 
-		$args = apply_filters( 'wc_stripe_create_account_args', array(
+		$new_customer_data = apply_filters( 'wc_stripe_create_account_args', array(
 			'user_login'      => $username,
 			'user_email'      => $user_email,
-			'first_name'      => $first_name,
-			'last_name'       => $last_name,
-			'display_name'    => $first_name,
-			'user_pass'       => wp_generate_password(),
+			'user_pass'       => $password,
+			'role'            => 'customer',
 		) );
 
-		$user_id = wp_insert_user( $args );
+		$user_id = wp_insert_user( $new_customer_data );
 
 		if ( is_wp_error( $user_id ) ) {
 			throw new Exception( $user_id->get_error_message() );
 		}
 
-		$user               = get_user_by( 'id', $user_id );
-		$password_reset_key = get_password_reset_key( $user );
-		$user->set_role( 'customer' );
-
-		$subject = apply_filters( 'wc_stripe_create_account_subject', wp_specialchars_decode( get_bloginfo( 'name' ), ENT_QUOTES ) . ' ' . __( 'Account Created', 'woocommerce-gateway-stripe' ) );
-
-		ob_start();
-		require_once WC_STRIPE_PLUGIN_PATH . '/templates/emails/account-created-email.php';
-		$message = ob_get_clean();
-
-		add_filter( 'wp_mail_from', array( $this, 'get_from_address' ) );
-		add_filter( 'wp_mail_from_name', array( $this, 'get_from_name' ) );
-		add_filter( 'wp_mail_content_type', array( $this, 'get_content_type' ) );
-
-		if ( ! wp_mail( $user_email, $subject, $message ) ) {
-			WC_Stripe_Logger::log( '(Create Account) Email was not sent successfully!' );
-		}
-
-		remove_filter( 'wp_mail_from', array( $this, 'get_from_address' ) );
-		remove_filter( 'wp_mail_from_name', array( $this, 'get_from_name' ) );
-		remove_filter( 'wp_mail_content_type', array( $this, 'get_content_type' ) );
+		do_action( 'woocommerce_created_customer', $user_id, $new_customer_data, $password );
 
 		return $user_id;
 	}
