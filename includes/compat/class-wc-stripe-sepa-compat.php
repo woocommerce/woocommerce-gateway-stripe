89a9279d2d1a86ee3c2d48a3054367f9abdc7a36
roykho <roykho77@gmail.com>
2017-08-25 08:52:27 -0700

    Combine preorders and subs compat

--- includes/compat/class-wc-stripe-sepa-compat.php
+++ includes/compat/class-wc-stripe-sepa-compat.php
@@ -4,29 +4,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Compatibility class for Subscriptions.
+ * Compatibility class for Subscriptions and Pre-Orders.
  *
- * @extends WC_Gateway_Stripe
+ * @extends WC_Gateway_Stripe_Sepa
  */
-class WC_Stripe_Sepa_Subscriptions_Compat extends WC_Gateway_Stripe_Sepa {
+class WC_Stripe_Sepa_Compat extends WC_Gateway_Stripe_Sepa {
 	/**
 	 * Constructor
 	 */
 	public function __construct() {
 		parent::__construct();
 
-		add_action( 'woocommerce_scheduled_subscription_payment_' . $this->id, array( $this, 'scheduled_subscription_payment' ), 10, 2 );
-		add_action( 'wcs_resubscribe_order_created', array( $this, 'delete_resubscribe_meta' ), 10 );
-		add_action( 'wcs_renewal_order_created', array( $this, 'delete_renewal_meta' ), 10 );
-		add_action( 'woocommerce_subscription_failing_payment_method_updated_' . $this->id, array( $this, 'update_failing_payment_method' ), 10, 2 );
+		if ( class_exists( 'WC_Subscriptions_Order' ) ) {
+			add_action( 'woocommerce_scheduled_subscription_payment_' . $this->id, array( $this, 'scheduled_subscription_payment' ), 10, 2 );
+			add_action( 'wcs_resubscribe_order_created', array( $this, 'delete_resubscribe_meta' ), 10 );
+			add_action( 'wcs_renewal_order_created', array( $this, 'delete_renewal_meta' ), 10 );
+			add_action( 'woocommerce_subscription_failing_payment_method_updated_stripe', array( $this, 'update_failing_payment_method' ), 10, 2 );
 
-		// display the credit card used for a subscription in the "My Subscriptions" table
-		add_filter( 'woocommerce_my_subscriptions_payment_method', array( $this, 'maybe_render_subscription_payment_method' ), 10, 2 );
+			// display the credit card used for a subscription in the "My Subscriptions" table
+			add_filter( 'woocommerce_my_subscriptions_payment_method', array( $this, 'maybe_render_subscription_payment_method' ), 10, 2 );
 
-		// allow store managers to manually set Stripe as the payment method on a subscription
-		add_filter( 'woocommerce_subscription_payment_meta', array( $this, 'add_subscription_payment_meta' ), 10, 2 );
-		add_filter( 'woocommerce_subscription_validate_payment_meta', array( $this, 'validate_subscription_payment_meta' ), 10, 2 );
-		add_filter( 'wc_stripe_display_save_payment_method_checkbox', array( $this, 'maybe_hide_save_checkbox' ) );
+			// allow store managers to manually set Stripe as the payment method on a subscription
+			add_filter( 'woocommerce_subscription_payment_meta', array( $this, 'add_subscription_payment_meta' ), 10, 2 );
+			add_filter( 'woocommerce_subscription_validate_payment_meta', array( $this, 'validate_subscription_payment_meta' ), 10, 2 );
+			add_filter( 'wc_stripe_display_save_payment_method_checkbox', array( $this, 'maybe_hide_save_checkbox' ) );
+		}
+
+		if ( class_exists( 'WC_Pre_Orders_Order' ) ) {
+			add_action( 'wc_pre_orders_process_pre_order_completion_payment_' . $this->id, array( $this, 'process_pre_order_release_payment' ) );
+		}
 	}
 
 	/**
@@ -54,15 +60,25 @@ class WC_Stripe_Sepa_Subscriptions_Compat extends WC_Gateway_Stripe_Sepa {
 	}
 
 	/**
+	 * Is $order_id a pre-order?
+	 * @param  int  $order_id
+	 * @return boolean
+	 */
+	protected function is_pre_order( $order_id ) {
+		return ( class_exists( 'WC_Pre_Orders_Order' ) && WC_Pre_Orders_Order::order_contains_pre_order( $order_id ) );
+	}
+
+	/**
 	 * Process the payment based on type.
 	 * @param  int $order_id
 	 * @return array
 	 */
 	public function process_payment( $order_id, $retry = true, $force_save_source = false ) {
 		if ( $this->has_subscription( $order_id ) ) {
-			// Regular payment with force save source enabled
+			// Regular payment with force customer enabled
 			return parent::process_payment( $order_id, true, true );
-
+		} elseif ( $this->is_pre_order( $order_id ) ) {
+			return $this->process_pre_order( $order_id, $retry, $force_save_source );
 		} else {
 			return parent::process_payment( $order_id, $retry, $force_save_source );
 		}
@@ -341,7 +357,7 @@ class WC_Stripe_Sepa_Subscriptions_Compat extends WC_Gateway_Stripe_Sepa {
 		if ( $sources ) {
 			$found_source = false;
 			foreach ( $sources as $source ) {
-				if ( 'card' === $source->type ) {
+				if ( isset( $source->type ) && 'card' === $source->type ) {
 					$card = $source->card;
 				}
 
@@ -364,5 +380,94 @@ class WC_Stripe_Sepa_Subscriptions_Compat extends WC_Gateway_Stripe_Sepa {
 		}
 
 		return $payment_method_to_display;
+	}
+
+	/**
+	 * Process the pre-order
+	 * @param int $order_id
+	 * @return array
+	 */
+	public function process_pre_order( $order_id, $retry, $force_save_source ) {
+		if ( WC_Pre_Orders_Order::order_requires_payment_tokenization( $order_id ) ) {
+			try {
+				$order = wc_get_order( $order_id );
+
+				if ( $order->get_total() * 100 < WC_Stripe_Helper::get_minimum_amount() ) {
+					/* translators: minimum amount */
+					throw new Exception( sprintf( __( 'Sorry, the minimum allowed order total is %1$s to use this payment method.', 'woocommerce-gateway-stripe' ), wc_price( WC_Stripe_Helper::get_minimum_amount() / 100 ) ) );
+				}
+
+				$source = $this->prepare_source( get_current_user_id(), true );
+
+				// We need a source on file to continue.
+				if ( empty( $source->customer ) || empty( $source->source ) ) {
+					throw new Exception( __( 'Unable to store payment details. Please try again.', 'woocommerce-gateway-stripe' ) );
+				}
+
+				// Store source to order meta
+				$this->save_source( $order, $source );
+
+				// Remove cart
+				WC()->cart->empty_cart();
+
+				// Is pre ordered!
+				WC_Pre_Orders_Order::mark_order_as_pre_ordered( $order );
+
+				// Return thank you page redirect
+				return array(
+					'result'   => 'success',
+					'redirect' => $this->get_return_url( $order ),
+				);
+			} catch ( Exception $e ) {
+				wc_add_notice( $e->getMessage(), 'error' );
+				return;
+			}
+		} else {
+			return parent::process_payment( $order_id, $retry, $force_save_source );
+		}
+	}
+
+	/**
+	 * Process a pre-order payment when the pre-order is released
+	 * @param WC_Order $order
+	 * @return void
+	 */
+	public function process_pre_order_release_payment( $order ) {
+		try {
+			// Define some callbacks if the first attempt fails.
+			$retry_callbacks = array(
+				'remove_order_source_before_retry',
+				'remove_order_customer_before_retry',
+			);
+
+			while ( 1 ) {
+				$source   = $this->prepare_order_source( $order );
+				$response = WC_Stripe_API::request( $this->generate_payment_request( $order, $source ) );
+
+				if ( ! empty( $response->error ) ) {
+					if ( 0 === sizeof( $retry_callbacks ) ) {
+						throw new Exception( $response->error->message );
+					} else {
+						$retry_callback = array_shift( $retry_callbacks );
+						call_user_func( array( $this, $retry_callback ), $order );
+					}
+				} else {
+					// Successful
+					$this->process_response( $response, $order );
+					break;
+				}
+			}
+		} catch ( Exception $e ) {
+			/* translators: error message */
+			$order_note = sprintf( __( 'Stripe Transaction Failed (%s)', 'woocommerce-gateway-stripe' ), $e->getMessage() );
+
+			// Mark order as failed if not already set,
+			// otherwise, make sure we add the order note so we can detect when someone fails to check out multiple times
+			if ( ! $order->has_status( 'failed' ) ) {
+				$order->update_status( 'failed', $order_note );
+			} else {
+				$order->add_order_note( $order_note );
+			}
+		}
 	}
 }
