238c85d4edd649f9e118bc05485081888124b55c
roykho <roykho77@gmail.com>
2017-11-21 21:06:27 -0800

    Fix typo with endpoints verbiage

--- includes/admin/stripe-bancontact-settings.php
+++ includes/admin/stripe-bancontact-settings.php
@@ -41,7 +41,7 @@ return apply_filters( 'wc_stripe_bancontact_settings',
 			'desc_tip'    => true,
 		),
 		'webhook' => array(
-			'title'       => __( 'Webhook Enpoints', 'woocommerce-gateway-stripe' ),
+			'title'       => __( 'Webhook Endpoints', 'woocommerce-gateway-stripe' ),
 			'type'        => 'title',
 			/* translators: webhook URL */
 			'description' => sprintf( __( 'You must add the webhook endpoint <strong style="background-color:#ddd;">&nbsp;&nbsp;%s&nbsp;&nbsp;</strong> to your Stripe Account Settings <a href="https://dashboard.stripe.com/account/webhooks" target="_blank">Here</a> so you can receive notifications on the charge statuses.', 'woocommerce-gateway-stripe' ), $webhook_url ),
