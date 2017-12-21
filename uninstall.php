bee9af05c24a1dbfebdd9038deb1cf555c13c8ca
roykho <roykho77@gmail.com>
2017-12-20 20:57:43 -0800

    Update remove data constant

--- uninstall.php
+++ uninstall.php
@@ -9,11 +9,11 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 /*
- * Only remove ALL product and page data if WC_STRIPE_REMOVE_ALL_DATA constant is set to true in user's
+ * Only remove ALL product and page data if WC_REMOVE_ALL_DATA constant is set to true in user's
  * wp-config.php. This is to prevent data loss when deleting the plugin from the backend
  * and to ensure only the site owner can perform this action.
  */
-if ( defined( 'WC_STRIPE_REMOVE_ALL_DATA' ) && true === WC_STRIPE_REMOVE_ALL_DATA ) {
+if ( defined( 'WC_REMOVE_ALL_DATA' ) && true === WC_REMOVE_ALL_DATA ) {
 	// Delete options.
 	delete_option( 'woocommerce_stripe_settings' );
 	delete_option( 'wc_stripe_show_request_api_notice' );
