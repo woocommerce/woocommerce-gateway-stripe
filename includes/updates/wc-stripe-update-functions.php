82a302188b7c6080f9436391132d2a6bb014528e
Nicolas Gehin <nicolas@rvola.com>
2018-02-15 11:11:08 +0100

    Update 4.1.0 - Metaname
    Using spaces in meta_key names is really not good practice.
    
    This commit allows you to run a database update to set more conventional names to meta Stripe:
    
    "Stripe Fee" -> "_stripe_fee"
    "Net Revenue From Stripe" -> "_stripe_net"

--- includes/updates/wc-stripe-update-functions.php
+++ includes/updates/wc-stripe-update-functions.php
@@ -11,3 +11,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! defined( 'WC_STRIPE_VERSION' ) ) {
 	exit;
 }
+
+/**
+ * 4.1.0 UPDATE
+ */
+function wc_stripe_update_410() {
+	wc_stripe_update_410_metaname();
+}
+
+/**
+ * Update meta_key name for "fee" & "net revenue".
+ */
+function wc_stripe_update_410_metaname() {
+	global $wpdb;
+
+	// Fees.
+	$wpdb->query( "
+		UPDATE $wpdb->postmeta
+		SET `meta_key` = '_stripe_fee'
+		WHERE `meta_key` = 'Stripe Fee'
+	" );
+
+	// Net.
+	$wpdb->query( "
+		UPDATE $wpdb->postmeta
+		SET `meta_key` = '_stripe_net'
+		WHERE `meta_key` = 'Net Revenue From Stripe'
+	" );
+}
