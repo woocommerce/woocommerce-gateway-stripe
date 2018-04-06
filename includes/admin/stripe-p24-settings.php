ac50d06fbdc128c2ca108d55a553ec106be48984
Marco Almeida <webdados@users.noreply.github.com>
2018-04-06 15:08:08 +0100

    Use i18n placeholders

--- includes/admin/stripe-p24-settings.php
+++ includes/admin/stripe-p24-settings.php
@@ -8,7 +8,7 @@ $webhook_url = WC_Stripe_Helper::get_webhook_url();
 return apply_filters( 'wc_stripe_p24_settings',
 	array(
 		'geo_target' => array(
-			'description' => __( 'Relevant Payer Geography: Poland', 'woocommerce-gateway-stripe' ),
+			'description' => sprintf( __( 'Relevant Payer Geography: %s', 'woocommerce-gateway-stripe' ), __( 'Poland', 'woocommerce-gateway-stripe' ) ),
 			'type'        => 'title',
 		),
 		'activation' => array(
