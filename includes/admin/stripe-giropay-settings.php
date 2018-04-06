bad90830af367ba522c2fd5e48484430e2f2f97f
Marco Almeida <webdados@users.noreply.github.com>
2018-04-06 15:06:27 +0100

    Use i18n placeholders

--- includes/admin/stripe-giropay-settings.php
+++ includes/admin/stripe-giropay-settings.php
@@ -12,7 +12,7 @@ return apply_filters( 'wc_stripe_giropay_settings',
 			'type'        => 'title',
 		),
 		'guide' => array(
-			'description' => __( '<a href="https://stripe.com/payments/payment-methods-guide#giropay" target="_blank">Payment Method Guide</a>', 'woocommerce-gateway-stripe' ),
+			'description' => sprintf( __( '<a href="%s" target="_blank">Payment Method Guide</a>', 'woocommerce-gateway-stripe' ), 'https://stripe.com/payments/payment-methods-guide#giropay' ),
 			'type'        => 'title',
 		),
 		'activation' => array(
