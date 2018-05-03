5eedc51b9be5e0b250707c571eb638331173ee7b
roykho <roykho77@gmail.com>
2018-05-03 05:22:19 -0700

    Update privacy notice method name

--- includes/admin/class-wc-stripe-privacy.php
+++ includes/admin/class-wc-stripe-privacy.php
@@ -53,7 +53,7 @@ class WC_Stripe_Privacy extends WC_Abstract_Privacy {
 	 * Gets the message of the privacy to display.
 	 *
 	 */
-	public function get_message() {
+	public function get_privacy_message() {
 		return wpautop( sprintf( __( 'By using this extension, you may be storing personal data or sharing data with an external service. <a href="%s" target="_blank">Learn more about how this works, including what you may want to include in your privacy policy.</a>', 'woocommerce-gateway-stripe' ), 'https://docs.woocommerce.com/privacy/?woocommerce-gateway-stripe' ) );
 	}
 
