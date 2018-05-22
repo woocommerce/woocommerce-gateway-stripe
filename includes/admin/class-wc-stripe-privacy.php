538a27f0540942ea8aa570ae24e5c765b80a95e9
roykho <roykho77@gmail.com>
2018-05-22 16:48:07 -0700

    Update privacy link

--- includes/admin/class-wc-stripe-privacy.php
+++ includes/admin/class-wc-stripe-privacy.php
@@ -54,7 +54,7 @@ class WC_Stripe_Privacy extends WC_Abstract_Privacy {
 	 *
 	 */
 	public function get_privacy_message() {
-		return wpautop( sprintf( __( 'By using this extension, you may be storing personal data or sharing data with an external service. <a href="%s" target="_blank">Learn more about how this works, including what you may want to include in your privacy policy.</a>', 'woocommerce-gateway-stripe' ), 'https://docs.woocommerce.com/privacy/?woocommerce-gateway-stripe' ) );
+		return wpautop( sprintf( __( 'By using this extension, you may be storing personal data or sharing data with an external service. <a href="%s" target="_blank">Learn more about how this works, including what you may want to include in your privacy policy.</a>', 'woocommerce-gateway-stripe' ), 'https://docs.woocommerce.com/document/privacy-payments/#woocommerce-gateway-stripe' ) );
 	}
 
 	/**
