bd26568a019b5f016578b8d4eba8ec27cb47f8e6
roykho <roykho77@gmail.com>
2018-09-27 20:12:44 -0700

    Use helper function for version compare wc

--- includes/class-wc-stripe-logger.php
+++ includes/class-wc-stripe-logger.php
@@ -27,10 +27,10 @@ class WC_Stripe_Logger {
 
 		if ( apply_filters( 'wc_stripe_logging', true, $message ) ) {
 			if ( empty( self::$logger ) ) {
-				if ( version_compare( WC_VERSION, '3.0.0', '>=' ) ) {
-					self::$logger = wc_get_logger();
-				} else {
+				if ( WC_Stripe_Helper::is_wc_lt( '3.0' ) ) {
 					self::$logger = new WC_Logger();
+				} else {
+					self::$logger = wc_get_logger();
 				}
 			}
 
@@ -57,10 +57,10 @@ class WC_Stripe_Logger {
 
 			}
 
-			if ( version_compare( WC_VERSION, '3.0.0', '>=' ) ) {
-				self::$logger->debug( $log_entry, array( 'source' => self::WC_LOG_FILENAME ) );
-			} else {
+			if ( WC_Stripe_Helper::is_wc_lt( '3.0' ) ) {
 				self::$logger->add( self::WC_LOG_FILENAME, $log_entry );
+			} else {
+				self::$logger->debug( $log_entry, array( 'source' => self::WC_LOG_FILENAME ) );
 			}
 		}
 	}
