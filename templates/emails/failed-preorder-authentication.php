c9295d7dba0244bddc3268ecbf63decc77ddb9f7
Radoslav Georgiev <rageorgiev@gmail.com>
2019-10-10 13:19:06 +0200

    SCA Subscriptions: Reminder Emails (#991)
    * Adding failed auth emails for SCA.
    
    * Forked the subs renewal for pre-orders.
    
    * Switching to the URL format from #986.
    
    * Removing a duplicate email addition method
    
    * Adjusting emails to use the `wc_gateway_stripe_process_payment_authentication_required` action and not check for error codes.
    
    * Adjusting the email actions and methods to work without an error.
    
    * Switching the order status from `on-hold` to `failed` in subs tests.
    
    * Improved wording based on @levinmedia's feedbac.
    
    * Ensuring that wcs_* methods are not required in order to make sure pre-orders works.
    
    * Replacing incorrect i18n textdomains.
    
    * Ensuring that even "failed auth" emails include the custom pre-orders message.

---
+++ templates/emails/failed-preorder-authentication.php
@@ -0,0 +1,63 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+?>
+
+<?php do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+
+<?php
+$pre_wc_30 = version_compare( WC_VERSION, '3.0', '<' );
+$billing_email = $pre_wc_30 ? $order->billing_email : $order->get_billing_email();
+$billing_phone = $pre_wc_30 ? $order->billing_phone : $order->get_billing_phone();
+
+?>
+<p><?php
+	echo wp_kses(
+		sprintf(
+			// translators: %s is a link to the payment re-authentication URL.
+			_x( 'Your pre-order is now available, but payment cannot be completed automatically. %s', 'In failed SCA authentication for a pre-order.', 'woocommerce-gateway-stripe' ),
+			'<a href="' . esc_url( $authorization_url ) . '">' . esc_html__( 'Authorize the payment now &raquo;', 'woocommerce-gateway-stripe' ) . '</a>'
+		),
+		array( 'a' => array( 'href' => true ) )
+	);
+?></p>
+
+<?php if ( $email->get_custom_message() ) : ?>
+	<blockquote><?php echo wpautop( wptexturize( $email->get_custom_message() ) ); ?></blockquote>
+<?php endif; ?>
+
+<?php
+do_action( 'woocommerce_email_before_order_table', $order, false, $plain_text, $email );
+
+/*
+ * @hooked WC_Emails::order_details() Shows the order details table.
+ * @hooked WC_Structured_Data::generate_order_data() Generates structured data.
+ * @hooked WC_Structured_Data::output_structured_data() Outputs structured data.
+ * @since 2.5.0
+ */
+do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
+
+/*
+ * @hooked WC_Emails::order_meta() Shows order meta data.
+ */
+do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
+
+do_action( 'woocommerce_email_after_order_table', $order, false, $plain_text, $email );
+
+/*
+ * @hooked WC_Emails::customer_details() Shows customer details
+ * @hooked WC_Emails::email_address() Shows email address
+ */
+do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
+
+?>
+<p>
+<?php esc_html_e( 'Thanks for shopping with us.', 'woocommerce-gateway-stripe' ); ?>
+</p>
+<?php
+
+/*
+ * @hooked WC_Emails::email_footer() Output the email footer
+ */
+do_action( 'woocommerce_email_footer', $email );
