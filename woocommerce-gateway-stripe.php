bb9c50bbd81a2b831c5851afe38a1e21190dd5f7
Daniel Rey López <daniel.rey.lopez@automattic.com>
2019-11-12 12:39:52 +0000

    Declare WordPress 5.3 and WooCommerce 3.8 compatibility (#1085)
    Fixes #999

--- woocommerce-gateway-stripe.php
+++ woocommerce-gateway-stripe.php
@@ -7,9 +7,9 @@
  * Author URI: https://woocommerce.com/
  * Version: 4.3.0
  * Requires at least: 4.4
- * Tested up to: 5.2.4
+ * Tested up to: 5.3.0
  * WC requires at least: 2.6
- * WC tested up to: 3.7.1
+ * WC tested up to: 3.8
  * Text Domain: woocommerce-gateway-stripe
  * Domain Path: /languages
  *
