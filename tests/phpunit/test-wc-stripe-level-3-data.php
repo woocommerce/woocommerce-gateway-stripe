79e27706994d6d6388f2bb69513e0634ec3bcbe7
Valerie <valerie@automattic.com>
2020-02-01 23:12:19 +0900

    Try to use the function

--- tests/phpunit/test-wc-stripe-level-3-data.php
+++ tests/phpunit/test-wc-stripe-level-3-data.php
@@ -8,6 +8,9 @@ class WC_Stripe_Level_3_Data_Test extends WP_UnitTestCase {
 	public function test_data_for_mutli_item_order() {
 		$order = WC_Helper_Order::create_order();
 		var_dump( $order );
+		$gateway = new WC_Gateway_Stripe();
+		$result = $gateway->get_level_3_data_from_order( $order );
+		var_dump( $result );
 		$this->assertTrue( true );
 	}
 }
