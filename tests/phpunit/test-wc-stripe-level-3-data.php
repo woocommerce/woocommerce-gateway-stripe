c3891166c34a1648d3771d202f70b437a641fc64
Valerie <valerie@automattic.com>
2020-02-02 10:19:08 +0900

    echo stuff

--- tests/phpunit/test-wc-stripe-level-3-data.php
+++ tests/phpunit/test-wc-stripe-level-3-data.php
@@ -7,9 +7,11 @@
 class WC_Stripe_Level_3_Data_Test extends WP_UnitTestCase {
 	public function test_data_for_mutli_item_order() {
 		$order = WC_Helper_Order::create_order();
+		echo '$order:';
 		var_dump( $order );
 		$gateway = new WC_Gateway_Stripe();
 		$result = $gateway->get_level_3_data_from_order( $order );
+		echo '$result:';
 		var_dump( $result );
 		$this->assertTrue( true );
 	}
