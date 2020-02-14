f15dd67383cad68426cb5ba7d453808850fdd540
Radoslav Georgiev <rageorgiev@gmail.com>
2020-02-14 14:25:43 +0200

    Tagging 4.3.2

--- woocommerce-gateway-stripe.php
+++ woocommerce-gateway-stripe.php
@@ -5,11 +5,11 @@
  * Description: Take credit card payments on your store using Stripe.
  * Author: WooCommerce
  * Author URI: https://woocommerce.com/
- * Version: 4.3.1
+ * Version: 4.3.2
  * Requires at least: 4.4
- * Tested up to: 5.3.0
+ * Tested up to: 5.3.2
  * WC requires at least: 2.6
- * WC tested up to: 3.8
+ * WC tested up to: 3.9.2
  * Text Domain: woocommerce-gateway-stripe
  * Domain Path: /languages
  *
@@ -46,7 +46,7 @@ function woocommerce_gateway_stripe_init() {
 		/**
 		 * Required minimums and constants
 		 */
-		define( 'WC_STRIPE_VERSION', '4.3.1' );
+		define( 'WC_STRIPE_VERSION', '4.3.2' );
 		define( 'WC_STRIPE_MIN_PHP_VER', '5.6.0' );
 		define( 'WC_STRIPE_MIN_WC_VER', '2.6.0' );
 		define( 'WC_STRIPE_MAIN_FILE', __FILE__ );
