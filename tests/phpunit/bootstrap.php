4d68d30f4e7ebc6eff3af262c70f63eff5c563a5
Vladimir Reznichenko <kalessil@gmail.com>
2020-11-25 09:03:52 +0100

    Travis: use patched PHPUnit when running tests on PHP8

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -4,6 +4,14 @@ if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
+if ( PHP_VERSION_ID >= 80000 && file_exists( $_tests_dir . '/includes/phpunit7/MockObject' ) ) {
+	// WP Core test library includes patches for PHPUnit 7 to make it compatible with PHP8.
+	require_once $_tests_dir . '/includes/phpunit7/MockObject/Builder/NamespaceMatch.php';
+	require_once $_tests_dir . '/includes/phpunit7/MockObject/Builder/ParametersMatch.php';
+	require_once $_tests_dir . '/includes/phpunit7/MockObject/InvocationMocker.php';
+	require_once $_tests_dir . '/includes/phpunit7/MockObject/MockMethod.php';
+}
+
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
