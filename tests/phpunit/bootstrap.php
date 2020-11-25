e40dc73500b1156dde707837a486177263f37a6b
Vladimir Reznichenko <kalessil@gmail.com>
2020-11-25 09:10:49 +0100

    Travis: PHPUnit bootstrap code tweaks

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -1,8 +1,6 @@
 <?php
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' ) ?: '/tmp/wordpress-tests-lib';
 
 if ( PHP_VERSION_ID >= 80000 && file_exists( $_tests_dir . '/includes/phpunit7/MockObject' ) ) {
 	// WP Core test library includes patches for PHPUnit 7 to make it compatible with PHP8.
@@ -15,22 +13,21 @@ if ( PHP_VERSION_ID >= 80000 && file_exists( $_tests_dir . '/includes/phpunit7/M
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	$plugin_dir = dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/';
-	require $plugin_dir . 'woocommerce/woocommerce.php';
-	require dirname( __FILE__ ) . '/setup.php';
-	require $plugin_dir . 'woocommerce-gateway-stripe/woocommerce-gateway-stripe.php';
+	$plugin_dir = dirname( dirname( dirname( __DIR__ ) ) );
+	require $plugin_dir . '/woocommerce/woocommerce.php';
+	require __DIR__ . '/setup.php';
+	require $plugin_dir . '/woocommerce-gateway-stripe/woocommerce-gateway-stripe.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
 
-$wc_tests_framework_base_dir = dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/woocommerce/tests/framework/';
-require_once( $wc_tests_framework_base_dir . 'class-wc-mock-session-handler.php' );
-//require_once( $wc_tests_framework_base_dir . 'class-wc-unit-test-case.php' );
-require_once( $wc_tests_framework_base_dir . 'helpers/class-wc-helper-product.php' );
-require_once( $wc_tests_framework_base_dir . 'helpers/class-wc-helper-coupon.php' );
-require_once( $wc_tests_framework_base_dir . 'helpers/class-wc-helper-fee.php' );
-require_once( $wc_tests_framework_base_dir . 'helpers/class-wc-helper-shipping.php' );
-require_once( $wc_tests_framework_base_dir . 'helpers/class-wc-helper-customer.php' );
-require_once( $wc_tests_framework_base_dir . 'helpers/class-wc-helper-order.php' );
+$wc_tests_framework_base_dir = dirname( dirname( dirname( __DIR__ ) ) ) . '/woocommerce/tests/framework';
+require_once( $wc_tests_framework_base_dir . '/class-wc-mock-session-handler.php' );
+require_once( $wc_tests_framework_base_dir . '/helpers/class-wc-helper-product.php' );
+require_once( $wc_tests_framework_base_dir . '/helpers/class-wc-helper-coupon.php' );
+require_once( $wc_tests_framework_base_dir . '/helpers/class-wc-helper-fee.php' );
+require_once( $wc_tests_framework_base_dir . '/helpers/class-wc-helper-shipping.php' );
+require_once( $wc_tests_framework_base_dir . '/helpers/class-wc-helper-customer.php' );
+require_once( $wc_tests_framework_base_dir . '/helpers/class-wc-helper-order.php' );
