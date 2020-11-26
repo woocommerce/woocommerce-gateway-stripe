00cafaaa67f042ac33071a6327de07265f43ebf6
Vladimir Reznichenko <kalessil@gmail.com>
2020-11-26 15:38:15 +0100

    Travis: implicitly require testing helpers.

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -23,6 +23,14 @@ tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
 
+require_once( __DIR__ . '/helpers/class-wc-helper-order.php' );
+require_once( __DIR__ . '/helpers/class-wc-helper-product.php' );
+require_once( __DIR__ . '/helpers/class-wc-helper-shipping.php' );
+require_once( __DIR__ . '/helpers/class-wc-helper-token.php' );
+require_once( __DIR__ . '/helpers/class-wc-helper-subscription.php' );
+require_once( __DIR__ . '/helpers/class-wc-helper-subscriptions.php' );
+require_once( __DIR__ . '/helpers/class-wc-helper-subscriptions-cart.php' );
+
 // We use outdated PHPUnit version, which emits deprecation errors in PHP 7.4 (deprecated reflection APIs).
 if ( defined( 'PHP_VERSION_ID' ) && PHP_VERSION_ID >= 70400 ) {
 	error_reporting( error_reporting() ^ E_DEPRECATED ); // phpcs:ignore
