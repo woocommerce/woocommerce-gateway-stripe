b200b0ecf558fda520fa9ab149231ed55fe5a9b2
Vladimir Reznichenko <kalessil@gmail.com>
2020-11-26 09:30:04 +0100

    Travis: re-worked tests bootstrap as tests are failing due to missing WC tables.

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -13,10 +13,15 @@ if ( PHP_VERSION_ID >= 80000 && file_exists( $_tests_dir . '/includes/phpunit7/M
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	$plugin_dir = dirname( dirname( dirname( __DIR__ ) ) );
-	require $plugin_dir . '/woocommerce/woocommerce.php';
+	// NOTE: this will skip the dependency check so the plugin can load. The test environment
+	// needs to still make sure that all dependencies exist for it to successfully run.
+	define( 'WCPAY_TEST_ENV', true );
+
+	// Load the WooCommerce plugin so we can use its classes in our WooCommerce Payments plugin.
+	require_once ABSPATH . '/wp-content/plugins/woocommerce/woocommerce.php';
+
 	require __DIR__ . '/setup.php';
-	require $plugin_dir . '/woocommerce-gateway-stripe/woocommerce-gateway-stripe.php';
+	require dirname( dirname ( __DIR__ ) ) . '/woocommerce-gateway-stripe.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
