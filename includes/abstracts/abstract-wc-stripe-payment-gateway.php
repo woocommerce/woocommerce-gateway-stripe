4b36ab2f21702e6a2d884edaeb95322b8c118b1f
Adam Heckler <aheckler@users.noreply.github.com>
2021-01-20 15:24:39 -0500

    Provide link to Stripe docs

--- includes/abstracts/abstract-wc-stripe-payment-gateway.php
+++ includes/abstracts/abstract-wc-stripe-payment-gateway.php
@@ -851,7 +851,7 @@ abstract class WC_Stripe_Payment_Gateway extends WC_Payment_Gateway_CC {
 		}
 
 		if ( $reason ) {
-			// Trim the refund reason to a max of 500 characters.
+			// Trim the refund reason to a max of 500 characters due to Stripe limits: https://stripe.com/docs/api/metadata.
 			if ( strlen( $reason ) > 500 ) {
 				$reason = function_exists( 'mb_substr' ) ? mb_substr( $reason, 0, 450 ) : substr( $reason, 0, 450 );
 				// Add some explainer text indicating where to find the full refund reason.
