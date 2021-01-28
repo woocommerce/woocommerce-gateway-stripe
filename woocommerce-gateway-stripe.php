26c1e1a4a831e3de62a91d0e165ff35dbeb2e550
Valerie <valerie@automattic.com>
2021-01-28 19:55:41 +0900

    Updated WC tested version to 4.9

--- woocommerce-gateway-stripe.php
+++ woocommerce-gateway-stripe.php
@@ -9,7 +9,7 @@
  * Requires at least: 4.4
  * Tested up to: 5.6
  * WC requires at least: 3.0
- * WC tested up to: 4.3
+ * WC tested up to: 4.9
  * Text Domain: woocommerce-gateway-stripe
  * Domain Path: /languages
  *
