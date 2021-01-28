0abb8dedfdeb9ca0b719bb4a4798069cad89d641
Valerie <valerie@automattic.com>
2021-01-28 19:43:40 +0900

    Update tested WP version to 5.6

--- woocommerce-gateway-stripe.php
+++ woocommerce-gateway-stripe.php
@@ -7,7 +7,7 @@
  * Author URI: https://woocommerce.com/
  * Version: 4.7.0
  * Requires at least: 4.4
- * Tested up to: 5.5
+ * Tested up to: 5.6
  * WC requires at least: 3.0
  * WC tested up to: 4.3
  * Text Domain: woocommerce-gateway-stripe
