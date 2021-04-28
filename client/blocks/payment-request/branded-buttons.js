b62108622cb7aca09343c41f26b6f47ed4bb8c9c
Kristófer R <kristofer.thorlaksson@automattic.com>
2021-04-28 00:52:10 +0000

    Check GPay svg via hook instead of temporary <img>

--- client/blocks/payment-request/branded-buttons.js
+++ client/blocks/payment-request/branded-buttons.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { useState } from '@wordpress/element';
+import { useState, useEffect } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -19,6 +19,28 @@ export const shouldUseGooglePayBrand = () => {
 	return isChrome && ! isBrave;
 };
 
+const useLocalizedGoogleSvg = ( type, theme, locale ) => {
+	// If we're using the short button type (i.e. logo only) make sure we get the logo only SVG.
+	const googlePlaySvg =
+		type === 'long'
+			? `https://www.gstatic.com/instantbuy/svg/${ theme }/${ locale }.svg`
+			: `https://www.gstatic.com/instantbuy/svg/${ theme }_gpay.svg`;
+
+	const [ url, setUrl ] = useState( googlePlaySvg );
+
+	useEffect( () => {
+		const im = document.createElement( 'img' );
+		im.addEventListener( 'error', () => {
+			setUrl(
+				`https://www.gstatic.com/instantbuy/svg/${ theme }/en.svg`
+			);
+		} );
+		im.src = url;
+	}, [ url, theme ] );
+
+	return url;
+};
+
 export const GooglePayButton = ( { onButtonClicked } ) => {
 	const {
 		theme = 'dark',
@@ -32,26 +54,9 @@ export const GooglePayButton = ( { onButtonClicked } ) => {
 	const { branded_type } = wc_stripe_payment_request_params.button; // eslint-disable-line camelcase
 	const type = allowedTypes.includes( branded_type ) ? branded_type : 'long'; // eslint-disable-line camelcase
 
-	// If we're using the short button type (i.e. logo only) make sure we get the logo only SVG.
-	const googlePlaySvg =
-		type === 'long'
-			? `https://www.gstatic.com/instantbuy/svg/${ theme }/${ locale }.svg`
-			: `https://www.gstatic.com/instantbuy/svg/${ theme }_gpay.svg`;
-	const [ backgroundUrl, setBackgroundUrl ] = useState( googlePlaySvg );
-
 	// Let's make sure the localized Google Pay button exists, otherwise we fall back to the
 	// english version. This test element is not used on purpose.
-	const _testImage = ( // eslint-disable-line no-unused-vars
-		<img
-			src={ backgroundUrl }
-			alt={ 'test' }
-			onError={ () => {
-				setBackgroundUrl(
-					`https://www.gstatic.com/instantbuy/svg/${ theme }/en.svg`
-				);
-			} }
-		/>
-	);
+	const backgroundUrl = useLocalizedGoogleSvg( type, theme, locale );
 
 	return (
 		<button
