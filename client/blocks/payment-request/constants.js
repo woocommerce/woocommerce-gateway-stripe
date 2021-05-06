09f3bfc4f4202a9466134408283e76b6ba13890d
Kristófer R <kristofer.thorlaksson@automattic.com>
2021-05-06 03:42:57 +0000

    remove obsolete js files

--- client/blocks/payment-request/constants.js
+++ client/blocks/payment-request/constants.js
@@ -1,7 +1,1 @@
 export const PAYMENT_METHOD_NAME = 'payment_request';
-
-export const DEFAULT_STRIPE_EVENT_HANDLERS = {
-	shippingAddressChange: null,
-	shippingOptionChange: null,
-	source: null,
-};
