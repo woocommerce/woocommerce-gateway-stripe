47d11c06cfd0a93b885df94294b1fd4ce8e2bd1f
Kristófer R <kristofer.thorlaksson@automattic.com>
2021-05-19 02:23:33 +0000

    Silence console error when only loading payment request script

--- assets/js/stripe-payment-request.js
+++ assets/js/stripe-payment-request.js
@@ -3,7 +3,7 @@ jQuery( function( $ ) {
 	'use strict';
 
 	var stripe = Stripe( wc_stripe_payment_request_params.stripe.key, {
-		locale: wc_stripe_params.stripe_locale || 'auto',
+		locale: typeof wc_stripe_params !== 'undefined' ? wc_stripe_params.stripe_locale : 'auto',
 	} ),
 		paymentRequestType;
 
