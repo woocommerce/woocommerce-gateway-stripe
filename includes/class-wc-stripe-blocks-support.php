6835543d3764f4d9148fff13b221503ea2f96863
Kristófer R <kristofer.thorlaksson@automattic.com>
2021-05-27 20:06:17 +0000

    remove unused functions

--- includes/class-wc-stripe-blocks-support.php
+++ includes/class-wc-stripe-blocks-support.php
@@ -144,86 +144,12 @@ final class WC_Stripe_Blocks_Support extends AbstractPaymentMethodType {
 	}
 
 	/**
-	 * Returns the label to use accompanying the total in the stripe statement.
-	 *
-	 * @return string Statement descriptor.
-	 */
-	private function get_total_label() {
-		return ! empty( $this->settings['statement_descriptor'] ) ? WC_Stripe_Helper::clean_statement_descriptor( $this->settings['statement_descriptor'] ) : '';
-	}
-
-	/**
-	 * Returns the publishable api key for the Stripe service.
-	 *
-	 * @return string Public api key.
-	 */
-	private function get_publishable_key() {
-		$test_mode   = ( ! empty( $this->settings['testmode'] ) && 'yes' === $this->settings['testmode'] );
-		$setting_key = $test_mode ? 'test_publishable_key' : 'publishable_key';
-		return ! empty( $this->settings[ $setting_key ] ) ? $this->settings[ $setting_key ] : '';
-	}
-
-	/**
-	 * Returns whether to allow prepaid cards for payments.
-	 *
-	 * @return bool True means to allow prepaid card (default).
-	 */
-	private function get_allow_prepaid_card() {
-		return apply_filters( 'wc_stripe_allow_prepaid_card', true );
-	}
-
-	/**
 	 * Returns the title string to use in the UI (customisable via admin settings screen).
 	 *
 	 * @return string Title / label string
 	 */
 	private function get_title() {
 		return isset( $this->settings['title'] ) ? $this->settings['title'] : __( 'Credit / Debit Card', 'woocommerce-gateway-stripe' );
-	}
-
-	/**
-	 * Return the button type for the payment button.
-	 *
-	 * @return string Defaults to 'default'.
-	 */
-	private function get_button_type() {
-		return isset( $this->settings['payment_request_button_type'] ) ? $this->settings['payment_request_button_type'] : 'default';
-	}
-
-	/**
-	 * Return the theme to use for the payment button.
-	 *
-	 * @return string Defaults to 'dark'.
-	 */
-	private function get_button_theme() {
-		return isset( $this->settings['payment_request_button_theme'] ) ? $this->settings['payment_request_button_theme'] : 'dark';
-	}
-
-	/**
-	 * Return the height for the payment button.
-	 *
-	 * @return string A pixel value for the height (defaults to '64').
-	 */
-	private function get_button_height() {
-		return isset( $this->settings['payment_request_button_height'] ) ? str_replace( 'px', '', $this->settings['payment_request_button_height'] ) : '64';
-	}
-
-	/**
-	 * Return the inline cc option.
-	 *
-	 * @return boolean True if the inline CC form option is enabled.
-	 */
-	private function get_inline_cc_form() {
-		return isset( $this->settings['inline_cc_form'] ) && 'yes' === $this->settings['inline_cc_form'];
-	}
-
-	/**
-	 * Return the locale for the payment button.
-	 *
-	 * @return string Defaults to en_US.
-	 */
-	private function get_button_locale() {
-		return apply_filters( 'wc_stripe_payment_request_button_locale', substr( get_locale(), 0, 2 ) );
 	}
 
 	/**
