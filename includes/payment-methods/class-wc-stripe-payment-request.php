ee7d6689b420fd1a45d06574c2ec1fa16c805b8f
Alfredo Sumaran <a.sumaran@gmail.com>
2021-06-15 17:37:33 -0500

    Remove duplicate assignment

--- includes/payment-methods/class-wc-stripe-payment-request.php
+++ includes/payment-methods/class-wc-stripe-payment-request.php
@@ -1128,8 +1128,6 @@ class WC_Stripe_Payment_Request {
 			// First empty the cart to prevent wrong calculation.
 			WC()->cart->empty_cart();
 
-			$product_type = $product->get_type();
-
 			if ( ( 'variable' === $product_type || 'variable-subscription' === $product_type ) && isset( $_POST['attributes'] ) ) {
 				$attributes = wc_clean( wp_unslash( $_POST['attributes'] ) );
 
