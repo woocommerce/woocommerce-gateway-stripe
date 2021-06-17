ea68ccaab14a582a29f220c241764d63de1d657c
Alfredo Sumaran <a.sumaran@gmail.com>
2021-06-17 18:16:11 -0500

    Exclude bundled items from display items list

--- includes/payment-methods/class-wc-stripe-payment-request.php
+++ includes/payment-methods/class-wc-stripe-payment-request.php
@@ -1471,6 +1471,11 @@ class WC_Stripe_Payment_Request {
 		// Default show only subtotal instead of itemization.
 		if ( ! apply_filters( 'wc_stripe_payment_request_hide_itemization', true ) || $itemized_display_items ) {
 			foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+				// Exclude bundled items to avoid making the list too big
+				if ( function_exists( 'wc_pb_is_bundled_cart_item' ) && wc_pb_is_bundled_cart_item( $cart_item ) ) {
+					continue;
+				}
+
 				$amount         = $cart_item['line_subtotal'];
 				$quantity_label = 1 < $cart_item['quantity'] ? ' (x' . $cart_item['quantity'] . ')' : '';
 
