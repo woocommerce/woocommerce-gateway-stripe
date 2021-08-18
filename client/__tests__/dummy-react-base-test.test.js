0c300001e90dd76f53b2a073cd0e4abf24d0ab54
frosso <francesco.rosso@automattic.com>
2021-08-18 16:12:48 -0500

    added a dummy react test

--- client/__tests__/dummy-react-base-test.test.js
+++ client/__tests__/dummy-react-base-test.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {screen, render} from '@testing-library/react'
-import userEvent from '@testing-library/user-event';
+import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 describe('Dummy React base test', () => {
 	it('should render a react component', () => {
@@ -8,17 +8,17 @@ describe('Dummy React base test', () => {
 			<div>Dummy element</div>
 		)
 
-		expect(screen.queryByText('Dummy element')).toBeInTheDocument();
+		expect(screen.queryByText('Dummy element')).toBeInTheDocument()
 	})
 
 	it('should test user events', () => {
-		const handleClick = jest.fn();
+		const handleClick = jest.fn()
 		render(<button onClick={handleClick}>Click me</button>)
 
-		expect(handleClick).not.toHaveBeenCalled();
+		expect(handleClick).not.toHaveBeenCalled()
 
-		userEvent.click(screen.getByText('Click me'));
+		userEvent.click(screen.getByText('Click me'))
 
-		expect(handleClick).toHaveBeenCalled();
+		expect(handleClick).toHaveBeenCalled()
 	})
 })
