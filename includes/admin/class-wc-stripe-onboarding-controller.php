d1c11a1f0b1314d552fd7efbc0aea9e595534d16
Harris Wong <harris.wong@automattic.com>
2021-08-24 17:29:56 -0600

    Remove unused code and lint fix

--- includes/admin/class-wc-stripe-onboarding-controller.php
+++ includes/admin/class-wc-stripe-onboarding-controller.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class WC_Stripe_Onboarding_Controller {
 	public function __construct() {
-        add_action( 'admin_menu', [ $this, 'add_onboarding_wizard' ] );
+		add_action( 'admin_menu', [ $this, 'add_onboarding_wizard' ] );
 		add_action( 'admin_enqueue_scripts', [ $this, 'admin_scripts' ] );
 	}
 
@@ -19,38 +19,43 @@ class WC_Stripe_Onboarding_Controller {
 	 * Load admin scripts.
 	 */
 	public function admin_scripts() {
-        // Webpack generates an assets file containing a dependencies array for our built JS file.
-        $script_path       = 'build/additional_methods_setup.js';
-        $script_asset_path = WC_STRIPE_PLUGIN_PATH . '/build/additional_methods_setup.asset.php';
-        $script_url        = plugins_url( $script_path, WC_STRIPE_MAIN_FILE );
-        $script_asset      = file_exists( $script_asset_path )
-            ? require $script_asset_path
-            : [ 'dependencies' => [] ];
+		// Webpack generates an assets file containing a dependencies array for our built JS file.
+		$script_path       = 'build/additional_methods_setup.js';
+		$script_asset_path = WC_STRIPE_PLUGIN_PATH . '/build/additional_methods_setup.asset.php';
+		$script_url        = plugins_url( $script_path, WC_STRIPE_MAIN_FILE );
+		$script_asset      = file_exists( $script_asset_path )
+			? require $script_asset_path
+			: [ 'dependencies' => [] ];
 
-        wp_register_script( // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
-            'wc_stripe_onboarding_wizard',
-            $script_url,
-            $script_asset['dependencies'],
-            null,
-            true
-        );
+		wp_register_script( // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
+			'wc_stripe_onboarding_wizard',
+			$script_url,
+			$script_asset['dependencies'],
+			null,
+			true
+		);
 
-        wp_enqueue_script( 'wc_stripe_onboarding_wizard' );
+		wp_enqueue_script( 'wc_stripe_onboarding_wizard' );
 	}
 
-    public function add_onboarding_wizard() {
-        add_submenu_page(
-            null, // Hide this submenu from admin menu
-            __( 'Onboarding Wizard', 'wc_stripe' ),
-            __( 'Onboarding Wizard', 'wc_stripe' ),
-            'manage_woocommerce',
-            'wc_stripe-onboarding_wizard',
-            [$this, 'render_onboarding_wizard']
-        );
-    }
+	/**
+	 * Create an admin page without a side menu: wp-admin/admin.php?page=wc_stripe-onboarding_wizard
+	 */
+	public function add_onboarding_wizard() {
+		add_submenu_page(
+			null, // Hide this submenu from admin menu
+			__( 'Onboarding Wizard', 'woocommerce-gateway-stripe' ),
+			__( 'Onboarding Wizard', 'woocommerce-gateway-stripe' ),
+			'manage_woocommerce',
+			'wc_stripe-onboarding_wizard',
+			[ $this, 'render_onboarding_wizard' ]
+		);
+	}
 
-    public function render_onboarding_wizard()
-    {
-        echo '<div id="wc-stripe-onboarding-wizard-container"></div>';
-    }
+	/**
+	 * Output a container for react app to mount on.
+	 */
+	public function render_onboarding_wizard() {
+		echo '<div id="wc-stripe-onboarding-wizard-container"></div>';
+	}
 }
