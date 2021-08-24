5f764c97b3138719c631b23130dd551888367dcc
frosso <francesco.rosso@automattic.com>
2021-08-24 11:50:26 -0500

    feat: add payment method icons

---
+++ client/payment-method-icons/apple-pay/index.js
@@ -0,0 +1,14 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import IconWithShell from '../styles/icon-with-shell';
+import icon from './icon.svg';
+
+const ApplePayIcon = ( props ) => <IconWithShell { ...props } src={ icon } />;
+
+export default ApplePayIcon;
