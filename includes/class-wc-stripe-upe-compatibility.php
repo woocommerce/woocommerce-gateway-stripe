dd087033e10a4b9a5bc5dca5c888c4a0923717a5
frosso <francesco.rosso@automattic.com>
2021-08-26 14:16:21 -0500

    added compatibility notice link

--- includes/class-wc-stripe-upe-compatibility.php
+++ includes/class-wc-stripe-upe-compatibility.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 class WC_Stripe_UPE_Compatibility {
 	const MIN_WP_VERSION  = '5.6';
 	const MIN_WC_VERSION  = '5.5';
-	const LEARN_MORE_LINK = '?TODO';
+	const LEARN_MORE_LINK = 'https://developer.woocommerce.com/2021/08/26/developer-advisory-woocommerce-stripe-support-policy-change/';
 
 	public static function is_wp_supported() {
 		return version_compare( get_bloginfo( 'version' ), self::MIN_WP_VERSION, '>=' );
