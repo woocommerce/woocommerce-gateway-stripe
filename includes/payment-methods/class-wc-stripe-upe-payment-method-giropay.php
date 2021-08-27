32c2253fd1fde8af10894b842635ad2b27d11804
Samir Merchant <samir.merchant@automattic.com>
2021-08-27 16:07:37 -0400

    giropay payment method is not reusable

--- includes/payment-methods/class-wc-stripe-upe-payment-method-giropay.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-method-giropay.php
@@ -18,6 +18,6 @@ class WC_Stripe_UPE_Payment_Method_Giropay extends WC_Stripe_UPE_Payment_Method 
 	public function __construct() {
 		$this->stripe_id   = self::STRIPE_ID;
 		$this->title       = 'Pay with Giropay';
-		$this->is_reusable = true; // TODO: is it?
+		$this->is_reusable = false; // TODO: is it?
 	}
 }
