b0116ac47893d984b0e40f6019d6420bfb25fb7e
Harris Wong <harris.wong@automattic.com>
2021-08-30 15:17:45 -0600

    Eslint fix

--- client/payment-methods-map.js
+++ client/payment-methods-map.js
@@ -7,12 +7,9 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import CreditCardIcon from './payment-method-icons/cards';
-// import BancontactIcon from './payment-method-icons/bancontact';
 import GiropayIcon from './payment-method-icons/giropay';
 import SofortIcon from './payment-method-icons/sofort';
 import SepaIcon from './payment-method-icons/sepa';
-// import P24Icon from './payment-method-icons/p24';
-// import IdealIcon from './payment-method-icons/ideal';
 
 export default {
 	card: {
@@ -25,16 +22,6 @@ export default {
 		Icon: CreditCardIcon,
 		currencies: [],
 	},
-	// bancontact: {
-	// 	id: 'bancontact',
-	// 	label: __( 'Bancontact', 'woocommerce-gateway-stripe' ),
-	// 	description: __(
-	// 		'Bancontact is a bank redirect payment method offered by more than 80% of online businesses in Belgium.',
-	// 		'woocommerce-gateway-stripe'
-	// 	),
-	// 	Icon: BancontactIcon,
-	// 	currencies: [ 'EUR' ],
-	// },
 	giropay: {
 		id: 'giropay',
 		label: __( 'giropay', 'woocommerce-gateway-stripe' ),
@@ -45,26 +32,6 @@ export default {
 		Icon: GiropayIcon,
 		currencies: [ 'EUR' ],
 	},
-	// ideal: {
-	// 	id: 'ideal',
-	// 	label: __( 'iDEAL', 'woocommerce-gateway-stripe' ),
-	// 	description: __(
-	// 		'Expand your business with iDEAL — Netherlands’s most popular payment method.',
-	// 		'woocommerce-gateway-stripe'
-	// 	),
-	// 	Icon: IdealIcon,
-	// 	currencies: [ 'EUR' ],
-	// },
-	// p24: {
-	// 	id: 'p24',
-	// 	label: __( 'Przelewy24 (P24)', 'woocommerce-gateway-stripe' ),
-	// 	description: __(
-	// 		'Accept payments with Przelewy24 (P24), the most popular payment method in Poland.',
-	// 		'woocommerce-gateway-stripe'
-	// 	),
-	// 	Icon: P24Icon,
-	// 	currencies: [ 'EUR', 'PLN' ],
-	// },
 	sepa_debit: {
 		id: 'sepa_debit',
 		label: __( 'Direct debit payment', 'woocommerce-gateway-stripe' ),
