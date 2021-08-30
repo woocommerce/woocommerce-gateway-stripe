a00876dff23107ecc4e81824c9833164d6847130
Harris Wong <harris.wong@automattic.com>
2021-08-30 16:29:40 -0600

    Remove "," for prettier

--- client/additional-methods-setup/upe-preview-methods-selector/__tests__/enable-upe-preview-task.test.js
+++ client/additional-methods-setup/upe-preview-methods-selector/__tests__/enable-upe-preview-task.test.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-import { render, screen, } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 /**
