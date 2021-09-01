181f139744225cae4a003bd0d25344dca91ac779
Mayisha <mayisha007@gmail.com>
2021-09-01 11:07:42 +0600

    Add Stripe banner as a component

---
+++ client/payment-method-icons/stripe-banner/index.js
@@ -0,0 +1,13 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import icon from './icon.svg';
+
+const StripeBanner = () => <img src={ icon } alt="" />;
+
+export default StripeBanner;
