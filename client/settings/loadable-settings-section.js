4a00c2b11f55a0df2e63991104260a0c16b05fb2
Waclaw Jacek <waclaw.jacek@automattic.com>
2021-09-07 15:10:52 +0200

    Port Loadable, LoadableBlock, and LoadableSettingsBlock

---
+++ client/settings/loadable-settings-section.js
@@ -0,0 +1,23 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import { useSettings } from '../data';
+import { LoadableBlock } from '../components/loadable';
+
+const LoadableSettingsSection = ( { children, numLines } ) => {
+	const { isLoading } = useSettings();
+
+	return (
+		<LoadableBlock isLoading={ isLoading } numLines={ numLines }>
+			{ children }
+		</LoadableBlock>
+	);
+};
+
+export default LoadableSettingsSection;
