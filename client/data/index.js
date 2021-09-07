6b263898897c902a2b3730ad7964bcc4c580c3ee
Waclaw Jacek <waclaw.jacek@automattic.com>
2021-09-07 16:19:34 +0200

    Add SaveSettingsSection data API tests

--- client/data/index.js
+++ client/data/index.js
@@ -11,5 +11,5 @@ initStore();
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const WC_STRIPE_STORE_NAME = STORE_NAME;
 
-// We only ask for hooks when importing directly from 'wc_stripe/data'.
+// We only ask for hooks when importing directly from 'wcstripe/data'.
 export * from './settings/hooks';
