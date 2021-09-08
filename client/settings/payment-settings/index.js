6ddac9d3e70a967fe8b089149cc53df786b3d8f2
Harris Wong <harris.wong@automattic.com>
2021-09-08 17:35:01 -0600

    Refactor into general settings section file

--- client/settings/payment-settings/index.js
+++ client/settings/payment-settings/index.js
@@ -1,17 +1,9 @@
 /**
  * External dependencies
  */
-import { React, useState } from 'react';
+import { React } from 'react';
 import { __ } from '@wordpress/i18n';
-import {
-	Button,
-	Card,
-	CardFooter,
-	CheckboxControl,
-	ExternalLink,
-} from '@wordpress/components';
-import interpolateComponents from 'interpolate-components';
-import styled from '@emotion/styled';
+import { Card, ExternalLink } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -21,6 +13,7 @@ import CardBody from '../card-body';
 import PaymentsAndTransactionsSection from '../payments-and-transactions-section';
 import AdvancedSettingsSection from '../advanced-settings-section';
 import CustomizationOptionsNotice from '../customization-options-notice';
+import GeneralSettingsSection from './general-settings-section';
 import './style.scss';
 
 const GeneralSettingsDescription = () => (
@@ -78,84 +71,6 @@ const PaymentsAndTransactionsDescription = () => (
 		</ExternalLink>
 	</>
 );
-
-const GeneralSettingsSection = () => {
-	const [ enableStripe, setEnableStripe ] = useState( false );
-	const [ enableTestMode, setEnableTestMode ] = useState( false );
-
-	const toggleEnableStripe = () => setEnableStripe( ! enableStripe );
-	const toggleEnableTestMode = () => setEnableTestMode( ! enableTestMode );
-
-	const CheckboxDescription = styled.span`
-		font-style: italic;
-		display: block;
-	`;
-
-	const GeneralSettingsOptionWrapper = styled.div`
-		&:first-child {
-			margin-top: 26px;
-		}
-		&:not( :last-child ) {
-			margin-bottom: 26px;
-		}
-		&:last-child {
-			margin-bottom: 24px;
-		}
-	`;
-
-	return (
-		<Card>
-			<CardBody>
-				<GeneralSettingsOptionWrapper>
-					<CheckboxControl
-						checked={ enableStripe }
-						onChange={ toggleEnableStripe }
-						label={ __(
-							'Enable Stripe',
-							'woocommerce-gateway-stripe'
-						) }
-					/>
-					<CheckboxDescription>
-						{ __(
-							'When enabled, payment methods powered by Stripe will appear on checkout.',
-							'woocommerce-gateway-stripe'
-						) }
-					</CheckboxDescription>
-				</GeneralSettingsOptionWrapper>
-
-				<GeneralSettingsOptionWrapper>
-					<CheckboxControl
-						checked={ enableTestMode }
-						onChange={ toggleEnableTestMode }
-						label={ __(
-							'Enable test mode',
-							'woocommerce-gateway-stripe'
-						) }
-					/>
-					<CheckboxDescription>
-						{ interpolateComponents( {
-							mixedString: __(
-								'Use {{testCardNumbersLink /}} to simulate various transactions. {{learnMoreLink/}}',
-								'woocommerce-gateway-stripe'
-							),
-							components: {
-								testCardNumbersLink: (
-									<a href="?TODO">test card numbers</a>
-								),
-								learnMoreLink: <a href="?TODO">Learn more</a>,
-							},
-						} ) }
-					</CheckboxDescription>
-				</GeneralSettingsOptionWrapper>
-			</CardBody>
-			<CardFooter>
-				<Button isSecondary href="?TODO">
-					{ __( 'Edit account keys', 'woocommerce-gateway-stripe' ) }
-				</Button>
-			</CardFooter>
-		</Card>
-	);
-};
 
 const AccountDetailsSection = () => {
 	return (
