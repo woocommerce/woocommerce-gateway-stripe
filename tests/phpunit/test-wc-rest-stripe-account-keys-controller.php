2155d1282686c7318fc5052cdbf8f5f6cef5a61b
Harris Wong <harris.wong@automattic.com>
2021-09-13 12:03:32 -0600

    Fix phplint

--- tests/phpunit/test-wc-rest-stripe-account-keys-controller.php
+++ tests/phpunit/test-wc-rest-stripe-account-keys-controller.php
@@ -48,11 +48,11 @@ class WC_REST_Stripe_Account_Keys_Controller_Test extends WP_UnitTestCase {
 		$expected = [
 			'settings' => [
 				'test_publishable_key' => 'original-test-key-9999',
-				'test_secret_key' => '',
-				'test_webhook_secret' => '',
-				'publishable_key' => 'original-live-key-9999',
-				'secret_key' => '',
-				'webhook_secret' => '',
+				'test_secret_key'      => '',
+				'test_webhook_secret'  => '',
+				'publishable_key'      => 'original-live-key-9999',
+				'secret_key'           => '',
+				'webhook_secret'       => '',
 			],
 		];
 
