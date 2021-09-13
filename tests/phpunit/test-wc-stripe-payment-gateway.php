a889898562027dce01ac72a6bb9ed18cfaf29202
Mayisha <mayisha007@gmail.com>
2021-09-13 16:16:09 +0600

    Move tests inside WC_Stripe_Payment_Gateway_Test class

--- tests/phpunit/test-wc-stripe-payment-gateway.php
+++ tests/phpunit/test-wc-stripe-payment-gateway.php
@@ -7,11 +7,18 @@
 
 class WC_Stripe_Payment_Gateway_Test extends WP_UnitTestCase {
 	/**
-	 * Gateway under test.
+	 * Stripe Gateway under test.
 	 *
 	 * @var WC_Gateway_Stripe
 	 */
 	private $gateway;
+
+	/**
+	 * Giropay Gateway under test.
+	 *
+	 * @var WC_Gateway_Stripe_Giropay
+	 */
+	private $giropay_gateway;
 
 	/**
 	 * Sets up things all tests need.
@@ -19,7 +26,8 @@ class WC_Stripe_Payment_Gateway_Test extends WP_UnitTestCase {
 	public function setUp() {
 		parent::setUp();
 
-		$this->gateway = new WC_Gateway_Stripe();
+		$this->gateway         = new WC_Gateway_Stripe();
+		$this->giropay_gateway = new WC_Gateway_Stripe_Giropay();
 	}
 
 	/**
@@ -27,6 +35,42 @@ class WC_Stripe_Payment_Gateway_Test extends WP_UnitTestCase {
 	 */
 	private function updateOrderMeta( $order, $key, $value ) {
 		$order->update_meta_data( $key, $value );
+	}
+
+	/**
+	 * Should print a placeholder div with id 'wc-stripe-upe-opt-in-banner'
+	 */
+	public function test_admin_options_when_stripe_is_connected() {
+		$stripe_settings                         = get_option( 'woocommerce_stripe_settings' );
+		$stripe_settings['enabled']              = 'yes';
+		$stripe_settings['testmode']             = 'yes';
+		$stripe_settings['test_publishable_key'] = 'pk_test_key';
+		$stripe_settings['test_secret_key']      = 'sk_test_key';
+		update_option( 'woocommerce_stripe_settings', $stripe_settings );
+		update_option( '_wcstripe_feature_upe_settings', 'yes' );
+
+		ob_start();
+		$this->giropay_gateway->admin_options();
+		$output = ob_get_clean();
+		$this->assertStringMatchesFormat( '%aid="wc-stripe-upe-opt-in-banner"%a', $output );
+	}
+
+	/**
+	 * Should print a placeholder div with id 'wc-stripe-new-account-container'
+	 */
+	public function test_admin_options_when_stripe_is_not_connected() {
+		$stripe_settings                         = get_option( 'woocommerce_stripe_settings' );
+		$stripe_settings['enabled']              = 'yes';
+		$stripe_settings['testmode']             = 'yes';
+		$stripe_settings['test_publishable_key'] = '';
+		$stripe_settings['test_secret_key']      = '';
+		update_option( 'woocommerce_stripe_settings', $stripe_settings );
+		update_option( '_wcstripe_feature_upe_settings', 'yes' );
+
+		ob_start();
+		$this->giropay_gateway->admin_options();
+		$output = ob_get_clean();
+		$this->assertStringMatchesFormat( '%aid="wc-stripe-new-account-container"%a', $output );
 	}
 
 	/**
