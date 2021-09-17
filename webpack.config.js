5653bab3d8b82f994e097d8a5dca0a59291b7659
frosso <francesco.rosso@automattic.com>
2021-09-17 17:08:02 -0500

    adding webpack to the dependencies to satisfy lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const webpack = require( 'webpack' );
 const path = require( 'path' );
+const webpack = require( 'webpack' );
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 const DependencyExtractionWebpackPlugin = require( '@woocommerce/dependency-extraction-webpack-plugin' );
 
