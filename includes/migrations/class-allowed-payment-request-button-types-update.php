c51ba96af9c577fe6438bf194dde2feda3546c67
frosso <francesco.rosso@automattic.com>
2021-09-16 19:56:12 -0500

    feat: express checkout options migration

---
+++ includes/migrations/class-allowed-payment-request-button-types-update.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Class Allowed_Payment_Request_Button_Types_Update
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class Allowed_Payment_Request_Button_Types_Update
+ *
+ * Remaps deprecated payment request button types to fallback values.
+ *
+ * @since 5.6.0
+ */
+class Allowed_Payment_Request_Button_Types_Update {
+	/**
+	 * Allowed_Payment_Request_Button_Types_Update constructor.
+	 */
+	public function __construct() {
+		add_action( 'woocommerce_stripe_updated', [ $this, 'maybe_migrate' ] );
+	}
+
+	/**
+	 * Only execute the migration if not applied yet.
+	 */
+	public function maybe_migrate() {
+		$gateways       = $this->get_gateways();
+		$stripe_gateway = $gateways[ WC_Gateway_Stripe::ID ];
+
+		// "custom" or "branded" are no longer valid values for the button type - map them to new ones
+		$button_type = $stripe_gateway->get_option( 'payment_request_button_type' );
+		if ( in_array( $button_type, [ 'branded', 'custom' ], true ) ) {
+			$branded_type = $stripe_gateway->get_option( 'payment_request_button_branded_type' );
+			$stripe_gateway->update_option(
+				'payment_request_button_type',
+				$this->map_button_type( $button_type, $branded_type )
+			);
+		}
+	}
+
+	/**
+	 * Maps deprecated button types to fallback values.
+	 *
+	 * @param mixed $button_type "payment_request_button_type" value.
+	 * @param mixed $branded_type "payment_request_button_branded_type" value.
+	 *
+	 * @return mixed
+	 */
+	private function map_button_type( $button_type, $branded_type ) {
+		// "branded" with "logo only" => "default" (same result)
+		if ( 'branded' === $button_type && 'short' === $branded_type ) {
+			return 'default';
+		}
+
+		// "branded" with anything else (which would be "long"/"Text and logo") => "buy"
+		if ( 'branded' === $button_type ) {
+			return 'buy';
+		}
+
+		// "custom" is no longer valid => "default"
+		if ( 'custom' === $button_type ) {
+			return 'buy';
+		}
+
+		// anything else is good
+		return $button_type;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function get_gateways(): array {
+		return WC()->payment_gateways()->payment_gateways();
+	}
+}
---
+++ tests/phpunit/admin/migrations/test-class-allowed-payment-request-button-types-update.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Class Allowed_Payment_Request_Button_Types_Update_Test
+ */
+
+use PHPUnit\Framework\MockObject\MockObject;
+
+/**
+ * Allowed_Payment_Request_Button_Types_Update unit tests.
+ */
+class Allowed_Payment_Request_Button_Types_Update_Test extends WP_UnitTestCase {
+
+	/**
+	 * WCPay gateway mock.
+	 *
+	 * @var MockObject|WC_Gateway_Stripe
+	 */
+	private $gateway_mock;
+
+	/**
+	 * @var Allowed_Payment_Request_Button_Types_Update
+	 */
+	private $migration;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->gateway_mock = $this->getMockBuilder( WC_Gateway_Stripe::class )
+								   ->disableOriginalConstructor()
+								   ->getMock();
+		$this->migration    = $this->getMockBuilder( Allowed_Payment_Request_Button_Types_Update::class )
+								   ->disableOriginalConstructor()
+								   ->setMethods( [ 'get_gateways' ] )
+								   ->getMock();
+	}
+
+	/**
+	 * @dataProvider deprecated_values_provider
+	 */
+	public function test_it_maps_deprecated_button_type_values( string $button_type, string $branded_type, string $expected_mapped_value ) {
+		$old_settings = [
+			'payment_request_button_type'         => $button_type,
+			'payment_request_button_branded_type' => $branded_type,
+		];
+
+		$this->setup_environment( $old_settings );
+		$this->gateway_mock->expects( $this->once() )
+						   ->method( 'update_option' )
+						   ->with( 'payment_request_button_type', $expected_mapped_value );
+
+		$this->migration->maybe_migrate();
+	}
+
+	/**
+	 * @dataProvider not_deprecated_values_provider
+	 */
+	public function test_it_does_not_map_values_other_than_deprecated( $button_type ) {
+		$this->setup_environment( [ 'payment_request_button_type' => $button_type ] );
+		$this->gateway_mock->expects( $this->never() )
+						   ->method( 'update_option' );
+
+		$this->migration->maybe_migrate();
+	}
+
+	private function setup_environment( $settings ) {
+		$this->gateway_mock->method( 'get_option' )
+						->willReturnCallback(
+							function ( $key ) use ( $settings ) {
+								return $settings[ $key ] ?? '';
+							}
+						);
+		$this->migration->method( 'get_gateways' )->willReturn( [ 'stripe' => $this->gateway_mock ] );
+
+	}
+
+	public function deprecated_values_provider() {
+		return [
+			'branded with type = short mapped to default' => [ 'branded', 'short', 'default' ],
+			'branded with type != short mapped to buy'    => [ 'branded', 'foo', 'buy' ],
+			'branded with missing type mapped to buy'     => [ 'branded', '', 'buy' ],
+			'custom mapped to buy'                        => [ 'custom', '', 'buy' ],
+		];
+	}
+
+	public function not_deprecated_values_provider() {
+		return [
+			'empty value' => [ '' ],
+			[ 'foo' ],
+			[ 'default' ],
+			[ 'buy' ],
+			[ 'donate' ],
+		];
+	}
+}
