3324a868bd52eb83739f1577c9a22b880a0f36c1
Brett Shumaker <brettshumaker@gmail.com>
2021-09-20 13:29:25 -0400

    Adds statement previews for full and shortened bank statement text. (#1871)
    This creates a new StatementPreview component that accepts icon, title, text, and className props. The icon should be a string that matches a pre-defined property name of an icon SVG in the component. Current options for this are `creditCard` and `bank`. The title is a string of text shown at the top of the preview and describes the preview. text is the text that will show under the "Transaction" heading on the preview. And className will be added to the outer div of the preview card.
    
    The "amount" shown on the card will respect the shop's currency settings for currency symbol, currency symbol position, decimal separator, and precision.

---
+++ client/settings/payments-and-transactions-section/statement-preview/icons/creditCard.js
@@ -0,0 +1,31 @@
+export const CreditCardIcon = (
+	<svg
+		width="17"
+		height="16"
+		viewBox="0 0 17 16"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		role="img"
+		aria-label="credit card icon"
+	>
+		<mask
+			id="mask-cc"
+			style={ { maskType: 'alpha' } }
+			maskUnits="userSpaceOnUse"
+			x="1"
+			y="2"
+			width="14"
+			height="12"
+		>
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M13.3647 2.66669H2.69808C1.95808 2.66669 1.37141 3.26002 1.37141 4.00002L1.36475 12C1.36475 12.74 1.95808 13.3334 2.69808 13.3334H13.3647C14.1047 13.3334 14.6981 12.74 14.6981 12V4.00002C14.6981 3.26002 14.1047 2.66669 13.3647 2.66669ZM13.3647 12H2.69808V8.00002H13.3647V12ZM2.69808 5.33335H13.3647V4.00002H2.69808V5.33335Z"
+				fill="white"
+			/>
+		</mask>
+		<g mask="url(#mask-cc)">
+			<rect x="0.0314941" width="16" height="16" fill="#1E1E1E" />
+		</g>
+	</svg>
+);
