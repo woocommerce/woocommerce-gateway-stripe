0489554d78780c6ca13f8596ef52fc87a3afc7af
Brett Shumaker <brettshumaker@gmail.com>
2021-09-22 13:00:19 -0400

    Add test that the `DisableConfirmationModal` calls the correct handlers.

---
+++ client/payment-gateways/__tests__/index.test.js
@@ -0,0 +1,38 @@
+/**
+ * External dependencies
+ */
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+/**
+ * Internal dependencies
+ */
+import DisableConfirmationModal from '../disable-confirmation-modal';
+
+jest.mock( '../../data', () => ( {
+	useEnabledPaymentMethodIds: jest.fn().mockReturnValue( [ [] ] ),
+	usePaymentRequestEnabledSettings: jest.fn().mockReturnValue( '' ),
+} ) );
+
+describe( 'DisableConfirmationModal', () => {
+	it( 'calls the onClose handler on cancel', async () => {
+		const handleCloseMock = jest.fn();
+		render( <DisableConfirmationModal onClose={ handleCloseMock } /> );
+
+		expect( handleCloseMock ).not.toHaveBeenCalled();
+
+		userEvent.click( screen.getByText( 'Cancel' ) );
+
+		expect( handleCloseMock ).toHaveBeenCalled();
+	} );
+
+	it( 'calls the onConfirm handler on cancel', async () => {
+		const handleConfirmMock = jest.fn();
+		render( <DisableConfirmationModal onConfirm={ handleConfirmMock } /> );
+
+		expect( handleConfirmMock ).not.toHaveBeenCalled();
+
+		userEvent.click( screen.getByText( 'Disable' ) );
+
+		expect( handleConfirmMock ).toHaveBeenCalled();
+	} );
+} );
