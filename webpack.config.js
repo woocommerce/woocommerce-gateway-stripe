e1c4ec1c5c85e8877e9afd05fa15ec79108f13f9
Brett Shumaker <brettshumaker@gmail.com>
2021-09-22 16:28:47 -0400

    Adds "Disable Stripe" confirmation modal (#1932)
    This PR adds a "disable Stripe" confirmation modal when a user attempts to disable the Stripe payment method when UPE is enabled and they have 1 or more payment methods added or express checkouts are enabled. If the user cancels the modal, the gateway remains enabled, if they confirm it the gateway is disabled.

--- webpack.config.js
+++ webpack.config.js
@@ -40,5 +40,6 @@ module.exports = {
 		upe_onboarding_wizard: './client/upe-onboarding-wizard/index.js',
 		upe_opt_in_banner: './client/entrypoints/upe-opt-in-banner/index.js',
 		upe_settings: './client/settings/index.js',
+		payment_gateways: './client/entrypoints/payment-gateways/index.js',
 	},
 };
