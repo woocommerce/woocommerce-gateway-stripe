2d8cef1b5a8ce2744a5401fca8aba27d7eae1bf7
Brett Shumaker <brettshumaker@gmail.com>
2021-09-22 15:11:03 -0400

    Updated path in test

--- client/entrypoints/payment-gateways/__tests__/index.test.js
+++ client/entrypoints/payment-gateways/__tests__/index.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import DisableConfirmationModal from '../disable-confirmation-modal';
 
-jest.mock( '../../data', () => ( {
+jest.mock( '../../../data', () => ( {
 	useEnabledPaymentMethodIds: jest.fn().mockReturnValue( [ [] ] ),
 	usePaymentRequestEnabledSettings: jest.fn().mockReturnValue( '' ),
 } ) );
