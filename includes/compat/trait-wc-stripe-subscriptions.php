7e511961cecb5cf0f084344a9a4ddc65aaa59f1e
Kristófer R <kristofer.thorlaksson@automattic.com>
2021-09-28 20:20:26 -0400

    Fix fatal PHP errors and API errors

--- includes/compat/trait-wc-stripe-subscriptions.php
+++ includes/compat/trait-wc-stripe-subscriptions.php
@@ -93,14 +93,15 @@ trait WC_Stripe_Subscriptions_Trait {
 			return $request;
 		}
 
-		$sub = $subscriptions[0];
+		$sub = reset( $subscriptions );
 
 		// TODO: Maybe the order ID isn't unique enough? What happens when there are multiple subscriptions
 		// with different payment intervals in the order?
+		$start_date = new DateTime( $sub->get_date( 'start', 'gmt' ) ); // TODO: Better to use site timezone?
 		$request['payment_method_options']['card']['mandate_options']['reference']      = $order->get_id();
-		$request['payment_method_options']['card']['mandate_options']['amount']         = $order->get_total();
+		$request['payment_method_options']['card']['mandate_options']['amount']         = WC_Stripe_Helper::get_stripe_amount( $order->get_total() );
 		$request['payment_method_options']['card']['mandate_options']['amount_type']    = 'fixed'; // TODO: Are there any cases where amount is variable?
-		$request['payment_method_options']['card']['mandate_options']['start_date']     = $sub->get_date( 'start', 'gmt' ); // TODO: Better to use site timezone?
+		$request['payment_method_options']['card']['mandate_options']['start_date']     = $start_date->getTimestamp();
 		$request['payment_method_options']['card']['mandate_options']['interval']       = $sub->get_billing_period();
 		$request['payment_method_options']['card']['mandate_options']['interval_count'] = $sub->get_billing_interval();
 
