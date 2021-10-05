8565638eb4234f8984a4078f6135c9aaace4b1f8
Mayisha <mayisha007@gmail.com>
2021-10-05 15:12:21 +0600

    Enqueue overlay script inside WC_Stripe_Payment_Gateways_Controller class

--- includes/admin/class-wc-stripe-payment-gateways-controller.php
+++ includes/admin/class-wc-stripe-payment-gateways-controller.php
@@ -25,6 +25,13 @@ class WC_Stripe_Payment_Gateways_Controller {
 			add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_payments_scripts' ] );
 			add_action( 'woocommerce_admin_field_payment_gateways', [ $this, 'wc_stripe_gateway_container' ] );
 		}
+
+		$show_information_overlay = get_option( 'wc_stripe_show_information_overlay' );
+
+		if ( empty( $show_information_overlay ) ) {
+			add_action( 'admin_enqueue_scripts', [ $this, 'information_overlay_script' ] );
+			add_action( 'woocommerce_admin_field_payment_gateways', [ $this, 'wc_stripe_information_overlay_container' ], 5 );
+		}
 	}
 
 	public function register_payments_scripts() {
@@ -75,6 +82,52 @@ class WC_Stripe_Payment_Gateways_Controller {
 	 */
 	public function wc_stripe_gateway_container() {
 		?><div id="wc-stripe-payment-gateways-container" />
+		<?php
+	}
+
+	/**
+	 * Enqueues the script to show overlay once UPE has been enabled.
+	 */
+	public function information_overlay_script() {
+		if ( ! is_admin() ) {
+			return;
+		}
+
+		global $current_tab, $current_section;
+
+		if ( ! isset( $current_tab ) || 'checkout' !== $current_tab ) {
+			return;
+		}
+
+		if ( ! empty( $current_section ) ) {
+			return;
+		}
+
+		// Webpack generates an assets file containing a dependencies array for our built JS file.
+		$script_asset_path = WC_STRIPE_PLUGIN_PATH . '/build/upe_information_overlay.asset.php';
+		$script_asset      = file_exists( $script_asset_path )
+			? require $script_asset_path
+			: [
+				'dependencies' => [],
+				'version'      => WC_STRIPE_VERSION,
+			];
+
+		wp_register_script(
+			'woocommerce_stripe_upe_information_overlay',
+			plugins_url( 'build/upe_information_overlay.js', WC_STRIPE_MAIN_FILE ),
+			$script_asset['dependencies'],
+			$script_asset['version'],
+			true
+		);
+		wp_enqueue_script( 'woocommerce_stripe_upe_information_overlay' );
+	}
+
+	/**
+	 * Adds a container to the "payment gateways" page to render the information overlay
+	 */
+	public function wc_stripe_information_overlay_container() {
+		?>
+		<div id="wc-stripe-information-overlay-container" />
 		<?php
 	}
 
