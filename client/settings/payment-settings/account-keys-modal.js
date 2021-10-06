680abbb71c6d139910678ac1787b80dba4f7e851
thomas shellberg <tommy.shellberg@automattic.com>
2021-10-06 15:32:11 +0200

    Load keys from settings.

--- client/settings/payment-settings/account-keys-modal.js
+++ client/settings/payment-settings/account-keys-modal.js
@@ -1,13 +1,101 @@
 import { __ } from '@wordpress/i18n';
-import { React } from 'react';
+import { React, useState } from 'react';
 import { Button, TextControl } from '@wordpress/components';
 import interpolateComponents from 'interpolate-components';
+import { useAccountKeys } from 'wcstripe/data/account-keys';
 import { useTestMode } from 'wcstripe/data';
 import ConfirmationModal from 'wcstripe/components/confirmation-modal';
 import InlineNotice from 'wcstripe/components/inline-notice';
 
 export const AccountKeysModal = ( { type, onClose } ) => {
 	const [ , setTestMode ] = useTestMode();
+	const { accountKeys, isSaving } = useAccountKeys();
+
+	// @todo - make a higher-order component for these and pass in label and help?
+	const PublishableKey = () => {
+		const [ value, setValue ] = useState(
+			type === 'test'
+				? accountKeys.test_publishable_key
+				: accountKeys.publishable_key
+		);
+		return (
+			<TextControl
+				label={
+					type === 'test'
+						? __(
+								'Test publishable key',
+								'woocommerce-gateway-stripe'
+						  )
+						: __(
+								'Live publishable key',
+								'woocommerce-gateway-stripe'
+						  )
+				}
+				help={
+					type === 'test'
+						? __(
+								'Only values starting with "pk_test_" will be saved.',
+								'woocommerce-gateway-stripe'
+						  )
+						: __(
+								'Only values starting with "pk_live_" will be saved.',
+								'woocommerce-gateway-stripe'
+						  )
+				}
+				value={ value }
+				onChange={ ( val ) => setValue( val ) }
+			/>
+		);
+	};
+
+	const SecretKey = () => {
+		const [ value, setValue ] = useState(
+			type === 'test'
+				? accountKeys.test_secret_key
+				: accountKeys.secret_key
+		);
+		return (
+			<TextControl
+				label={
+					type === 'test'
+						? __( 'Test secret key', 'woocommerce-gateway-stripe' )
+						: __( 'Live secret key', 'woocommerce-gateway-stripe' )
+				}
+				help={
+					type === 'test'
+						? __(
+								'Only values starting with "sk_test_" or "rk_test_" will be saved.',
+								'woocommerce-gateway-stripe'
+						  )
+						: __(
+								'Only values starting with "sk_live_" or "rk_live_" will be saved.',
+								'woocommerce-gateway-stripe'
+						  )
+				}
+				value={ value }
+				onChange={ ( val ) => setValue( val ) }
+			/>
+		);
+	};
+
+	const WebhookSecret = () => {
+		const [ value, setValue ] = useState(
+			type === 'test'
+				? accountKeys.test_webhook_secret
+				: accountKeys.webhook_secret
+		);
+		return (
+			<TextControl
+				label={ __( 'Webhook secret', 'woocommerce-gateway-stripe' ) }
+				help={ __(
+					'Get your webhook signing secret from the webhooks section in your Stripe account.',
+					'woocommerce-gateway-stripe'
+				) }
+				value={ value }
+				onChange={ ( val ) => setValue( val ) }
+			/>
+		);
+	};
 
 	const handleSave = () => {
 		setTestMode( type === 'test' );
@@ -22,7 +110,12 @@ export const AccountKeysModal = ( { type, onClose } ) => {
 					<Button isSecondary onClick={ onClose }>
 						{ __( 'Cancel', 'woocommerce-gateway-stripe' ) }
 					</Button>
-					<Button isPrimary onClick={ handleSave }>
+					<Button
+						isPrimary
+						isBusy={ isSaving }
+						disabled={ isSaving }
+						onClick={ handleSave }
+					>
 						{ __( 'Save changes', 'woocommerce-gateway-stripe' ) }
 					</Button>
 				</>
@@ -66,55 +159,9 @@ export const AccountKeysModal = ( { type, onClose } ) => {
 							},
 					  } ) }
 			</InlineNotice>
-			<TextControl
-				label={
-					type === 'test'
-						? __(
-								'Test publishable key',
-								'woocommerce-gateway-stripe'
-						  )
-						: __(
-								'Live publishable key',
-								'woocommerce-gateway-stripe'
-						  )
-				}
-				help={
-					type === 'test'
-						? __(
-								'Only values starting with "pk_test_" will be saved.',
-								'woocommerce-gateway-stripe'
-						  )
-						: __(
-								'Only values starting with "pk_live_" will be saved.',
-								'woocommerce-gateway-stripe'
-						  )
-				}
-			/>
-			<TextControl
-				label={
-					type === 'test'
-						? __( 'Test secret key', 'woocommerce-gateway-stripe' )
-						: __( 'Live secret key', 'woocommerce-gateway-stripe' )
-				}
-				help={
-					type === 'test'
-						? __(
-								'Only values starting with "sk_test_" or "rk_test_" will be saved.',
-								'woocommerce-gateway-stripe'
-						  )
-						: __(
-								'Only values starting with "sk_live_" or "rk_live_" will be saved.',
-								'woocommerce-gateway-stripe'
-						  )
-				}
-			/>
-			<TextControl
-				label={ __( 'Webhook secret', 'woocommerce-gateway-stripe' ) }
-				help={ __(
-					'Get your webhook signing secret from the webhooks section in your Stripe account.',
-					'woocommerce-gateway-stripe'
-				) }
-			/>
+			<PublishableKey />
+			<SecretKey />
+			<WebhookSecret />
 		</ConfirmationModal>
 	);
 };
