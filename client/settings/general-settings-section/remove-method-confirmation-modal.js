f4c5ddd9378a7c0303349e873872ea32cf5e3489
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2021-10-25 15:00:56 -0300

    Fix modal overflow on small screens

--- client/settings/general-settings-section/remove-method-confirmation-modal.js
+++ client/settings/general-settings-section/remove-method-confirmation-modal.js
@@ -7,6 +7,16 @@ import AlertTitle from 'wcstripe/components/confirmation-modal/alert-title';
 
 const RemoveMethodConfirmationModal = ( { method, onClose, onConfirm } ) => {
 	const { label } = PaymentMethodsMap[ method ];
+
+	const confirmMethodRemovalString = sprintf(
+		/* translators: %1: payment method name (e.g.: giropay, EPS, Sofort, etc). */
+		__(
+			'Are you sure you want to remove %1$s? Your customers will no longer be able to pay using %1$s.',
+			'woocommerce-gateway-stripe'
+		),
+		`<strong>${ label }</strong>`
+	);
+
 	return (
 		<ConfirmationModal
 			title={
@@ -33,16 +43,11 @@ const RemoveMethodConfirmationModal = ( { method, onClose, onConfirm } ) => {
 				</>
 			}
 		>
-			<p>
-				{ sprintf(
-					/* translators: %1: payment method name (e.g.: giropay, EPS, Sofort, etc). */
-					__(
-						'Are you sure you want to remove %1$s? Your customers will no longer be able to pay using %1$s.',
-						'woocommerce-gateway-stripe'
-					),
-					label
-				) }
-			</p>
+			<p
+				dangerouslySetInnerHTML={ {
+					__html: confirmMethodRemovalString,
+				} }
+			/>
 			<p>
 				{ __(
 					'You can add it again at any time in Stripe settings.',
