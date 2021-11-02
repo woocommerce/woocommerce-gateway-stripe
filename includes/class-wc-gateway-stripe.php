fd116387eed8909f1829eccdfd81bb1342f25764
Kristófer R <kristofer.thorlaksson@automattic.com>
2021-11-02 16:48:29 -0400

    Only load Stripe JS on the cart page when required
    Previously we always loaded the Stripe JS on relevant pages through the
    main gateway class, but now we have an additional check there to make
    sure this doesn't happen.

--- includes/class-wc-gateway-stripe.php
+++ includes/class-wc-gateway-stripe.php
@@ -415,6 +415,17 @@ class WC_Gateway_Stripe extends WC_Stripe_Payment_Gateway {
 			return;
 		}
 
+		if (
+			is_cart()
+			&& ! in_array(
+				'cart',
+				$this->get_option( 'payment_request_button_locations', [ 'product', 'cart' ] ),
+				true
+			)
+		) {
+			return;
+		}
+
 		// If Stripe is not enabled bail.
 		if ( 'no' === $this->enabled ) {
 			return;
