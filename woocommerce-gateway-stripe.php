06d6237c370db108f11ff3311f595b0811aab52e
Dean Taylor <deanmarktaylor@gmail.com>
2021-11-05 00:31:51 +0000

    Support async attributes via `wp_script_add_data`

--- woocommerce-gateway-stripe.php
+++ woocommerce-gateway-stripe.php
@@ -652,6 +652,38 @@ function woocommerce_gateway_stripe_init() {
 		add_action( 'admin_notices', 'woocommerce_stripe_wc_not_supported' );
 		return;
 	}
+	
+	add_filter( 'script_loader_tag',
+		/**
+		 * Adds async/defer attributes to enqueued / registered scripts.
+		 *
+		 * If #12009 lands in WordPress, this function can no-op since it would be handled in core.
+		 * @link https://core.trac.wordpress.org/ticket/12009
+		 *
+		 * Source:
+		 * @link https://github.com/WordPress/twentytwenty/blob/master/classes/class-twentytwenty-script-loader.php
+		 *
+		 *
+		 * @param string $tag The script tag.
+		 * @param string $handle The script handle.
+		 *
+		 * @return string Script HTML string.
+		 */
+		function ( $tag, $handle ) {
+			foreach ( [ 'async', 'defer' ] as $attr ) {
+				if ( ! wp_scripts()->get_data( $handle, $attr ) ) {
+					continue;
+				}
+				// Prevent adding attribute when already added in #12009.
+				if ( ! preg_match( ":\s$attr(=|>|\s):", $tag ) ) {
+					$tag = preg_replace( ':(?=></script>):', " $attr", $tag, 1 );
+				}
+				// Only allow async or defer, not both.
+				break;
+			}
+
+			return $tag;
+		}, 10, 2 );
 
 	woocommerce_gateway_stripe();
 }
