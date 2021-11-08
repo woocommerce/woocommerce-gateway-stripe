f1c0bcc5af749b2ff7d72de776728f8cc3dc1388
César Costa <costa.cesar.a@hotmail.com>
2021-11-08 16:23:33 -0300

    Add multi-currency support to missing currency pill

--- client/components/payment-method-missing-currency-pill/index.js
+++ client/components/payment-method-missing-currency-pill/index.js
@@ -1,10 +1,11 @@
 import { __, sprintf } from '@wordpress/i18n';
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
 import styled from '@emotion/styled';
 import PaymentMethodsMap from '../../payment-methods-map';
+import WCPaySettingsContext from '../../settings/wcpay-settings-context';
+import UpeToggleContext from '../../settings/upe-toggle/context';
 import Pill from 'wcstripe/components/pill';
 import Tooltip from 'wcstripe/components/tooltip';
-import UpeToggleContext from 'wcstripe/settings/upe-toggle/context';
 
 const StyledPill = styled( Pill )`
 	border: 1px solid #f0b849;
@@ -15,8 +16,28 @@ const StyledPill = styled( Pill )`
 
 const PaymentMethodMissingCurrencyPill = ( { id, label } ) => {
 	const { isUpeEnabled } = useContext( UpeToggleContext );
-	const paymentMethodCurrencies = PaymentMethodsMap[ id ]?.currencies || [];
+	const { multiCurrency } = useContext( WCPaySettingsContext );
+	const paymentMethodCurrencies = useMemo(
+		() => PaymentMethodsMap[ id ]?.currencies || [],
+		[ id ]
+	);
 	const storeCurrency = window?.wcSettings?.currency?.code;
+
+	// adding support to multi-currency in case WooCommerce Payments is installed
+	const enabledCurrencies = useMemo(
+		() => Object.keys( multiCurrency?.enabled || {} ),
+		[ multiCurrency ]
+	);
+	const isMultiCurrencyEnabled = enabledCurrencies.length > 0;
+	// intersect all enabled currencies with the payment method currencies
+	const multiCurrencyIntersection = useMemo( () => {
+		if ( isMultiCurrencyEnabled ) {
+			return enabledCurrencies.filter( ( currency ) =>
+				paymentMethodCurrencies.includes( currency )
+			);
+		}
+		return [];
+	}, [ isMultiCurrencyEnabled, enabledCurrencies, paymentMethodCurrencies ] );
 
 	if ( ! isUpeEnabled ) {
 		return null;
@@ -24,7 +45,10 @@ const PaymentMethodMissingCurrencyPill = ( { id, label } ) => {
 
 	if (
 		id !== 'card' &&
-		! paymentMethodCurrencies.includes( storeCurrency )
+		! (
+			paymentMethodCurrencies.includes( storeCurrency ) ||
+			multiCurrencyIntersection.length
+		)
 	) {
 		return (
 			<Tooltip
