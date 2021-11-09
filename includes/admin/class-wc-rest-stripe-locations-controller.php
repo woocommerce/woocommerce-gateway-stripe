37b9aef9a7af3913befa5af7456d1130f60e5218
Brian Yu <brian@brianyu.me>
2021-11-09 14:59:06 -0500

    Add API route for updating terminal location

--- includes/admin/class-wc-rest-stripe-locations-controller.php
+++ includes/admin/class-wc-rest-stripe-locations-controller.php
@@ -48,9 +48,18 @@ class WC_REST_Stripe_Locations_Controller extends WC_Stripe_REST_Base_Controller
 				'callback'            => [ $this, 'get_all_locations' ],
 				'permission_callback' => [ $this, 'check_permission' ],
 				'args'                => [
-					'ending_before'  => [ 'type' => 'string' ],
-					'limit'          => [ 'type' => 'integer' ],
-					'starting_after' => [ 'type' => 'string' ],
+					'ending_before'  => [
+						'type' => 'string',
+						'required' => false,
+					],
+					'limit'          => [
+						'type' => 'integer',
+						'required' => false,
+					],
+					'starting_after' => [
+						'type' => 'string',
+						'required' => false,
+					],
 				],
 			]
 		);
@@ -61,6 +70,25 @@ class WC_REST_Stripe_Locations_Controller extends WC_Stripe_REST_Base_Controller
 				'methods'             => WP_REST_Server::READABLE,
 				'callback'            => [ $this, 'get_location' ],
 				'permission_callback' => [ $this, 'check_permission' ],
+			]
+		);
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base . '/(?P<location_id>\w+)',
+			[
+				'methods'             => WP_REST_Server::CREATABLE,
+				'callback'            => [ $this, 'update_location' ],
+				'permission_callback' => [ $this, 'check_permission' ],
+				'args'                => [
+					'display_name' => [
+						'type'     => 'string',
+						'required' => false,
+					],
+					'address'      => [
+						'type'     => 'object',
+						'required' => false,
+					],
+				],
 			]
 		);
 	}
@@ -97,6 +125,23 @@ class WC_REST_Stripe_Locations_Controller extends WC_Stripe_REST_Base_Controller
 	 */
 	public function get_location( $request ) {
 		$response = WC_Stripe_API::request( [], 'terminal/locations/' . urlencode( $request['location_id'] ), 'GET' );
+		return rest_ensure_response( $response );
+	}
+
+	/**
+	 * Update a terminal location via Stripe API.
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 */
+	public function update_location( $request ) {
+		$body = [];
+		if ( isset( $request['display_name'] ) ) {
+			$body['display_name'] = $request['display_name'];
+		}
+		if ( isset( $request['address'] ) ) {
+			$body['address'] = $request['address'];
+		}
+		$response = WC_Stripe_API::request( $body, 'terminal/locations/' . urlencode( $request['location_id'] ), 'POST' );
 		return rest_ensure_response( $response );
 	}
 
