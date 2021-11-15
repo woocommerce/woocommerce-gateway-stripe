67c77764417579b7842a5e4207ce295c31704be4
Hector Lovo <hector.lovo@automattic.com>
2021-11-15 16:39:33 -0500

    Fix broken link "https://docs.woocommerce.com/document/stripe/#styling"
    The "#styling" section does not exist. Instead, linking to the following
    "https://woocommerce.com/document/stripe/#new-checkout-experience".
    
    For more info, see the following conversation:
    https://github.com/woocommerce/woocommerce-gateway-stripe/issues/2149#issuecomment-969299485

--- includes/admin/class-wc-stripe-admin-notices.php
+++ includes/admin/class-wc-stripe-admin-notices.php
@@ -138,7 +138,7 @@ class WC_Stripe_Admin_Notices {
 
 			if ( empty( $show_style_notice ) ) {
 				/* translators: 1) int version 2) int version */
-				$message = __( 'WooCommerce Stripe - We recently made changes to Stripe that may impact the appearance of your checkout. If your checkout has changed unexpectedly, please follow these <a href="https://docs.woocommerce.com/document/stripe/#styling" target="_blank">instructions</a> to fix.', 'woocommerce-gateway-stripe' );
+				$message = __( 'WooCommerce Stripe - We recently made changes to Stripe that may impact the appearance of your checkout. If your checkout has changed unexpectedly, please follow these <a href="https://woocommerce.com/document/stripe/#new-checkout-experience" target="_blank">instructions</a> to fix.', 'woocommerce-gateway-stripe' );
 
 				$this->add_admin_notice( 'style', 'notice notice-warning', $message, true );
 
