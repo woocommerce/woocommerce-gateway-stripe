b56182d253056e2dfb14e9a04a8e1b82c94669f5
Ricardo Metring <ricardometring@gmail.com>
2021-11-15 23:26:31 -0300

    Add some tests

--- client/settings/payment-settings/__tests__/general-settings-section.test.js
+++ client/settings/payment-settings/__tests__/general-settings-section.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import GeneralSettingsSection from '../general-settings-section';
-import { useIsStripeEnabled, useTestMode } from 'wcstripe/data';
+import {
+	useIsStripeEnabled,
+	useTestMode,
+	useTitle,
+	useUpeTitle,
+	useDescription,
+} from 'wcstripe/data';
 import {
 	useAccountKeys,
 	useAccountKeysPublishableKey,
@@ -12,10 +18,14 @@ import {
 	useAccountKeysTestSecretKey,
 	useAccountKeysTestWebhookSecret,
 } from 'wcstripe/data/account-keys/hooks';
+import UpeToggleContext from 'wcstripe/settings/upe-toggle/context';
 
 jest.mock( 'wcstripe/data', () => ( {
 	useIsStripeEnabled: jest.fn(),
 	useTestMode: jest.fn(),
+	useTitle: jest.fn().mockReturnValue( [] ),
+	useUpeTitle: jest.fn().mockReturnValue( [] ),
+	useDescription: jest.fn().mockReturnValue( [] ),
 } ) );
 
 jest.mock( 'wcstripe/data/account-keys/hooks', () => ( {
@@ -108,5 +118,30 @@ describe( 'GeneralSettingsSection', () => {
 			/edit test account keys & webhooks/i
 		);
 		expect( accountKeysModal ).toBeInTheDocument();
+	} );
+
+	it( 'should render gateway title and description fields when UPE is disabled', () => {
+		useTitle.mockReturnValue( [ 'Title', jest.fn() ] );
+		useUpeTitle.mockReturnValue( [ 'UPE title', jest.fn() ] );
+		useDescription.mockReturnValue( [ 'Description', jest.fn() ] );
+
+		render( <GeneralSettingsSection /> );
+
+		expect( screen.getByDisplayValue( 'Title' ) ).toBeInTheDocument();
+		expect( screen.getByDisplayValue( 'Description' ) ).toBeInTheDocument();
+	} );
+
+	it( 'should render UPE title field when UPE is enabled', () => {
+		useTitle.mockReturnValue( [ 'Title', jest.fn() ] );
+		useUpeTitle.mockReturnValue( [ 'UPE title', jest.fn() ] );
+		useDescription.mockReturnValue( [ 'Description', jest.fn() ] );
+
+		render(
+			<UpeToggleContext.Provider value={ { isUpeEnabled: true } }>
+				<GeneralSettingsSection />
+			</UpeToggleContext.Provider>
+		);
+
+		expect( screen.getByDisplayValue( 'UPE title' ) ).toBeInTheDocument();
 	} );
 } );
