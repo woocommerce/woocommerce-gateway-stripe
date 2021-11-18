7c701b9beb780339576ff79b2519b4226f1860c3
Diego Curbelo <diego@curbelo.com>
2021-11-18 11:47:26 -0300

    Remove the Account Keys modal auto-display when enablign disabling test mode

--- client/settings/payment-settings/test-mode-checkbox.js
+++ client/settings/payment-settings/test-mode-checkbox.js
@@ -1,50 +1,18 @@
 import { __ } from '@wordpress/i18n';
-import { React, useState } from 'react';
+import { React } from 'react';
 import { CheckboxControl } from '@wordpress/components';
 import interpolateComponents from 'interpolate-components';
-import { AccountKeysModal } from './account-keys-modal';
 import { useTestMode } from 'wcstripe/data';
-import { useAccountKeys } from 'wcstripe/data/account-keys';
 
 const TestModeCheckbox = () => {
-	const [ modalType, setModalType ] = useState( '' );
 	const [ isTestModeEnabled, setTestMode ] = useTestMode();
-	const { accountKeys } = useAccountKeys();
-
-	const missingLiveKeys =
-		! accountKeys.publishable_key || ! accountKeys.secret_key;
-	const missingTestKeys =
-		! accountKeys.test_publishable_key || ! accountKeys.test_secret_key;
 
 	const handleCheckboxChange = ( isChecked ) => {
-		// are we enabling test mode without the necessary keys?
-		if ( isChecked && missingTestKeys ) {
-			setModalType( 'test' );
-			return;
-		}
-
-		// are we enabling live mode without the necessary keys?
-		if ( ! isChecked && missingLiveKeys ) {
-			setModalType( 'live' );
-			return;
-		}
-
-		// all keys are present, GTG
 		setTestMode( isChecked );
-	};
-
-	const handleModalDismiss = () => {
-		setModalType( '' );
 	};
 
 	return (
 		<>
-			{ modalType && (
-				<AccountKeysModal
-					type={ modalType }
-					onClose={ handleModalDismiss }
-				/>
-			) }
 			<CheckboxControl
 				checked={ isTestModeEnabled }
 				onChange={ handleCheckboxChange }
