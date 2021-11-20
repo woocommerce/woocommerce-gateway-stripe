57ca5254131a705747596e84ee0d58381c7cda99
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2021-11-20 12:09:52 -0300

    fixed problem that would not allow to paste a CNPJ on boleto #2066

--- assets/js/stripe.js
+++ assets/js/stripe.js
@@ -268,7 +268,7 @@ jQuery( function( $ ) {
 			//Mask CPF/CNPJ field when using Boleto
 			$( document ).on( 'change', '.woocommerce-checkout-payment', function () {
 				var TaxIdMaskBehavior = function ( val ) {
-						return val.replace( /\D/g, '' ).length >= 12 ? '00.000.000/0000-00' : '000.000.000-009';
+						return val.replace( /\D/g, '' ).length >= 12 ? '00.000.000/0000-00' : '000.000.000-009999';
 					},
 					spOptions = {
 						onKeyPress: function ( val, e, field, options ) {
