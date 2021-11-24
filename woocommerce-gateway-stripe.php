5345863848772aa7c893cbb0a1bf25fa179432fd
Diego Curbelo <diego@curbelo.com>
2021-11-24 10:17:51 -0300

    Revert "Bump latest supported version for woocommerce (#2177)"
    This reverts commit fefb0fa0a36315be02f95a39f7148959964d0973.

--- woocommerce-gateway-stripe.php
+++ woocommerce-gateway-stripe.php
@@ -8,8 +8,8 @@
  * Version: 5.8.0
  * Requires at least: 5.6
  * Tested up to: 5.8
- * WC requires at least: 5.7
- * WC tested up to: 5.9
+ * WC requires at least: 5.6
+ * WC tested up to: 5.8
  * Text Domain: woocommerce-gateway-stripe
  * Domain Path: /languages
  */
@@ -23,8 +23,8 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 define( 'WC_STRIPE_VERSION', '5.8.0' ); // WRCS: DEFINED_VERSION.
 define( 'WC_STRIPE_MIN_PHP_VER', '7.0.0' );
-define( 'WC_STRIPE_MIN_WC_VER', '5.7' );
-define( 'WC_STRIPE_FUTURE_MIN_WC_VER', '5.8' );
+define( 'WC_STRIPE_MIN_WC_VER', '5.6' );
+define( 'WC_STRIPE_FUTURE_MIN_WC_VER', '5.7' );
 define( 'WC_STRIPE_MAIN_FILE', __FILE__ );
 define( 'WC_STRIPE_ABSPATH', __DIR__ . '/' );
 define( 'WC_STRIPE_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
