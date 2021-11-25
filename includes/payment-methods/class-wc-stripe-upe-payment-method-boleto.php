8a935693f48e385948e4313532b1a5f7ba00ae89
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2021-11-25 16:14:06 -0300

    fixed bug that would not allow orders using boleto/oxxo to go to processing on upe checkout and bug that would not show boleto as the payment method on edit order when using upe #2066

--- includes/payment-methods/class-wc-stripe-upe-payment-method-boleto.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-method-boleto.php
@@ -30,5 +30,23 @@ class WC_Stripe_UPE_Payment_Method_Boleto extends WC_Stripe_UPE_Payment_Method {
 			'Boleto is an official payment method in Brazil. Customers receive a voucher that can be paid at authorized agencies or banks, ATMs, or online bank portals.',
 			'woocommerce-gateway-stripe'
 		);
+
+		add_filter( 'wc_stripe_allowed_payment_processing_statuses', [ $this, 'add_allowed_payment_processing_statuses' ], 10, 2 );
+	}
+
+	/**
+	 * Adds on-hold as accepted status during webhook handling on orders paid with Boleto
+	 *
+	 * @param $allowed_statuses
+	 * @param $order
+	 *
+	 * @return mixed
+	 */
+	public function add_allowed_payment_processing_statuses( $allowed_statuses, $order ) {
+		if ( 'boleto' === $order->get_meta( '_stripe_upe_payment_type' ) && ! in_array( 'on-hold', $allowed_statuses ) ) {
+			$allowed_statuses[] = 'on-hold';
+		}
+
+		return $allowed_statuses;
 	}
 }
