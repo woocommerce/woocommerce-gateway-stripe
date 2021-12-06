f11e8e020c9af95fa4ce6238e4e34fc2ef26221f
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2021-12-06 18:28:30 -0300

    Add selectors to help with account capabilities

--- client/data/account/actions.js
+++ client/data/account/actions.js
@@ -1,7 +1,7 @@
-import { dispatch } from '@wordpress/data';
+import { dispatch, select } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { apiFetch } from '@wordpress/data-controls';
-import { NAMESPACE } from '../constants';
+import { NAMESPACE, STORE_NAME } from '../constants';
 import ACTION_TYPES from './action-types';
 
 export function updateAccount( payload ) {
@@ -22,12 +22,43 @@ export function* refreshAccount() {
 	try {
 		yield updateIsRefreshingAccount( true );
 
+		const activeCapabilitiesBeforeRefresh = select(
+			STORE_NAME
+		).getAccountCapabilitiesByStatus( 'active' );
+
+		// console.log( activeCapabilitiesBeforeRefresh );
+
 		const data = yield apiFetch( {
 			method: 'post',
 			path: `${ NAMESPACE }/account/refresh`,
 		} );
 
 		yield updateAccount( data );
+
+		const activeCapabilitiesAfterRefresh = select(
+			STORE_NAME
+		).getAccountCapabilitiesByStatus( 'inactive' );
+
+		// console.log( activeCapabilitiesAfterRefresh );
+
+		const newPaymentMethods = activeCapabilitiesAfterRefresh.filter(
+			( paymentMethod ) =>
+				! activeCapabilitiesBeforeRefresh.includes( paymentMethod )
+		);
+
+		// console.log( newPaymentMethods );
+
+		if ( newPaymentMethods.length ) {
+			yield dispatch( 'core/notices' ).createSuccessNotice(
+				__(
+					'You can now accept payments with X, Y, Z.',
+					'woocommerce-gateway-stripe'
+				),
+				{
+					icon: '🚀',
+				}
+			);
+		}
 	} catch ( e ) {
 		yield dispatch( 'core/notices' ).createErrorNotice(
 			__( 'Error updating account data.', 'woocommerce-gateway-stripe' )
