c8805b1b718a18c3c8c9a58b88fbe7d2dfcc2153
César Costa <costa.cesar.a@hotmail.com>
2021-12-07 15:28:22 -0300

    Remove UPE onboarding wizard entrypoint

--- client/index.js
+++ client/index.js
@@ -1,20 +1,7 @@
-import { __ } from '@wordpress/i18n';
 import { addFilter } from '@wordpress/hooks';
-import OnboardingWizard from './upe-onboarding-wizard/onboarding-wizard.js';
 
 addFilter(
 	'woocommerce_admin_pages_list',
 	'woocommerce-gateway-stripe',
-	( pages ) => {
-		pages.push( {
-			container: OnboardingWizard,
-			path: '/onboarding',
-			breadcrumbs: [ __( 'Onboarding', 'woocommerce-gateway-stripe' ) ],
-			navArgs: {
-				id: 'wc-stripe-onboarding',
-			},
-		} );
-
-		return pages;
-	}
+	( pages ) => pages
 );
