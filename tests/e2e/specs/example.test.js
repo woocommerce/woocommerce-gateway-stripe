b1d7f67ca304de6eabee080b97d430cec0ca8a08
Danae Millan <danae.millan@automattic.com>
2021-12-08 13:23:41 -0300

    Set up the base WC e2e boilerplate
    At this point, the docker environment is the default one, without specific plugins or options set up. The tests don't run because puppeteer isn't set.

---
+++ tests/e2e/specs/example.test.js
@@ -0,0 +1,21 @@
+/**
+ * External dependencies
+ */
+import config from 'config';
+
+/**
+ * Internal dependencies
+ */
+import { StoreOwnerFlow } from '@woocommerce/e2e-utils';
+
+const TIMEOUT = 20000;
+
+describe( 'Store Owner', () => {
+	it(
+		'Store owner can log in',
+		async () => {
+			await StoreOwnerFlow.login();
+		},
+		TIMEOUT
+	);
+} );
