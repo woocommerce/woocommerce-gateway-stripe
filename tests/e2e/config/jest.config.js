d22ebd49956dd831867200a8e76632f3facca2bb
Danae Millan <danae.millan@automattic.com>
2021-12-09 14:31:56 -0300

    Create separate specs for merchant and shopper e2e tests flows
    This will allow us to better organize the tests.

--- tests/e2e/config/jest.config.js
+++ tests/e2e/config/jest.config.js
@@ -2,7 +2,10 @@ const path = require( 'path' );
 const { useE2EJestConfig } = require( '@woocommerce/e2e-environment' );
 
 const jestConfig = useE2EJestConfig( {
-	roots: [ path.resolve( __dirname, '../specs' ) ],
+	roots: [
+		path.resolve( __dirname, '../specs/merchant' ),
+		path.resolve( __dirname, '../specs/shopper' ),
+	],
 } );
 
 module.exports = jestConfig;
