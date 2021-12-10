d1a1d1a479e4e65625fa01a51371373d2396fc4c
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2021-12-10 17:32:58 -0300

    fixed name of utils and added assertion to UPE flow

--- tests/e2e/utils/settings.js
+++ tests/e2e/utils/settings.js
@@ -1,12 +1,12 @@
 import config from 'config';
-import { buttons } from './buttons';
+import { buttonsUtils } from './buttons';
 
 const baseUrl = config.get( 'url' );
 
 const UPE_SETTINGS_PAGE =
 	baseUrl + 'wp-admin/admin.php?page=wc-settings&tab=checkout&section=stripe';
 
-export const stripeSettings = {
+export const stripeSettingsUtils = {
 	/**
 	 * Opens Upe settings page
 	 */
@@ -20,25 +20,30 @@ export const stripeSettings = {
 	 * Activates Upe using the new settings page
 	 */
 	activateUpe: async () => {
-		await stripeSettings.openUpeSettingsPage();
-		await buttons.clickButtonWithText( 'Enable in your store' );
+		await stripeSettingsUtils.openUpeSettingsPage();
+		await buttonsUtils.clickButtonWithText( 'Enable in your store' );
 		await page.waitForSelector( '#wc-stripe-onboarding-wizard-container' );
 
-		await buttons.clickButtonWithText( 'Enable' );
+		await buttonsUtils.clickButtonWithText( 'Enable' );
 		await page.waitForSelector(
 			'.add-payment-methods-task__payment-selector-title',
 			{ visible: true }
 		);
 
-		await buttons.clickButtonWithText( 'Add payment methods' );
+		await buttonsUtils.clickButtonWithText( 'Add payment methods' );
 		await page.waitForSelector(
 			'.setup-complete-task__enabled-methods-list',
 			{ visible: true }
 		);
 
-		await buttons.clickButtonWithText( 'Go to Stripe settings' );
+		await buttonsUtils.clickButtonWithText( 'Go to Stripe settings' );
 		await page.waitForSelector( '#wc-stripe-account-settings-container', {
 			visible: true,
 		} );
+
+		await expect( page ).not.toMatch(
+			'Enable the new Stripe checkout experience'
+		);
+		await expect( page ).toMatch( 'giropay' );
 	},
 };
