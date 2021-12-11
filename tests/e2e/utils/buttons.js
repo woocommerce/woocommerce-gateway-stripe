6630b6e58a136066774350f274ee93dadde33950
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2021-12-10 23:24:08 -0300

    now the clickButtonWithText can click on buttons with a matching aria-label

--- tests/e2e/utils/buttons.js
+++ tests/e2e/utils/buttons.js
@@ -1,10 +1,15 @@
 export const buttonsUtils = {
 	clickButtonWithText: async ( text ) => {
-		await page.waitForXPath( '//*[text() = "' + text + '"]', {
-			visible: true,
-		} );
+		await page.waitForXPath(
+			'//*[text() = "' + text + '"]|//*[@aria-label="' + text + '"]',
+			{
+				visible: true,
+			}
+		);
 
-		const [ button ] = await page.$x( '//*[text() = "' + text + '"]' );
+		const [ button ] = await page.$x(
+			'//*[text() = "' + text + '"]|//*[@aria-label="' + text + '"]'
+		);
 
 		if ( ! button ) {
 			throw new Error( 'Button not found' );
