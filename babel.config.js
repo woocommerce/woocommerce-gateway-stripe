b1f102794d164d678564eefc888b4bccfaf89e29
Diego Curbelo <diego@curbelo.com>
2021-12-21 14:38:35 -0300

    Add README to languages folder

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,32 @@
 // :exploding_head: same case here: https://github.com/facebook/jest/issues/9292#issuecomment-625750534
 module.exports = {
 	ignore: [],
-	presets: [ '@wordpress/babel-preset-default' ],
+	presets: [
+		[
+			'@wordpress/babel-preset-default',
+			{
+				cacheDirectory: false,
+				cacheCompression: false,
+			},
+		],
+	],
 	plugins: [
 		'@emotion',
 		[ '@babel/transform-runtime', { corejs: 3 } ],
 		'@babel/plugin-proposal-optional-chaining',
 		'@babel/plugin-proposal-nullish-coalescing-operator',
 	],
+	env: {
+		production: {
+			plugins: [
+				[
+					'@wordpress/babel-plugin-makepot',
+					{
+						output:
+							'languages/woocommerce-gateway-stripe-client.pot',
+					},
+				],
+			],
+		},
+	},
 };
