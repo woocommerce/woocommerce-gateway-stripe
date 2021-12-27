662ab24db2b6a275e0bfd856785d9647c7b9b18e
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2021-12-27 11:46:30 -0300

    added functions to save credit card via my account

---
+++ tests/e2e/utils/shopper/account.js
@@ -0,0 +1,47 @@
+import config from 'config';
+
+import {
+	checkUseNewPaymentMethod,
+	confirmCardAuthentication,
+	fillUpeCard,
+} from '../payments';
+
+const baseUrl = config.get( 'url' );
+
+const MY_ACCOUNT_ADD_PAYMENT_METHODS =
+	baseUrl + 'my-account/add-payment-method';
+
+export async function goToPaymentMethodsPage() {
+	await page.goto( MY_ACCOUNT_ADD_PAYMENT_METHODS, {
+		waitUntil: 'networkidle0',
+	} );
+}
+
+/**
+ * Happy path for adding a new payment method in 'My Account > Payment methods' page.
+ * It can handle 3DS and 3DS2 flows.
+ *
+ * @param {*} cardType Card type as defined in the `test.json` file. Examples: `basic`, `3ds2`, `declined`.
+ * @param {*} card Card object that you want to add as the new payment method.
+ */
+export async function addNewPaymentMethod( cardType, card ) {
+	await goToPaymentMethodsPage();
+	await checkUseNewPaymentMethod();
+	await fillUpeCard( card );
+
+	await expect( page ).toClick( 'button', {
+		text: 'Add payment method',
+	} );
+
+	const cardIs3DS =
+		cardType.toUpperCase().includes( '3DS' ) &&
+		! cardType.toLowerCase().includes( 'declined' );
+
+	if ( cardIs3DS ) {
+		await confirmCardAuthentication( cardType );
+	}
+
+	await page.waitForNavigation( {
+		waitUntil: 'networkidle0',
+	} );
+}
