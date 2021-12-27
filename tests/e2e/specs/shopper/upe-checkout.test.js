d218aebd6e53a838ae7e7a9817612ac3545028f4
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2021-12-27 18:21:34 -0300

    started to create a test to remove saved cards

--- tests/e2e/specs/shopper/upe-checkout.test.js
+++ tests/e2e/specs/shopper/upe-checkout.test.js
@@ -12,7 +12,10 @@ import {
 } from '../../utils/upe-settings';
 import { confirmCardAuthentication } from '../../utils/payments';
 import { merchant } from '@woocommerce/e2e-utils';
-import { addNewPaymentMethod } from '../../utils/shopper/account';
+import {
+	addNewPaymentMethod,
+	removedPaymentMethods,
+} from '../../utils/shopper/account';
 
 describe( 'Successfull Purchase', () => {
 	beforeAll( async () => {
@@ -22,48 +25,48 @@ describe( 'Successfull Purchase', () => {
 	} );
 
 	afterAll( async () => {
-		await merchant.logout();
+		// await merchant.logout();
 	} );
 
-	it( 'using a basic card', async () => {
-		await activatePaymentMethod( 'card' );
-		await setupProductCheckout(
-			config.get( 'addresses.customer.billing' )
-		);
-		await checkUseNewPaymentMethod();
-		const card = config.get( 'cards.basic' );
-		await fillUpeCard( card );
-
-		await expect( page ).toClick( '#place_order' );
-		await page.waitForNavigation( {
-			waitUntil: 'networkidle0',
-		} );
-		await expect( page ).toMatch( 'Order received' );
-	} );
-
-	it( 'using a SCA card', async () => {
-		await activatePaymentMethod( 'card' );
-		await setupProductCheckout(
-			config.get( 'addresses.customer.billing' )
-		);
-		const card = config.get( 'cards.sca' );
-
-		await checkUseNewPaymentMethod();
-
-		await fillUpeCard( card );
-		await expect( page ).toClick( '#place_order' );
-
-		await confirmCardAuthentication();
-
-		await page.waitForNavigation( {
-			waitUntil: 'networkidle0',
-		} );
-		await expect( page ).toMatch( 'Order received' );
-	} );
+	// it( 'using a basic card', async () => {
+	// 	await activatePaymentMethod( 'card' );
+	// 	await setupProductCheckout( config.get( 'addresses.customer.billing' ) );
+	// 	await checkUseNewPaymentMethod();
+	// 	await fillUpeCard( config.get( 'cards.basic' ) );
+	//
+	// 	await expect( page ).toClick( '#place_order' );
+	// 	await page.waitForNavigation( {
+	// 		waitUntil: 'networkidle0',
+	// 	} );
+	//
+	// 	await expect( page ).toMatch( 'Order received' );
+	// } );
+	//
+	// it( 'using a SCA card', async () => {
+	// 	await activatePaymentMethod( 'card' );
+	// 	await setupProductCheckout( config.get( 'addresses.customer.billing' ) );
+	// 	await checkUseNewPaymentMethod();
+	// 	await fillUpeCard( config.get( 'cards.sca' ) );
+	//
+	// 	await expect( page ).toClick( '#place_order' );
+	//
+	// 	await confirmCardAuthentication();
+	// 	await page.waitForNavigation( {
+	// 		waitUntil: 'networkidle0',
+	// 	} );
+	//
+	// 	await expect( page ).toMatch( 'Order received' );
+	// } );
 
 	it( 'save card', async () => {
-		const card = config.get( 'cards.basic' );
-		await addNewPaymentMethod( 'basic', card );
+		await addNewPaymentMethod( 'basic', config.get( 'cards.basic' ) );
+
 		await expect( page ).toMatch( 'Payment method successfully added' );
+	} );
+
+	it( 'use saved card', async () => {
+		await removedPaymentMethods();
+
+		await expect( page ).toMatch( 'Payment method deleted.' );
 	} );
 } );
