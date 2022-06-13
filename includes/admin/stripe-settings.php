1e356e532d6720cb1c98c965f8cc806c198dba4b
Hector Lovo <hector.lovo@automattic.com>
2022-06-13 14:32:10 -0400

    Update 'title' & 'title_upe' fields' type

--- includes/admin/stripe-settings.php
+++ includes/admin/stripe-settings.php
@@ -15,14 +15,14 @@ $stripe_settings = apply_filters(
 		],
 		'title'                               => [
 			'title'       => __( 'Title', 'woocommerce-gateway-stripe' ),
-			'type'        => 'text',
+			'type'        => 'safe_text',
 			'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce-gateway-stripe' ),
 			'default'     => __( 'Credit Card (Stripe)', 'woocommerce-gateway-stripe' ),
 			'desc_tip'    => true,
 		],
 		'title_upe'                           => [
 			'title'       => __( 'Title', 'woocommerce-gateway-stripe' ),
-			'type'        => 'text',
+			'type'        => 'safe_text',
 			'description' => __( 'This controls the title which the user sees during checkout when multiple payment methods are enabled.', 'woocommerce-gateway-stripe' ),
 			'default'     => __( 'Popular payment methods', 'woocommerce-gateway-stripe' ),
 			'desc_tip'    => true,
