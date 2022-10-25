ef1da38b7c430b5dfb728daf23ec09a9fb23441a
Diego Curbelo <diego@curbelo.com>
2022-10-25 02:05:40 -0300

    Update tests bootstrap with latest version

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -5,6 +5,8 @@
  * @package WooCommerce\Stripe
  */
 
+require_once __DIR__ . '/../../vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
