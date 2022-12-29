84fe49cefc8bf2e59d9e04b76e4948de363e5e80
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2022-12-29 14:01:43 -0300

    Add initial code for failure cards

--- tests/e2e/tests/checkout/card-failures.spec.js
+++ tests/e2e/tests/checkout/card-failures.spec.js
@@ -6,3 +6,32 @@ const {
 	setupCheckout,
 	fillCardDetails,
 } = require( '../../utils/payments' );
+
+test.beforeEach( async ( { page } ) => {
+	await emptyCart( page );
+	await setupProductCheckout( page );
+	await setupCheckout( page, config.get( 'addresses.customer.billing' ) );
+} );
+
+// Checkout failures (with various cards)
+test( 'customer cannot checkout with invalid cards @smoke', async ( {
+	page,
+} ) => {
+	const cards = [
+		config.get( 'cards.declined' ),
+		config.get( 'cards.declined-funds' ),
+		config.get( 'cards.declined-incorrect' ),
+		config.get( 'cards.declined-expired' ),
+		config.get( 'cards.declined-cvc' ),
+		config.get( 'cards.declined-processing' ),
+	];
+
+	for ( const card of cards ) {
+		await fillCardDetails( page, card );
+		await page.locator( 'text=Place order' ).click();
+
+		expect
+			.soft( await page.innerText( '.woocommerce-error' ) )
+			.toBe( card.error );
+	}
+} );
