bfb195611dc3a3a50e23bc2e0f52a799700b440c
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2023-01-02 10:15:27 -0300

    Add card failures test case

--- tests/e2e/tests/checkout/card-failures.spec.js
+++ tests/e2e/tests/checkout/card-failures.spec.js
@@ -13,26 +13,35 @@ test.beforeEach( async ( { page } ) => {
 	await setupCheckout( page, config.get( 'addresses.customer.billing' ) );
 } );
 
-// Checkout failures (with various cards)
-// @TODO group tests using describe and create a new test inside the loop. Give a name to the tests in the cards array.
-test( 'customer cannot checkout with invalid cards @smoke', async ( {
-	page,
-} ) => {
-	const cards = [
-		config.get( 'cards.declined' ),
-		config.get( 'cards.declined-funds' ),
-		config.get( 'cards.declined-incorrect' ),
-		config.get( 'cards.declined-expired' ),
-		config.get( 'cards.declined-cvc' ),
-		config.get( 'cards.declined-processing' ),
-	];
+const testFunctionForCard = async ( page, cardKey ) => {
+	const card = config.get( cardKey );
 
-	for ( const card of cards ) {
-		await fillCardDetails( page, card );
-		await page.locator( 'text=Place order' ).click();
+	await fillCardDetails( page, card );
+	await page.locator( 'text=Place order' ).click();
 
-		expect
-			.soft( await page.innerText( '.woocommerce-error' ) )
-			.toBe( card.error );
-	}
+	expect
+		.soft( await page.innerText( '.woocommerce-error' ) )
+		.toBe( card.error );
+};
+
+test.describe.configure( { mode: 'parallel' } );
+test.describe( 'customer cannot checkout with invalid cards', () => {
+	test( `a declined card shows the correct error message @smoke`, async ( {
+		page,
+	} ) => testFunctionForCard( page, 'cards.declined' ) );
+	test( `a card with insufficient funds shows the correct error message`, async ( {
+		page,
+	} ) => testFunctionForCard( page, 'cards.declined-funds' ) );
+	test( `a card with invalid number shows the correct error message`, async ( {
+		page,
+	} ) => testFunctionForCard( page, 'cards.declined-incorrect' ) );
+	test( `an expired card shows the correct error message`, async ( {
+		page,
+	} ) => testFunctionForCard( page, 'cards.declined-expired' ) );
+	test( `a card with incorrect CVC shows the correct error message @smoke`, async ( {
+		page,
+	} ) => testFunctionForCard( page, 'cards.declined-cvc' ) );
+	test( `an error processing the card shows the correct error message`, async ( {
+		page,
+	} ) => testFunctionForCard( page, 'cards.declined-processing' ) );
 } );
