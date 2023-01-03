8dcbe3bbb4ed4f2640d50cd41ff5baaa0d43b1fc
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2023-01-03 12:45:57 -0300

    Add SCA card test for regular checkout

--- tests/e2e/tests/checkout/sca-card.spec.js
+++ tests/e2e/tests/checkout/sca-card.spec.js
@@ -6,3 +6,34 @@ const {
 	setupCheckout,
 	fillCardDetails,
 } = require( '../../utils/payments' );
+
+test( 'customer can checkout with a SCA card @smoke', async ( { page } ) => {
+	await emptyCart( page );
+
+	await setupProductCheckout( page );
+	await setupCheckout( page, config.get( 'addresses.customer.billing' ) );
+	await fillCardDetails( page, config.get( 'cards.3ds' ) );
+	await page.locator( 'text=Place order' ).click();
+
+	// Wait until the SCA frame is available
+	while (
+		! page.frame( {
+			name: 'acsFrame',
+		} )
+	) {
+		await page.waitForTimeout( 1000 );
+	}
+
+	await page
+		.frame( {
+			name: 'acsFrame',
+		} )
+		.getByRole( 'button', { name: 'Complete authentication' } )
+		.click();
+
+	await page.waitForNavigation();
+
+	await expect( page.locator( 'h1.entry-title' ) ).toHaveText(
+		'Order received'
+	);
+} );
