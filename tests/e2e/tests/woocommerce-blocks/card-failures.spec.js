a5ecb463abace755f4203e2a37cfd2b67499eaa1
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2023-01-11 10:02:37 -0300

    Add card-failures spec

---
+++ tests/e2e/tests/woocommerce-blocks/card-failures.spec.js
@@ -0,0 +1,56 @@
+import { test, expect } from '@playwright/test';
+import config from 'config';
+import { payments } from '../../utils';
+
+const {
+	emptyCart,
+	setupProductCheckout,
+	setupBlocksCheckout,
+	fillCardDetails,
+} = payments;
+
+test.beforeEach( async ( { page } ) => {
+	await emptyCart( page );
+	await setupProductCheckout( page );
+	await setupBlocksCheckout(
+		page,
+		config.get( 'addresses.customer.billing' )
+	);
+} );
+
+const testCard = async ( page, cardKey ) => {
+	const card = config.get( cardKey );
+
+	await fillCardDetails( page, card );
+	await page.locator( 'text=Place order' ).click();
+
+	expect
+		.soft(
+			await page.innerText(
+				'.wc-block-checkout__payment-method .woocommerce-error'
+			)
+		)
+		.toBe( card.error );
+};
+
+test.describe.configure( { mode: 'parallel' } );
+test.describe( 'customer cannot checkout with invalid cards @blocks', () => {
+	test( `a declined card shows the correct error message @smoke`, async ( {
+		page,
+	} ) => testCard( page, 'cards.declined' ) );
+	test( `a card with insufficient funds shows the correct error message`, async ( {
+		page,
+	} ) => testCard( page, 'cards.declined-funds' ) );
+	test( `a card with invalid number shows the correct error message`, async ( {
+		page,
+	} ) => testCard( page, 'cards.declined-incorrect' ) );
+	test( `an expired card shows the correct error message`, async ( {
+		page,
+	} ) => testCard( page, 'cards.declined-expired' ) );
+	test( `a card with incorrect CVC shows the correct error message @smoke`, async ( {
+		page,
+	} ) => testCard( page, 'cards.declined-cvc' ) );
+	test( `an error processing the card shows the correct error message`, async ( {
+		page,
+	} ) => testCard( page, 'cards.declined-processing' ) );
+} );
