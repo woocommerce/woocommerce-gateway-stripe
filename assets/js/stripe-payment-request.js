76925eea59b6869f33af2e7c31725dd0456a5e1a
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2023-03-24 12:10:21 -0300

    Update trigger for add-onds PRB update

--- assets/js/stripe-payment-request.js
+++ assets/js/stripe-payment-request.js
@@ -681,11 +681,11 @@ jQuery( function( $ ) {
 			// Block the payment request button as soon as an "input" event is fired, to avoid sync issues
 			// when the customer clicks on the button before the debounced event is processed.
 			$( '.quantity' ).on( 'input', '.qty', blockPaymentRequestButton );
-			$('.quantity').on('input', '.qty', wc_stripe_payment_request.debounce(250, cartChangedHandler));
+			$( '.quantity' ).on('input', '.qty', wc_stripe_payment_request.debounce(250, cartChangedHandler));
 			
 			// Update payment request buttons if product add-ons are modified.
-			$( '.cart:not(.cart_group)' ).on( 'woocommerce-product-addons-update', blockPaymentRequestButton );
-			$( '.cart:not(.cart_group)' ).on( 'woocommerce-product-addons-update', wc_stripe_payment_request.debounce( 250, cartChangedHandler ));
+			$( '.cart:not(.cart_group)' ).on( 'updated_addons', blockPaymentRequestButton );
+			$( '.cart:not(.cart_group)' ).on( 'updated_addons', wc_stripe_payment_request.debounce( 250, cartChangedHandler ));
 
 			if ( $('.variations_form').length ) {
 				$( '.variations_form' ).on( 'found_variation.wc-variation-form', function ( evt, variation ) {
