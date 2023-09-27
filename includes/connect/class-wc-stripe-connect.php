86a20f4ab3cc064af651fbf48ffe4236c5f13ac1
Danae Millan <danae.millan@automattic.com>
2023-09-27 19:54:41 -0300

    Rename method and track events

--- includes/connect/class-wc-stripe-connect.php
+++ includes/connect/class-wc-stripe-connect.php
@@ -92,7 +92,7 @@ if ( ! class_exists( 'WC_Stripe_Connect' ) ) {
 
 				$response = $this->connect_oauth( wc_clean( wp_unslash( $_GET['wcs_stripe_state'] ) ), wc_clean( wp_unslash( $_GET['wcs_stripe_code'] ) ) );
 
-				$this->maybe_record_oboarding_track_event( is_wp_error( $response ) );
+				$this->record_account_connect_track_event( is_wp_error( $response ) );
 
 				wp_safe_redirect( esc_url_raw( remove_query_arg( [ 'wcs_stripe_state', 'wcs_stripe_code' ] ) ) );
 				exit;
@@ -182,22 +182,21 @@ if ( ! class_exists( 'WC_Stripe_Connect' ) ) {
 		}
 
 		/**
-		 * Records the onboarding flow track event if WC_Tracks exists.
+		 * Records a track event after the user is redirected back to the store from the Stripe UX.
 		 *
 		 * @param bool $had_error Whether the Stripe connection had an error.
-		 * @return void
 		 */
-		private function maybe_record_oboarding_track_event( $had_error ) {
+		private function record_account_connect_track_event( bool $had_error ) {
 			if ( ! class_exists( 'WC_Tracks' ) ) {
 				return;
 			}
 
 			$options    = get_option( self::SETTINGS_OPTION, [] );
 			$is_test    = isset( $options['testmode'] ) && 'yes' === $options['testmode'];
-			$event_name = ! $had_error ? 'wcstripe_onboarding_flow_redirected' : 'wcstripe_onboarding_flow_redirected_error';
+			$event_name = ! $had_error ? 'wcstripe_stripe_connected' : 'wcstripe_stripe_connect_error';
 
 			// We're recording this directly instead of queueing it because
-			// a queue wouldn't be processed due to the redirect below.
+			// a queue wouldn't be processed due to the redirect that comes after.
 			WC_Tracks::record_event( $event_name, [ 'is_test_mode' => $is_test ] );
 		}
 	}
