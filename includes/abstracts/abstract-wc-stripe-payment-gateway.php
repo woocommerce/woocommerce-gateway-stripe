5172ef709b7e1b00166c301f816b70443c50869c
Matt Allan <matttallan@gmail.com>
2023-11-02 14:45:08 +1000

    Add/update customer address details when processing payments and include shipping details on each payment_intent request  (#2731)
    * Always create and update customers in Stripe with the latest billing/shipping details
    
    * Ensure shipping details are always present in payment_intent requests
    
    * Update generate_customer_request() to populate customer address details by default
    
    * Because generate_customer_request() includes an address we no longer need use map_customer_data()
    
    * Clean up code checking for customer ID to be consistent
    
    * Introduce a mockable function that gets an instance of the stripe customer
    
    * Fix unit tests
    
    * Fix woocommerce_stripe_add_payment_method hook so that it attaches a user ID
    
    * Fix unit tests for functions using WC_Stripe_Customer::clear_cache
    
    * Add changelog
    
    * Adjust changelog entry to fix typos
    
    * Fix incorrect changelog entry in previous PR
    
    ---------
    
    Co-authored-by: Diego Curbelo <diego@curbelo.com>

--- includes/abstracts/abstract-wc-stripe-payment-gateway.php
+++ includes/abstracts/abstract-wc-stripe-payment-gateway.php
@@ -870,10 +870,8 @@ abstract class WC_Stripe_Payment_Gateway extends WC_Payment_Gateway_CC {
 			}
 		}
 
-		$customer_id = $customer->get_id();
-		if ( ! $customer_id ) {
-			$customer->set_id( $customer->create_customer() );
-			$customer_id = $customer->get_id();
+		if ( ! $customer->get_id() ) {
+			$customer_id = $customer->create_customer();
 		} else {
 			$customer_id = $customer->update_customer();
 		}
