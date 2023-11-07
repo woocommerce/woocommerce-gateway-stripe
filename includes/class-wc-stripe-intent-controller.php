woocommerce/woocommerce-gateway-stripe

commit 5172ef709b7e1b00166c301f816b70443c50869c

Add/update customer address details when processing payments and include shipping details on each payment_intent request  (#2731)
* Always create and update customers in Stripe with the latest billing/shipping details

* Ensure shipping details are always present in payment_intent requests

* Update generate_customer_request() to populate customer address details by default

* Because generate_customer_request() includes an address we no longer need use map_customer_data()

* Clean up code checking for customer ID to be consistent

* Introduce a mockable function that gets an instance of the stripe customer

* Fix unit tests

* Fix woocommerce_stripe_add_payment_method hook so that it attaches a user ID

* Fix unit tests for functions using WC_Stripe_Customer::clear_cache

* Add changelog

* Adjust changelog entry to fix typos

* Fix incorrect changelog entry in previous PR

---------

Co-authored-by: Diego Curbelo <diego@curbelo.com>

includes/class-wc-stripe-intent-controller.php
@@ -502,12 +502,13 @@ class WC_Stripe_Intent_Controller {
 	 */
 	public function init_setup_intent() {
 		// Determine the customer managing the payment methods, create one if we don't have one already.
-		$user        = wp_get_current_user();
-		$customer    = new WC_Stripe_Customer( $user->ID );
-		$customer_id = $customer->get_id();
-		if ( empty( $customer_id ) ) {
-			$customer_data = WC_Stripe_Customer::map_customer_data( null, new WC_Customer( $user->ID ) );
-			$customer_id   = $customer->create_customer( $customer_data );
+		$user     = wp_get_current_user();
+		$customer = new WC_Stripe_Customer( $user->ID );
+
+		if ( ! $customer->get_id() ) {
+			$customer_id = $customer->create_customer();
+		} else {
+			$customer_id = $custoner->update_customer();
 		}
 
 		$gateway              = $this->get_upe_gateway();

includes/abstracts/abstract-wc-stripe-payment-gateway.php
@@ -870,10 +870,8 @@ abstract class WC_Stripe_Payment_Gateway extends WC_Payment_Gateway_CC {
 			}
 		}
 
-		$customer_id = $customer->get_id();
-		if ( ! $customer_id ) {
-			$customer->set_id( $customer->create_customer() );
-			$customer_id = $customer->get_id();
+		if ( ! $customer->get_id() ) {
+			$customer_id = $customer->create_customer();
 		} else {
 			$customer_id = $customer->update_customer();
 		}

includes/class-wc-stripe-customer.php
@@ -122,8 +122,16 @@ class WC_Stripe_Customer {
 	 * @return array
 	 */
 	protected function generate_customer_request( $args = [] ) {
-		$billing_email = isset( $_POST['billing_email'] ) ? filter_var( wp_unslash( $_POST['billing_email'] ), FILTER_SANITIZE_EMAIL ) : '';
-		$user          = $this->get_user();
+		$billing_email  = isset( $_POST['billing_email'] ) ? filter_var( wp_unslash( $_POST['billing_email'] ), FILTER_SANITIZE_EMAIL ) : '';
+		$user           = $this->get_user();
+		$address_fields = [
+			'line1'       => 'billing_address_1',
+			'line2'       => 'billing_address_2',
+			'postal_code' => 'billing_postcode',
+			'city'        => 'billing_city',
+			'state'       => 'billing_state',
+			'country'     => 'billing_country',
+		];
 
 		if ( $user ) {
 			$billing_first_name = get_user_meta( $user->ID, 'billing_first_name', true );
@@ -172,6 +180,15 @@ class WC_Stripe_Customer {
 		$metadata                      = [];
 		$defaults['metadata']          = apply_filters( 'wc_stripe_customer_metadata', $metadata, $user );
 		$defaults['preferred_locales'] = $this->get_customer_preferred_locale( $user );
+
+		// Add customer address default values.
+		foreach ( $address_fields as $key => $field ) {
+			if ( $user ) {
+				$defaults['address'][ $key ] = get_user_meta( $user->ID, $field, true );
+			} else {
+				$defaults['address'][ $key ] = isset( $_POST[ $field ] ) ? filter_var( wp_unslash( $_POST[ $field ] ), FILTER_SANITIZE_STRING ) : ''; // phpcs:ignore WordPress.Security.NonceVerification
+			}
+		}
 
 		return wp_parse_args( $args, $defaults );
 	}

includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
@@ -542,14 +542,13 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 					'description'          => sprintf( __( '%1$s - Order %2$s', 'woocommerce-gateway-stripe' ), wp_specialchars_decode( get_bloginfo( 'name' ), ENT_QUOTES ), $order->get_order_number() ),
 				];
 
-				// Get user/customer for order.
-				$customer_id = $this->get_stripe_customer_id( $order );
-				if ( ! empty( $customer_id ) ) {
-					$request['customer'] = $customer_id;
+				$customer = $this->get_stripe_customer_from_order( $order );
+
+				// Update customer or create customer if customer does not exist.
+				if ( ! $customer->get_id() ) {
+					$request['customer'] = $customer->create_customer();
 				} else {
-					$user                = $this->get_user_from_order( $order );
-					$customer            = new WC_Stripe_Customer( $user->ID );
-					$request['customer'] = $customer->update_or_create_customer();// Update customer or create customer if customer does not exist.
+					$request['customer'] = $customer->update_customer();
 				}
 
 				if ( '' !== $selected_upe_payment_type ) {
@@ -578,6 +577,11 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 				}
 
 				$request['metadata'] = $this->get_metadata_from_order( $order );
+
+				// If order requires shipping, add the shipping address details to the payment intent request.
+				if ( method_exists( $order, 'get_shipping_postcode' ) && ! empty( $order->get_shipping_postcode() ) ) {
+					$request['shipping'] = $this->get_address_data_for_payment_request( $order );
+				}
 
 				// Run the necessary filter to make sure mandate information is added when it's required.
 				$request = apply_filters(
@@ -678,6 +682,11 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 				if ( false === $intent ) {
 					$request['capture_method'] = ( 'true' === $request_details['capture'] ) ? 'automatic' : 'manual';
 					$request['confirm']        = 'true';
+				}
+
+				// If order requires shipping, add the shipping address details to the payment intent request.
+				if ( method_exists( $order, 'get_shipping_postcode' ) && ! empty( $order->get_shipping_postcode() ) ) {
+					$request['shipping'] = $this->get_address_data_for_payment_request( $order );
 				}
 
 				// Run the necessary filter to make sure mandate information is added when it's required.
@@ -1002,13 +1011,13 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 			} else {
 				$payment_method_object = $intent->payment_method;
 			}
-			$user                    = $this->get_user_from_order( $order );
-			$customer                = new WC_Stripe_Customer( $user->ID );
+
+			$customer                = $this->get_stripe_customer_from_order( $order );
 			$prepared_payment_method = $this->prepare_payment_method( $payment_method_object );
 
 			$customer->clear_cache();
 			$this->save_payment_method_to_order( $order, $prepared_payment_method );
-			do_action( 'woocommerce_stripe_add_payment_method', $user->get_id(), $payment_method_object );
+			do_action( 'woocommerce_stripe_add_payment_method', $customer->get_user_id(), $payment_method_object );
 		}
 
 		if ( $payment_needed ) {
@@ -1158,6 +1167,20 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 			$user = wp_get_current_user();
 		}
 		return $user;
+	}
+
+	/**
+	 * Get WC Stripe Customer from WC Order.
+	 *
+	 * @param WC_Order $order
+	 *
+	 * @return WC_Stripe_Customer
+	 */
+	public function get_stripe_customer_from_order( $order ) {
+		$user     = $this->get_user_from_order( $order );
+		$customer = new WC_Stripe_Customer( $user->ID );
+
+		return $customer;
 	}
 
 	/**
@@ -1439,6 +1462,31 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 			return WC_Stripe_API::request_with_level3_data( $params, $path, $level3_data, $order );
 		}
 		return WC_Stripe_API::request( $params, $path, $method );
+	}
+
+	/**
+	 * Returns an array of address datato be used in a Stripe /payment_intents API request.
+	 *
+	 * Stripe docs: https://stripe.com/docs/api/payment_intents/create#create_payment_intent-shipping
+	 *
+	 * @since 7.7.0
+	 *
+	 * @param WC_Order $order Order to fetch address data from.
+	 *
+	 * @return array
+	 */
+	private function get_address_data_for_payment_request( $order ) {
+		return [
+			'name'    => trim( $order->get_shipping_first_name() . ' ' . $order->get_shipping_last_name() ),
+			'address' => [
+				'line1'       => $order->get_shipping_address_1(),
+				'line2'       => $order->get_shipping_address_2(),
+				'city'        => $order->get_shipping_city(),
+				'country'     => $order->get_shipping_country(),
+				'postal_code' => $order->get_shipping_postcode(),
+				'state'       => $order->get_shipping_state(),
+			],
+		];
 	}
 
 }

tests/phpunit/test-class-wc-stripe-upe-payment-gateway.php
@@ -11,6 +11,13 @@ class WC_Stripe_UPE_Payment_Gateway_Test extends WP_UnitTestCase {
 	private $mock_gateway;
 
 	/**
+	 * Mock WC Stripe Customer
+	 *
+	 * @var WC_Stripe_Customer
+	 */
+	private $mock_stripe_customer;
+
+	/**
 	 * Array mapping Stripe IDs to mock WC_Stripe_UPE_Payment_Methods.
 	 *
 	 * @var array
@@ -100,6 +107,7 @@ class WC_Stripe_UPE_Payment_Gateway_Test extends WP_UnitTestCase {
 					'is_pre_order_product_charged_upfront',
 					'prepare_order_source',
 					'stripe_request',
+					'get_stripe_customer_from_order',
 				]
 			)
 			->getMock();
@@ -108,6 +116,27 @@ class WC_Stripe_UPE_Payment_Gateway_Test extends WP_UnitTestCase {
 			->method( 'get_return_url' )
 			->will(
 				$this->returnValue( self::MOCK_RETURN_URL )
+			);
+
+		$this->mock_stripe_customer = $this->getMockBuilder( WC_Stripe_Customer::class )
+			->disableOriginalConstructor()
+			->setMethods(
+				[
+					'create_customer',
+					'update_customer',
+				]
+			)
+			->getMock();
+
+		$this->mock_stripe_customer->expects( $this->any() )
+			->method( 'create_customer' )
+			->will(
+				$this->returnValue( 'cus_mock' )
+			);
+		$this->mock_stripe_customer->expects( $this->any() )
+			->method( 'update_customer' )
+			->will(
+				$this->returnValue( 'cus_mock' )
 			);
 	}
 
@@ -255,11 +284,11 @@ class WC_Stripe_UPE_Payment_Gateway_Test extends WP_UnitTestCase {
 
 		$_POST = [ 'wc_payment_intent_id' => $payment_intent_id ];
 
-		$this->mock_gateway->expects( $this->once() )
-			->method( 'get_stripe_customer_id' )
+		$this->mock_gateway->expects( $this->any() )
+			->method( 'get_stripe_customer_from_order' )
 			->with( wc_get_order( $order_id ) )
 			->will(
-				$this->returnValue( $customer_id )
+				$this->returnValue( $this->mock_stripe_customer )
 			);
 		$this->mock_gateway->expects( $this->once() )
 			->method( 'stripe_request' )
@@ -423,6 +452,12 @@ class WC_Stripe_UPE_Payment_Gateway_Test extends WP_UnitTestCase {
 		$setup_intent_mock['payment_method'] = $payment_method_mock;
 		$setup_intent_mock['latest_charge']  = [];
 
+		$this->mock_gateway->expects( $this->any() )
+			->method( 'get_stripe_customer_from_order' )
+			->with( wc_get_order( $order_id ) )
+			->will(
+				$this->returnValue( $this->mock_stripe_customer )
+			);
 		$this->mock_gateway->expects( $this->once() )
 			->method( 'stripe_request' )
 			->with( "setup_intents/$setup_intent_id?expand[]=payment_method&expand[]=latest_attempt" )
@@ -468,6 +503,12 @@ class WC_Stripe_UPE_Payment_Gateway_Test extends WP_UnitTestCase {
 		$payment_intent_mock['payment_method']     = $payment_method_mock;
 		$payment_intent_mock['charges']['data'][0]['payment_method_details'] = $payment_method_mock;
 
+		$this->mock_gateway->expects( $this->any() )
+			->method( 'get_stripe_customer_from_order' )
+			->with( wc_get_order( $order_id ) )
+			->will(
+				$this->returnValue( $this->mock_stripe_customer )
+			);
 		$this->mock_gateway->expects( $this->once() )
 			->method( 'stripe_request' )
 			->with( "payment_intents/$payment_intent_id?expand[]=payment_method" )
@@ -520,7 +561,12 @@ class WC_Stripe_UPE_Payment_Gateway_Test extends WP_UnitTestCase {
 				'generated_sepa_debit' => $generated_payment_method_id,
 			],
 		];
-
+		$this->mock_gateway->expects( $this->any() )
+			->method( 'get_stripe_customer_from_order' )
+			->with( wc_get_order( $order_id ) )
+			->will(
+				$this->returnValue( $this->mock_stripe_customer )
+			);
 		$this->mock_gateway->expects( $this->exactly( 2 ) )
 			->method( 'stripe_request' )
 			->willReturnOnConsecutiveCalls(
@@ -574,6 +620,12 @@ class WC_Stripe_UPE_Payment_Gateway_Test extends WP_UnitTestCase {
 			],
 		];
 
+		$this->mock_gateway->expects( $this->any() )
+			->method( 'get_stripe_customer_from_order' )
+			->with( wc_get_order( $order_id ) )
+			->will(
+				$this->returnValue( $this->mock_stripe_customer )
+			);
 		$this->mock_gateway->expects( $this->exactly( 2 ) )
 			->method( 'stripe_request' )
 			->willReturnOnConsecutiveCalls(
@@ -1062,11 +1114,11 @@ class WC_Stripe_UPE_Payment_Gateway_Test extends WP_UnitTestCase {
 			->method( 'has_subscription' )
 			->will( $this->returnValue( true ) );
 
-		$this->mock_gateway->expects( $this->once() )
-			->method( 'get_stripe_customer_id' )
+		$this->mock_gateway->expects( $this->any() )
+			->method( 'get_stripe_customer_from_order' )
 			->with( wc_get_order( $order_id ) )
 			->will(
-				$this->returnValue( $customer_id )
+				$this->returnValue( $this->mock_stripe_customer )
 			);
 
 		$this->mock_gateway->expects( $this->once() )
@@ -1108,7 +1160,7 @@ class WC_Stripe_UPE_Payment_Gateway_Test extends WP_UnitTestCase {
 			->will( $this->returnValue( true ) );
 
 		$this->mock_gateway->expects( $this->never() )
-			->method( 'get_stripe_customer_id' );
+			->method( 'get_stripe_customer_from_order' );
 
 		$this->mock_gateway->expects( $this->never() )
 			->method( 'stripe_request' );
@@ -1335,6 +1387,12 @@ class WC_Stripe_UPE_Payment_Gateway_Test extends WP_UnitTestCase {
 					$this->array_to_object( $payment_intent_mock )
 				)
 			);
+		$this->mock_gateway->expects( $this->any() )
+			->method( 'get_stripe_customer_from_order' )
+			->with( wc_get_order( $order_id ) )
+			->will(
+				$this->returnValue( $this->mock_stripe_customer )
+			);
 
 		$this->mock_gateway->expects( $this->once() )
 			->method( 'mark_order_as_pre_ordered' );
@@ -1373,6 +1431,13 @@ class WC_Stripe_UPE_Payment_Gateway_Test extends WP_UnitTestCase {
 		$setup_intent_mock['id']             = $setup_intent_id;
 		$setup_intent_mock['payment_method'] = $payment_method_mock;
 		$setup_intent_mock['latest_charge']  = [];
+
+		$this->mock_gateway->expects( $this->any() )
+			->method( 'get_stripe_customer_from_order' )
+			->with( wc_get_order( $order_id ) )
+			->will(
+				$this->returnValue( $this->mock_stripe_customer )
+			);
 
 		// Mock order has pre-order product.
 		$this->mock_gateway->expects( $this->once() )

commit 0420f6c318fbb85ec3085fb94c216288a712c35e

Fix variable name typo introduced by #2731

includes/class-wc-stripe-intent-controller.php
@@ -508,7 +508,7 @@ class WC_Stripe_Intent_Controller {
 		if ( ! $customer->get_id() ) {
 			$customer_id = $customer->create_customer();
 		} else {
-			$customer_id = $custoner->update_customer();
+			$customer_id = $customer->update_customer();
 		}
 
 		$gateway              = $this->get_upe_gateway();