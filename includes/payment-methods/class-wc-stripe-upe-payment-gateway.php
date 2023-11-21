3c4fb1e7433a87559bf51b48ea4db705761d4810
Danae Millan <41606954+a-danae@users.noreply.github.com>
2023-11-21 09:59:58 +0100

    Use the order currency instead of the store currency when creating an intent
    Co-authored-by: Matt Allan <matttallan@gmail.com>

--- includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
@@ -1588,7 +1588,7 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 		$selected_payment_type        = sanitize_text_field( wp_unslash( $_POST['wc_stripe_selected_upe_payment_type'] ?? '' ) ); // phpcs:ignore WordPress.Security.NonceVerification.Missing
 		$capture_method               = empty( $this->get_option( 'capture' ) ) || $this->get_option( 'capture' ) === 'yes' ? 'automatic' : 'manual'; // automatic | manual.
 		$save_payment_method_to_store = isset( $_POST['save_payment_method'] ) ? 'yes' === wc_clean( wp_unslash( $_POST['save_payment_method'] ) ) : false;
-		$currency                     = strtolower( get_woocommerce_currency() );
+		$currency                     = strtolower( $order->get_currency() );
 		$amount                       = $order->get_total();
 
 		$payment_information = [
