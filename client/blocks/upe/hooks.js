6b0c5a794f7a789631114f97333f60641033a9e2
James Allan <james.allan@automattic.com>
2023-11-27 15:46:22 +1000

    Add function block comments to hooks functions

--- client/blocks/upe/hooks.js
+++ client/blocks/upe/hooks.js
@@ -1,6 +1,19 @@
 import { useEffect } from '@wordpress/element';
 import confirmCardPayment from './confirm-card-payment.js';
 
+/**
+ * Handles the Block Checkout onCheckoutSuccess event.
+ *
+ * Confirms the payment intent which was created on server and is now ready to be confirmed. The intent ID is passed in the paymentDetails object via the
+ * redirect arg which will be in the following format: #wc-stripe-confirm-pi/si:{order_id}:{client_secret}:{nonce}
+ *
+ * @param {*} api               The api object.
+ * @param {*} stripe            The Stripe object.
+ * @param {*} elements          The Stripe elements object.
+ * @param {*} onCheckoutSuccess The onCheckoutSuccess event.
+ * @param {*} emitResponse      Various helpers for usage with observer.
+ * @param {*} shouldSavePayment Whether or not to save the payment method.
+ */
 export const usePaymentCompleteHandler = (
 	api,
 	stripe,
@@ -26,6 +39,17 @@ export const usePaymentCompleteHandler = (
 	);
 };
 
+/**
+ * Handles the Block Checkout onCheckoutFail event.
+ *
+ * Displays the error message returned from server in the paymentDetails object in the PAYMENTS notice context container.
+ *
+ * @param {*} api            The api object.
+ * @param {*} stripe         The Stripe object.
+ * @param {*} elements       The Stripe elements object.
+ * @param {*} onCheckoutFail The onCheckoutFail event.
+ * @param {*} emitResponse   Various helpers for usage with observer.
+ */
 export const usePaymentFailHandler = (
 	api,
 	stripe,
