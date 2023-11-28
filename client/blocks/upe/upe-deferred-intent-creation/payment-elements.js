c9bcd5865b66704946649bebeaa6251ad4a50c1f
James Allan <james.allan@automattic.com>
2023-11-28 14:16:45 +1000

    Add support for passing the appearance to Stripe

--- client/blocks/upe/upe-deferred-intent-creation/payment-elements.js
+++ client/blocks/upe/upe-deferred-intent-creation/payment-elements.js
@@ -2,9 +2,36 @@
  * Internal dependencies
  */
 import { Elements } from '@stripe/react-stripe-js';
+import { getAppearance } from '../../../styles/upe';
 import PaymentProcessor from './payment-processor';
-import { getStripeServerData } from 'wcstripe/stripe-utils';
 import WCStripeAPI from 'wcstripe/api';
+import {
+	getStripeServerData,
+	setStorageWithExpiration,
+	getStorageWithExpiration,
+	storageKeys,
+} from 'wcstripe/stripe-utils';
+
+/**
+ * Initializes the appearance of the payment element by retrieving the UPE configuration
+ * from the API and saving the appearance if it doesn't exist. If the appearance already exists,
+ * it is simply returned.
+ *
+ * @return {Object} The appearance object for the UPE.
+ */
+function initializeAppearance() {
+	const themeName = getStripeServerData()?.theme_name;
+	const storageKey = `${ storageKeys.UPE_APPEARANCE }_${ themeName }`;
+	let appearance = getStorageWithExpiration( storageKey );
+
+	if ( ! appearance ) {
+		appearance = getAppearance();
+		const oneDayDuration = 24 * 60 * 60 * 1000;
+		setStorageWithExpiration( storageKey, appearance, oneDayDuration );
+	}
+
+	return appearance;
+}
 
 /**
  * Renders a Stripe Payment elements component.
@@ -19,6 +46,7 @@ const PaymentElements = ( { api, ...props } ) => {
 	const stripe = api.getStripe();
 	const amount = Number( getStripeServerData()?.cartTotal );
 	const currency = getStripeServerData()?.currency.toLowerCase();
+	const appearance = initializeAppearance();
 
 	return (
 		<Elements
@@ -29,6 +57,7 @@ const PaymentElements = ( { api, ...props } ) => {
 				currency,
 				paymentMethodCreation: 'manual',
 				paymentMethodTypes: [ props.paymentMethodId ],
+				appearance,
 			} }
 		>
 			<PaymentProcessor api={ api } { ...props } />
