49c2bccc154c53041510ef9ba0ff07286e24cbbc
Wesley Rosa <wesley.rosa@automattic.com>
2024-01-12 15:45:30 -0300

    Increasing code coverage

--- tests/phpunit/test-wc-stripe-intent-controller.php
+++ tests/phpunit/test-wc-stripe-intent-controller.php
@@ -36,7 +36,7 @@ class WC_Stripe_Intent_Controller_Test extends WP_UnitTestCase {
 		$this->order           = WC_Helper_Order::create_order();
 		$this->gateway         = $this->getMockBuilder( 'WC_Stripe_UPE_Payment_Gateway' )
 			->disableOriginalConstructor()
-			->setMethods( [ 'maybe_process_upe_redirect' ] )
+			->setMethods( [ 'maybe_process_upe_redirect', 'get_intent_from_order', 'get_upe_enabled_at_checkout_payment_method_ids' ] )
 			->getMock();
 		$this->mock_controller = $this->getMockBuilder( 'WC_Stripe_Intent_Controller' )
 			->disableOriginalConstructor()
@@ -113,5 +113,68 @@ class WC_Stripe_Intent_Controller_Test extends WP_UnitTestCase {
 		add_filter( 'pre_http_request', $test_request, 10, 3 );
 
 		$this->mock_controller->create_payment_intent( $this->order->get_id() );
+	}
+
+	/**
+	 * Test for `get_existing_compatible_payment_intent` method.
+	 *
+	 * @param string|null $associated_intent Associated intent.
+	 * @param string|null $expected Expected result.
+	 * @return void
+	 * @dataProvider provide_test_get_existing_compatible_payment_intent
+	 * @throws WC_Stripe_Exception If invalid payment method type is passed.
+	 */
+	public function test_get_existing_compatible_payment_intent( $associated_intent, $expected ) {
+		$this->gateway
+			->expects( $this->once() )
+			->method( 'get_intent_from_order' )
+			->willReturn( $associated_intent );
+
+		$this->gateway
+			->expects( $this->any() )
+			->method( 'get_upe_enabled_at_checkout_payment_method_ids' )
+			->willReturn(
+				[
+					WC_Stripe_UPE_Payment_Method_CC::STRIPE_ID,
+					WC_Stripe_UPE_Payment_Method_Link::STRIPE_ID,
+				]
+			);
+
+		$actual = $this->mock_controller->get_existing_compatible_payment_intent( $this->order, '' );
+		$this->assertEquals( $expected, $actual );
+	}
+
+	/**
+	 * Provider for `test_get_existing_compatible_payment_intent` method.
+	 *
+	 * @return array
+	 */
+	public function provide_test_get_existing_compatible_payment_intent() {
+		$compatible_intent     = (object) [
+			'id'                   => 'pi_123',
+			'payment_method_types' => [
+				WC_Stripe_UPE_Payment_Method_CC::STRIPE_ID,
+				WC_Stripe_UPE_Payment_Method_Link::STRIPE_ID,
+			],
+		];
+		$not_compatible_intent = (object) [
+			'id'                   => 'pi_456',
+			'payment_method_types' => [ 'boleto' ],
+		];
+
+		return [
+			'no intent associated with order' => [
+				'associated intent' => null,
+				'expected'          => null,
+			],
+			'compatible intent found'         => [
+				'associated intent' => $compatible_intent,
+				'expected'          => $compatible_intent,
+			],
+			'no compatible intent found'      => [
+				'associated intent' => $not_compatible_intent,
+				'expected'          => null,
+			],
+		];
 	}
 }
