4eb207efa856dca91417e065a979dd8abcaf8162
James Allan <james.allan@automattic.com>
2024-02-20 15:45:50 +1000

    Mock that automatic capture is enabled when testing payment method capabilities

--- tests/phpunit/test-class-wc-stripe-upe-payment-method.php
+++ tests/phpunit/test-class-wc-stripe-upe-payment-method.php
@@ -363,6 +363,7 @@ class WC_Stripe_UPE_Payment_Method_Test extends WP_UnitTestCase {
 			$this->set_mock_payment_method_return_value( 'get_capabilities_response', $mock_capabilities_response, true );
 			$this->set_mock_payment_method_return_value( 'get_woocommerce_currency', $currency );
 			$this->set_mock_payment_method_return_value( 'is_subscription_item_in_cart', false );
+			$this->set_mock_payment_method_return_value( 'is_automatic_capture_enabled', true );
 
 			$payment_method = $this->mock_payment_methods[ $id ];
 			$this->assertFalse( $payment_method->is_enabled_at_checkout() );
@@ -388,6 +389,7 @@ class WC_Stripe_UPE_Payment_Method_Test extends WP_UnitTestCase {
 			$this->set_mock_payment_method_return_value( 'get_woocommerce_currency', 'CASHMONEY', true );
 			$this->set_mock_payment_method_return_value( 'get_capabilities_response', self::MOCK_ACTIVE_CAPABILITIES_RESPONSE );
 			$this->set_mock_payment_method_return_value( 'is_subscription_item_in_cart', false );
+			$this->set_mock_payment_method_return_value( 'is_automatic_capture_enabled', true );
 
 			$payment_method       = $this->mock_payment_methods[ $id ];
 			$supported_currencies = $payment_method->get_supported_currencies();
