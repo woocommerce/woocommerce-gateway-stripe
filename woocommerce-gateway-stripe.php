25594a8c3187ebf70a6b48a9d75e4a9f359db52b
Mayisha <mayisha007@gmail.com>
2024-02-23 01:52:12 +0600

    add gateways to the WC gateways list as before. - they were removed in PR#2839

--- woocommerce-gateway-stripe.php
+++ woocommerce-gateway-stripe.php
@@ -372,13 +372,37 @@ function woocommerce_gateway_stripe() {
 			}
 
 			/**
-			 * Add the main Stripe gateway to WooCommerce.
+			 * Add the gateways to WooCommerce.
 			 *
 			 * @since 1.0.0
 			 * @version 5.6.0
 			 */
 			public function add_gateways( $methods ) {
 				$methods[] = $this->get_main_stripe_gateway();
+
+				if ( ! WC_Stripe_Feature_Flags::is_upe_preview_enabled() || ! WC_Stripe_Feature_Flags::is_upe_checkout_enabled() ) {
+					// These payment gateways will not be included in the gateway list when UPE is enabled:
+					$methods[] = WC_Gateway_Stripe_Alipay::class;
+					$methods[] = WC_Gateway_Stripe_Sepa::class;
+					$methods[] = WC_Gateway_Stripe_Giropay::class;
+					$methods[] = WC_Gateway_Stripe_Ideal::class;
+					$methods[] = WC_Gateway_Stripe_Bancontact::class;
+					$methods[] = WC_Gateway_Stripe_Eps::class;
+					$methods[] = WC_Gateway_Stripe_P24::class;
+					$methods[] = WC_Gateway_Stripe_Boleto::class;
+					$methods[] = WC_Gateway_Stripe_Oxxo::class;
+
+					/** Show Sofort if it's already enabled. Hide from the new merchants and keep it for the old ones who are already using this gateway, until we remove it completely.
+					 * Stripe is deprecating Sofort https://support.stripe.com/questions/sofort-is-being-deprecated-as-a-standalone-payment-method.
+					 */
+					$sofort_settings = get_option( 'woocommerce_stripe_sofort_settings', [] );
+					if ( isset( $sofort_settings['enabled'] ) && 'yes' === $sofort_settings['enabled'] ) {
+						$methods[] = WC_Gateway_Stripe_Sofort::class;
+					}
+				}
+
+				// Multibanco will always be added to the gateway list, regardless if UPE is enabled or disabled:
+				$methods[] = WC_Gateway_Stripe_Multibanco::class;
 
 				return $methods;
 			}
