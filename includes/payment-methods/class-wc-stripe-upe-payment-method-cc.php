4dc9c5244cbc030c3232b66651aa4373d8251924
Wesley Rosa <wesley.rosa@automattic.com>
2024-03-20 12:09:06 -0300

    Handling saving of co-branded credit cards

--- includes/payment-methods/class-wc-stripe-upe-payment-method-cc.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-method-cc.php
@@ -81,7 +81,13 @@ class WC_Stripe_UPE_Payment_Method_CC extends WC_Stripe_UPE_Payment_Method {
 	 * @return WC_Payment_Token_CC
 	 */
 	public function create_payment_token_for_user( $user_id, $payment_method ) {
-		$token = new WC_Payment_Token_CC();
+		if ( count( $payment_method->card->networks->available ) > 1 ) {
+			$token = new WC_Payment_Token_CC_Co_Branded();
+			$token->set_available_networks( $payment_method->card->networks->available );
+			$token->set_preferred_network( $payment_method->card->networks->preferred );
+		} else {
+			$token = new WC_Payment_Token_CC();
+		}
 		$token->set_expiry_month( $payment_method->card->exp_month );
 		$token->set_expiry_year( $payment_method->card->exp_year );
 		$token->set_card_type( strtolower( $payment_method->card->brand ) );
