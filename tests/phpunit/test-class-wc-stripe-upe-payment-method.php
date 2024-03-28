d0dc4c36abc19b1c4b0591e3dc77e1509cf9dd17
Wesley Rosa <wesley.rosa@automattic.com>
2024-03-28 09:10:22 -0300

    Fix tests

--- tests/phpunit/test-class-wc-stripe-upe-payment-method.php
+++ tests/phpunit/test-class-wc-stripe-upe-payment-method.php
@@ -483,6 +483,7 @@ class WC_Stripe_UPE_Payment_Method_Test extends WP_UnitTestCase {
 					$this->assertSame( $token->get_card_type(), $cartes_bancaires_brand );
 					unset( $card_payment_method_mock->card->networks->preferred );
 					if ( version_compare( WC_VERSION, '8.7.0', '>' ) ) {
+					if ( version_compare( WC_VERSION, '8.8.0', '>' ) ) {
 						$this->assertTrue( $token->is_co_branded() );
 						$this->assertSame( $token->get_available_networks(), $card_payment_method_mock->card->networks->available );
 						$this->assertSame( $token->get_preferred_network(), $card_payment_method_mock->card->networks->preferred );
