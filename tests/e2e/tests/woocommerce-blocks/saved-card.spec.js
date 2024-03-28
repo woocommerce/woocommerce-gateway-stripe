e9f243b7c9ef357252bd83451ab136315fb69a72
Wesley Rosa <wesley.rosa@automattic.com>
2024-03-28 14:18:53 -0300

    Revert e2e tests updates

--- tests/e2e/tests/woocommerce-blocks/saved-card.spec.js
+++ tests/e2e/tests/woocommerce-blocks/saved-card.spec.js
@@ -65,10 +65,16 @@ test( 'customer can checkout with a saved card @smoke @blocks', async ( {
 
 		// check that there are saved payment methods.
 		await expect(
-			page.locator( 'input[id^="wc-stripe-payment-token-"]' )
+			page.locator(
+				'input[id^="radio-control-wc-payment-method-saved-tokens-"]'
+			)
 		).toHaveCount( 1 );
 
-		await page.locator( 'input[id^="wc-stripe-payment-token-"]' ).click();
+		await page
+			.locator(
+				'input[id^="radio-control-wc-payment-method-saved-tokens-"]'
+			)
+			.click();
 
 		await page.locator( 'text=Place order' ).click();
 
