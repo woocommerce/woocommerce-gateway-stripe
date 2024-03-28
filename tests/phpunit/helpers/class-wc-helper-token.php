386cdb1f4ab0ecc2b84d92b6441dd8fa7fa604e2
Wesley Rosa <wesley.rosa@automattic.com>
2024-03-28 16:35:35 -0300

    Removing unnecessary test

--- tests/phpunit/helpers/class-wc-helper-token.php
+++ tests/phpunit/helpers/class-wc-helper-token.php
@@ -22,6 +22,11 @@ class WC_Helper_Token {
 		$token->set_last4( '4242' );
 		$token->set_expiry_month( 7 );
 		$token->set_expiry_year( intval( gmdate( 'Y' ) ) + 1 );
+		if ( version_compare( WC_VERSION, '8.7.0', '>' ) ) {
+			$token->set_available_networks( [ 'visa', 'cartes_bancaires' ] );
+			$token->set_preferred_network( 'visa' );
+			$token->save();
+		}
 		$token->save();
 
 		return WC_Payment_Tokens::get( $token->get_id() );
@@ -42,31 +47,6 @@ class WC_Helper_Token {
 		$token->set_user_id( is_null( $user_id ) ? get_current_user_id() : $user_id );
 		$token->set_last4( '4269' );
 		$token->save();
-
-		return WC_Payment_Tokens::get( $token->get_id() );
-	}
-
-	/**
-	 * Create a CC token (with co-branded cards support).
-	 *
-	 * @param string $payment_method Token payment method.
-	 * @param int    $user_id        ID of the token's user, defaults to get_current_user_id().
-	 * @param string $gateway        Token's Gateway ID, default to WC_Gateway_Stripe::ID
-	 */
-	public static function create_cc_stripe_token( $payment_method, $user_id = null, $gateway = WC_Gateway_Stripe::ID ) {
-		$token = new WC_Payment_Token_CC();
-		$token->set_token( $payment_method );
-		$token->set_gateway_id( $gateway );
-		$token->set_user_id( is_null( $user_id ) ? get_current_user_id() : $user_id );
-		$token->set_card_type( 'visa' );
-		$token->set_last4( '4242' );
-		$token->set_expiry_month( 7 );
-		$token->set_expiry_year( intval( gmdate( 'Y' ) ) + 1 );
-		if ( version_compare( WC_VERSION, '8.7.0', '>' ) ) {
-			$token->set_available_networks( [ 'visa', 'cartes_bancaires' ] );
-			$token->set_preferred_network( 'visa' );
-			$token->save();
-		}
 
 		return WC_Payment_Tokens::get( $token->get_id() );
 	}
