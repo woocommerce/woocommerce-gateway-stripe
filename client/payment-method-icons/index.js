a60c7f8fc6569302e87427692d9a412c5f667efb
Danae Millan <danae.millan@automattic.com>
2024-04-03 22:56:30 -0600

    Add Alipay and Multibanco icons on the block checkout

--- client/payment-method-icons/index.js
+++ client/payment-method-icons/index.js
@@ -1,5 +1,7 @@
+import AlipayIcon from './alipay';
 import CreditCardIcon from './cards';
 import GiropayIcon from './giropay';
+import MultibancoIcon from './multibanco';
 import SofortIcon from './sofort';
 import SepaIcon from './sepa';
 import EpsIcon from './eps';
@@ -10,8 +12,10 @@ import BoletoIcon from './boleto';
 import OxxoIcon from './oxxo';
 
 export default {
+	alipay: <AlipayIcon />,
 	card: <CreditCardIcon />,
 	giropay: <GiropayIcon />,
+	multibanco: <MultibancoIcon />,
 	sepa_debit: <SepaIcon />,
 	sofort: <SofortIcon />,
 	eps: <EpsIcon />,
