5cf44d2c5c6a01879bb06cab528b987d8b026d46
Danae Millan <danae.millan@automattic.com>
2024-04-04 13:36:37 -0600

    Update importing of the payment methods icons

--- client/payment-method-icons/index.js
+++ client/payment-method-icons/index.js
@@ -12,16 +12,16 @@ import BoletoIcon from './boleto';
 import OxxoIcon from './oxxo';
 
 export default {
-	alipay: <AlipayIcon />,
-	card: <CreditCardIcon />,
-	giropay: <GiropayIcon />,
-	multibanco: <MultibancoIcon />,
-	sepa_debit: <SepaIcon />,
-	sofort: <SofortIcon />,
-	eps: <EpsIcon />,
-	bancontact: <BancontactIcon />,
-	ideal: <IdealIcon />,
-	p24: <P24Icon />,
-	boleto: <BoletoIcon />,
-	oxxo: <OxxoIcon />,
+	alipay: AlipayIcon,
+	card: CreditCardIcon,
+	giropay: GiropayIcon,
+	multibanco: MultibancoIcon,
+	sepa_debit: SepaIcon,
+	sofort: SofortIcon,
+	eps: EpsIcon,
+	bancontact: BancontactIcon,
+	ideal: IdealIcon,
+	p24: P24Icon,
+	boleto: BoletoIcon,
+	oxxo: OxxoIcon,
 };
