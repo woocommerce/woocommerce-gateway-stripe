b4d81dfcd3c27a04771c65c08a94ef9c499fdac4
Wesley Rosa <wesley.rosa@automattic.com>
2024-04-05 18:13:35 -0300

    Removing unnecessary test part

--- tests/phpunit/test-class-wc-stripe-upe-payment-method.php
+++ tests/phpunit/test-class-wc-stripe-upe-payment-method.php
@@ -470,11 +470,6 @@ class WC_Stripe_UPE_Payment_Method_Test extends WP_UnitTestCase {
 					$this->assertTrue( 'WC_Payment_Token_CC' === get_class( $token ) );
 					$this->assertSame( $token->get_last4(), $card_payment_method_mock->card->last4 );
 					$this->assertSame( $token->get_token(), $card_payment_method_mock->id );
-					if ( version_compare( WC_VERSION, '8.8.0', '>' ) ) {
-						$this->assertTrue( $token->is_co_branded() );
-						$this->assertSame( $token->get_available_networks(), $card_payment_method_mock->card->networks->available );
-						$this->assertSame( $token->get_preferred_network(), $card_payment_method_mock->card->networks->preferred );
-					}
 					break;
 				case WC_Stripe_UPE_Payment_Method_Link::STRIPE_ID:
 					$link_payment_method_mock = $this->array_to_object( self::MOCK_LINK_PAYMENT_METHOD_TEMPLATE );
