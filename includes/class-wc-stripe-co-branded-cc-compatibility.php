d5f268e3389113859b8715aec16c6be891ef0a66
Wesley Rosa <wesley.rosa@automattic.com>
2024-04-15 12:23:26 -0300

    Updating minimum WC version required

--- includes/class-wc-stripe-co-branded-cc-compatibility.php
+++ includes/class-wc-stripe-co-branded-cc-compatibility.php
@@ -4,7 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class WC_Stripe_Co_Branded_CC_Compatibility {
-	const MIN_WC_VERSION = '8.8.0';
+	const MIN_WC_VERSION = '8.9.0';
 
 	public static function is_wc_supported() {
 		return 'beta' !== WC_VERSION && version_compare( WC_VERSION, self::MIN_WC_VERSION, '>' );
