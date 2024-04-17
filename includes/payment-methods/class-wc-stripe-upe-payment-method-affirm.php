9c303efc9cfcd80899f8f2e267e9ae6a49555134
Wesley Rosa <wesley.rosa@automattic.com>
2024-04-17 15:16:17 -0300

    Including temp payment icon

---
+++ includes/payment-methods/class-wc-stripe-upe-payment-method-affirm.php
@@ -0,0 +1,29 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * The Affirm Payment Method class extending UPE base class
+ */
+class WC_Stripe_UPE_Payment_Method_Affirm extends WC_Stripe_UPE_Payment_Method {
+
+	const STRIPE_ID = 'affirm';
+
+	/**
+	 * Constructor for Affirm payment method
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->stripe_id                    = self::STRIPE_ID;
+		$this->title                        = __( 'Affirm', 'woocommerce-gateway-stripe' );
+		$this->is_reusable                  = false;
+		$this->supported_currencies         = [ 'AUD', 'CAD', 'CHF', 'CZK', 'DKK', 'EUR', 'GBP', 'NOK', 'NZD', 'PLN', 'SEK', 'USD' ];
+		$this->accept_only_domestic_payment = true;
+		$this->label                        = __( 'Affirm', 'woocommerce-gateway-stripe' );
+		$this->description                  = __(
+			'Allow customers to pay over time with Affirm.',
+			'woocommerce-gateway-stripe'
+		);
+	}
+}
