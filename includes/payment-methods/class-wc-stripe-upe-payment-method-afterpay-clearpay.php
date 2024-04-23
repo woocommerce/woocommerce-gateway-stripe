bd7b79e40c2622c509bf76979c03c69217edea93
Wesley Rosa <wesley.rosa@automattic.com>
2024-04-23 18:48:55 -0300

    Improve conditional strings handling

--- includes/payment-methods/class-wc-stripe-upe-payment-method-afterpay-clearpay.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-method-afterpay-clearpay.php
@@ -16,23 +16,60 @@ class WC_Stripe_UPE_Payment_Method_Afterpay_Clearpay extends WC_Stripe_UPE_Payme
 	public function __construct() {
 		parent::__construct();
 
-		$cached_account_data = WC_Stripe::get_instance()->account->get_cached_account_data();
-		$account_country     = $cached_account_data['country'] ?? null;
-		$method_name         = 'GB' === $account_country ? __( 'Clearpay', 'woocommerce-gateway-stripe' ) : __( 'Afterpay', 'woocommerce-gateway-stripe' );
-
 		$this->stripe_id                    = self::STRIPE_ID;
-		$this->title                        = $method_name;
+		$this->title                        = __( 'Clearpay / Afterpay', 'woocommerce-gateway-stripe' );
 		$this->is_reusable                  = false;
 		$this->supported_currencies         = [ 'USD', 'CAD', 'GBP', 'AUD', 'NZD' ];
 		$this->supported_countries          = [ 'AU', 'CA', 'GB', 'NZ', 'US' ];
 		$this->accept_only_domestic_payment = true;
-		$this->label                        = $method_name;
-		$this->description                  = 'GB' === $account_country ? __(
-			'Allow customers to pay over time with Clearpay.',
+		$this->label                        = __( 'Clearpay / Afterpay', 'woocommerce-gateway-stripe' );
+		$this->description                  = __(
+			'Allow customers to pay over time with Clearpay / Afterpay.',
 			'woocommerce-gateway-stripe'
-		) : __(
+		);
+	}
+
+	/**
+	 * Returns payment method title
+	 *
+	 * @param array|bool $payment_details Optional payment details from charge object.
+	 *
+	 * @return string
+	 */
+	public function get_title( $payment_details = false ) {
+		if ( $this->is_gb_country() ) {
+			return __( 'Clearpay', 'woocommerce-gateway-stripe' );
+		}
+		return __( 'Afterpay', 'woocommerce-gateway-stripe' );
+	}
+
+	/**
+	 * /**
+	 *  Return the gateway's description.
+	 *
+	 * @return string
+	 */
+	public function get_description( $payment_details = false ) {
+		if ( $this->is_gb_country() ) {
+			return __(
+				'Allow customers to pay over time with Clearpay.',
+				'woocommerce-gateway-stripe'
+			);
+		}
+		return __(
 			'Allow customers to pay over time with Afterpay.',
 			'woocommerce-gateway-stripe'
 		);
+	}
+
+	/**
+	 * Returns true if the Stripe account country is GB
+	 *
+	 * @return boolean
+	 */
+	private function is_gb_country() {
+		$cached_account_data = WC_Stripe::get_instance()->account->get_cached_account_data();
+		$account_country     = $cached_account_data['country'] ?? null;
+		return 'GB' === $account_country;
 	}
 }
