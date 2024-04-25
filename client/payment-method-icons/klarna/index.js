daba441e4f8c4044ff21251ced614bd2f1fcc3c2
Danae Millan <danae.millan@automattic.com>
2024-04-25 14:10:38 -0600

    Fix Klarna icon sizing

--- client/payment-method-icons/klarna/index.js
+++ client/payment-method-icons/klarna/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import IconWithShell from '../styles/icon-with-shell';
 import icon from './icon.svg';
 
-const KlarnaIcon = ( props ) => <IconWithShell { ...props } src={ icon } />;
+const Wrapper = styled( IconWithShell )`
+	padding: 0;
+	border: none;
+
+	img {
+		outline: 1px solid #ddd;
+	}
+`;
+
+const KlarnaIcon = ( props ) => <Wrapper { ...props } src={ icon } />;
 
 export default KlarnaIcon;
