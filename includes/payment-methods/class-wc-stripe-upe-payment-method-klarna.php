ae0b9b1acae6bc67d83fa53cb0243dc516737ce1
James Allan <james.allan@automattic.com>
2024-05-01 15:41:57 +1000

    Restrict Klarna to domestic transactions and cross EEA border transactions

--- includes/payment-methods/class-wc-stripe-upe-payment-method-klarna.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-method-klarna.php
@@ -27,4 +27,40 @@ class WC_Stripe_UPE_Payment_Method_Klarna extends WC_Stripe_UPE_Payment_Method {
 			'woocommerce-gateway-stripe'
 		);
 	}
+
+	/**
+	 * Determines if the payment method is enabled at checkout.
+	 *
+	 * Klarna has unique requirements for domestic transactions. The customer must be located in the same country as the merchant's Stripe account.
+	 * Additionally, customers in the EEA can transact across all other EEA countries - including Switzerland and the UK.
+	 *
+	 * @return boolean
+	 */
+	public function is_enabled_at_checkout( $order_id = null, $account_domestic_currency = null ) {
+		if ( ! parent::is_enabled_at_checkout( $order_id, $account_domestic_currency ) ) {
+			return false;
+		}
+
+		// If there's no customer, default to true as to not block a payment attempt.
+		if ( ! isset( WC()->customer ) ) {
+			return true;
+		}
+
+		$billing_country = strtoupper( WC()->customer->get_billing_country() );
+		$account_country = strtoupper( WC_Stripe::get_instance()->account->get_cached_account_data()['country'] ?? WC()->countries->get_base_country() );
+
+		// A domestic transaction is allowed.
+		if ( $billing_country === $account_country ) {
+			return true;
+		}
+
+		// Countries in the EEA can transact across all other EEA countries. This includes Switzerland and the UK who aren't strictly in the EU.
+		$eea_countries = WC_Stripe_Helper::get_european_economic_area_countries() + [ 'CH', 'GB' ];
+
+		if ( in_array( $billing_country, $eea_countries, true ) && in_array( $account_country, $eea_countries, true ) ) {
+			return true;
+		}
+
+		return false;
+	}
 }
