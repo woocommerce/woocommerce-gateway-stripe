f2874f867ddee8da6846606d9557868e4d3cffc4
Wesley Rosa <wesley.rosa@automattic.com>
2024-05-16 10:03:16 -0300

    Fix subscription classes verification method

--- includes/compat/trait-wc-stripe-subscriptions-utilities.php
+++ includes/compat/trait-wc-stripe-subscriptions-utilities.php
@@ -19,7 +19,7 @@ trait WC_Stripe_Subscriptions_Utilities_Trait {
 	 * @return bool Whether subscriptions is enabled or not.
 	 */
 	public function is_subscriptions_enabled() {
-		return class_exists( 'WC_Subscriptions' ) && version_compare( WC_Subscriptions::$version, '2.2.0', '>=' );
+		return class_exists( 'WC_Subscriptions' ) && class_exists( 'WC_Subscription' ) && version_compare( WC_Subscriptions::$version, '2.2.0', '>=' );
 	}
 
 	/**
@@ -79,7 +79,7 @@ trait WC_Stripe_Subscriptions_Utilities_Trait {
 	 * @return bool Indicates whether the save payment method checkbox should be displayed or not.
 	 */
 	public function display_save_payment_method_checkbox( $display ) {
-		if ( ! $this->is_subscriptions_enabled() || WC_Subscriptions_Cart::cart_contains_subscription() || $this->is_changing_payment_method_for_subscription() ) {
+		if ( WC_Subscriptions_Cart::cart_contains_subscription() || $this->is_changing_payment_method_for_subscription() ) {
 			return false;
 		}
 		// Only render the "Save payment method" checkbox if there are no subscription products in the cart.
