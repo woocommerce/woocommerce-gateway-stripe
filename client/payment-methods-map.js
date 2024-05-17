ce440823f7cbc04b4249ff5c4278473fc7aafa66
Mayisha <mayisha007@gmail.com>
2024-05-17 15:02:29 +0600

    show wechat in the payment method list of the settings page

--- client/payment-methods-map.js
+++ client/payment-methods-map.js
@@ -205,4 +205,28 @@ export default {
 		Icon: icons.multibanco,
 		currencies: [ 'EUR' ],
 	},
+	wechat_pay: {
+		id: 'wechat_pay',
+		label: __( 'WeChat Pay', 'woocommerce-gateway-stripe' ),
+		description: __(
+			'WeChat Pay is a popular mobile payment and digital wallet service by WeChat in China.',
+			'woocommerce-gateway-stripe'
+		),
+		Icon: icons.alipay,
+		currencies: [
+			'CNY',
+			'AUD',
+			'CAD',
+			'EUR',
+			'GBP',
+			'HKD',
+			'JPY',
+			'SGD',
+			'USD',
+			'DKK',
+			'NOK',
+			'SEK',
+			'CHF',
+		],
+	},
 };
