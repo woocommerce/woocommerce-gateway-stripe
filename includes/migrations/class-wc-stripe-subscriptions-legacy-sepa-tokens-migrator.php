51cbbfb2675e0fececa44a29985bda5e31ae5581
Danae Millan <danae.millan@automattic.com>
2024-05-19 22:10:49 +0200

    Retrieve the token associated with the subscription during migration

--- includes/migrations/class-wc-stripe-subscriptions-legacy-sepa-tokens-migrator.php
+++ includes/migrations/class-wc-stripe-subscriptions-legacy-sepa-tokens-migrator.php
@@ -20,6 +20,13 @@ class WC_Stripe_Subscriptions_Legacy_SEPA_Tokens_Migrator extends WCS_Background
 	const LEGACY_TOKEN_PAYMENT_METHOD_META_KEY = '_migrated_sepa_payment_method';
 
 	/**
+	 * Subscription meta key used to store the associated source ID.
+	 *
+	 * @var string
+	 */
+	const SOURCE_ID_META_KEY = '_stripe_source_id';
+
+	/**
 	 * WC_Logger instance.
 	 *
 	 * @todo Use WC_Stripe_Logger?
@@ -55,6 +62,25 @@ class WC_Stripe_Subscriptions_Legacy_SEPA_Tokens_Migrator extends WCS_Background
 			$this->logger->info( sprintf( 'Migrating subscription #%1$d.', $subscription_id ) );
 
 			$subscription = $this->get_subscription_to_migrate( $subscription_id );
+			$source_id    = $subscription->get_meta( self::SOURCE_ID_META_KEY );
+			$user_id      = $subscription->get_user_id();
+
+			// It's possible that a token using the updated gateway ID already exists.
+			// We create these when the shopper vistis the Checkout and the My account > Payment methods page.
+			$updated_token = $this->get_subscription_token(
+				$source_id,
+				$user_id,
+				WC_Stripe_UPE_Payment_Gateway::ID . '_' . WC_Stripe_UPE_Payment_Method_Sepa::STRIPE_ID
+			);
+
+			// Create an updated token from the legacy one.
+			if ( ! $updated_token ) {
+				$legacy_token = $this->get_subscription_token(
+					$source_id,
+					$user_id,
+					WC_Gateway_Stripe_Sepa::ID
+				);
+			}
 
 			$this->update_subscription_legacy_payment_method( $subscription );
 		} catch ( \Exception $e ) {
@@ -100,6 +126,9 @@ class WC_Stripe_Subscriptions_Legacy_SEPA_Tokens_Migrator extends WCS_Background
 	 * @param WC_Subscription $subscription Subscriptions for which the payment method must be updated.
 	 */
 	private function update_subscription_legacy_payment_method( WC_Subscription $subscription ) {
+		// This is what we do in WC_Stripe_Subscriptions_Trait::maybe_update_source_on_subscription_order,
+		// but shouldn't we use WCS_Payment_Tokens::update_subscription_token() instead? Or WC_Order::add_payment_token()?
+		// The latter is the one used by WooPayments.
 		$sepa_gateway_id = WC_Stripe_UPE_Payment_Gateway::ID . '_' . WC_Stripe_UPE_Payment_Method_Sepa::STRIPE_ID;
 
 		// Update the payment method associated with the subscription.
@@ -109,6 +138,38 @@ class WC_Stripe_Subscriptions_Legacy_SEPA_Tokens_Migrator extends WCS_Background
 		$subscription->update_meta_data( self::LEGACY_TOKEN_PAYMENT_METHOD_META_KEY, WC_Gateway_Stripe_Sepa::ID );
 
 		$subscription->save();
+	}
+
+	/**
+	 * Returns the token associated with a subscription.
+	 *
+	 * @todo Is there a better way to retrieve these? WC_Order::get_payment_tokens() returns an empty array for some reason.
+	 *
+	 * @param string $subscription_source_id   The Source or Payment Method ID associated with the subscription.
+	 * @param int    $subscription_customer_id The WordPress User ID to whom the subscription belongs.
+	 * @param string $gateway_id               The ID of the payment gateway for which we're retrieving the tokens.
+	 * @return WC_Payment_Token|false
+	 */
+	private function get_subscription_token( string $subscription_source_id, int $subscription_customer_id, string $gateway_id ) {
+
+		// We specify the Gateway ID because
+		// - The Legacy SEPA gateway would be unavailable and its tokens are not retrieved.
+		// - The token value could be the same between two tokens with different gateway IDs,
+		//   like stripe_sepa (The Legacy gateway) and stripe_sepa_debit (The Updated gateway).
+		$customer_tokens = WC_Payment_Tokens::get_tokens(
+			[
+				'user_id'    => $subscription_customer_id,
+				'gateway_id' => $gateway_id,
+			]
+		);
+
+		foreach ( $customer_tokens as $token ) {
+			if ( $subscription_source_id === $token->get_token() ) {
+				return $token;
+			}
+		}
+
+		return false;
 	}
 
 	/**
