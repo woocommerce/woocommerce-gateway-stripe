d00a2a89a016b87423b1421f116c93b91ec4a7c3
James Allan <james.allan@automattic.com>
2024-05-29 13:07:23 +1000

    Use the payment_information['return_url'] as it's already in the UPE redirect format

--- includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
@@ -856,9 +856,10 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 						$order_id,
 						$payment_information['selected_payment_type'],
 						$payment_intent->client_secret,
-						// To ensure the payment method is saved on the customer returning to the store, we use a return URL that includes UPE redirect params.
-						rawurlencode( $this->get_return_url_for_redirect( $order, $payment_information['save_payment_method_to_store'] ) )
+						// To ensure the payment is confirmed when the customer returns to the site, use the UPE redirect URL.
+						rawurlencode( $payment_information['return_url'] )
 					);
+
 				} elseif ( isset( $payment_intent->next_action->type ) && in_array( $payment_intent->next_action->type, [ 'redirect_to_url', 'alipay_handle_redirect' ], true ) && ! empty( $payment_intent->next_action->{$payment_intent->next_action->type}->url ) ) {
 					$redirect = $payment_intent->next_action->{$payment_intent->next_action->type}->url;
 				} else {
