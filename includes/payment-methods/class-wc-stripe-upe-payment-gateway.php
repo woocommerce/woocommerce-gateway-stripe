68770b3870b5bf4720e2795c7aee6ac89770db47
James Allan <james.allan@automattic.com>
2024-05-30 12:23:27 +1000

    Remove changes to redirect url

--- includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
@@ -861,8 +861,7 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 						$order_id,
 						$payment_information['selected_payment_type'],
 						$payment_intent->client_secret,
-						// To ensure the payment is confirmed when the customer returns to the site, use the UPE redirect URL.
-						rawurlencode( $payment_information['return_url'] )
+						rawurlencode( $redirect )
 					);
 				} elseif ( isset( $payment_intent->next_action->type ) && in_array( $payment_intent->next_action->type, [ 'redirect_to_url', 'alipay_handle_redirect' ], true ) && ! empty( $payment_intent->next_action->{$payment_intent->next_action->type}->url ) ) {
 					$redirect = $payment_intent->next_action->{$payment_intent->next_action->type}->url;
