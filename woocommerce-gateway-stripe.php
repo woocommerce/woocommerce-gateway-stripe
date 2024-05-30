f94d79f5606cba0016d674e8a2600cd3ce1b4602
Niels Lange <info@nielslange.de>
2024-05-30 18:38:40 +0700

    Declare compatibility with the Cart and Checkout blocks

--- woocommerce-gateway-stripe.php
+++ woocommerce-gateway-stripe.php
@@ -822,6 +822,7 @@ add_action(
 	'before_woocommerce_init',
 	function() {
 		if ( class_exists( '\Automattic\WooCommerce\Utilities\FeaturesUtil' ) ) {
+			\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'cart_checkout_blocks', __FILE__, true );
 			\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
 		}
 	}
