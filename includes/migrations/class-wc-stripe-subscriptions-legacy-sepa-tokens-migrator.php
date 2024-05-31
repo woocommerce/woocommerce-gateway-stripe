698372672f269de943717b55fd7b7789f7abd7aa
Danae Millan <danae.millan@automattic.com>
2024-05-31 21:41:29 +0200

    Paginate the results when retrieving subscriptions to update

--- includes/migrations/class-wc-stripe-subscriptions-legacy-sepa-tokens-migrator.php
+++ includes/migrations/class-wc-stripe-subscriptions-legacy-sepa-tokens-migrator.php
@@ -51,7 +51,8 @@ class WC_Stripe_Subscriptions_Legacy_SEPA_Tokens_Migrator extends WCS_Background
 			[
 				'return'         => 'ids',
 				'type'           => 'shop_subscription',
-				'limit'          => 100,
+				'posts_per_page' => 20,
+				'paged'          => $page,
 				'status'         => 'any',
 				'paged'          => $page,
 				'payment_method' => WC_Gateway_Stripe_Sepa::ID,
