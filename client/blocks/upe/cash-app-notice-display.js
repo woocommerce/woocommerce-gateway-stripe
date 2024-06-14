12d4f8cae888e1d13682aa47ef21c6780fe96957
Wesley Rosa <wesley.rosa@automattic.com>
2024-06-14 19:23:15 -0300

    Adding text functions

--- client/blocks/upe/cash-app-notice-display.js
+++ client/blocks/upe/cash-app-notice-display.js
@@ -4,16 +4,24 @@
  * This helper adds a notice to the Cash App Pay payment method in the checkout form to inform the user that the transaction may be rejected due to its amount
  * depending on their account and transaction history (and when it is higher than 2000 USD).
  */
+import { __ } from '@wordpress/i18n';
 import { callWhenElementIsAvailable } from 'wcstripe/blocks/upe/call-when-element-is-available';
+import { getBlocksConfiguration } from 'wcstripe/blocks/utils';
 
-function showCashAppLimitNotice() {
+function maybeRenderCashAppLimitNotice() {
+	const amount = Number( getBlocksConfiguration()?.cartTotal );
+	if ( amount <= 2000 ) {
+		return;
+	}
+
 	const limitNotice = document.createElement( 'div' );
 	limitNotice.classList.add(
 		'woocommerce-info',
 		'wc-block-checkout__payment-method-limit-notice'
 	);
-	limitNotice.textContent =
-		'Please note that, depending on your account and transaction history, Cash App Pay may reject your transaction due to its amount.';
+	limitNotice.textContent = __(
+		'Please note that, depending on your account and transaction history, Cash App Pay may reject your transaction due to its amount.'
+	);
 	document
 		.querySelector(
 			'.wc-block-checkout__payment-method .wc-block-components-notices'
@@ -28,8 +36,8 @@ export function maybeShowCashAppLimitNotice() {
 
 	// If the tokens are already loaded, update the token labels.
 	if ( hasNoticeElement ) {
-		showCashAppLimitNotice();
+		maybeRenderCashAppLimitNotice();
 	} else {
-		callWhenElementIsAvailable( selector, showCashAppLimitNotice );
+		callWhenElementIsAvailable( selector, maybeRenderCashAppLimitNotice );
 	}
 }
