712fd910dab1db065f391cff22829123fbb5c6ea
Mayisha <33387139+Mayisha@users.noreply.github.com>
2024-06-14 10:49:28 +0600

    Allow customizing the label and description of the UPE payment methods (#3176)

--- includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
@@ -174,7 +174,7 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 
 		$main_settings              = get_option( 'woocommerce_stripe_settings' );
 		$this->title                = $this->payment_methods['card']->get_title();
-		$this->description          = '';
+		$this->description          = $this->payment_methods['card']->get_description();
 		$this->enabled              = $this->get_option( 'enabled' );
 		$this->saved_cards          = 'yes' === $this->get_option( 'saved_cards' );
 		$this->testmode             = ! empty( $main_settings['testmode'] ) && 'yes' === $main_settings['testmode'];
@@ -473,6 +473,7 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 			$settings[ $payment_method_id ] = [
 				'isReusable'          => $payment_method->is_reusable(),
 				'title'               => $payment_method->get_title(),
+				'description'         => $payment_method->get_description(),
 				'testingInstructions' => $payment_method->get_testing_instructions(),
 				'showSaveOption'      => $this->should_upe_payment_method_show_save_option( $payment_method ),
 				'countries'           => $payment_method->get_available_billing_countries(),
