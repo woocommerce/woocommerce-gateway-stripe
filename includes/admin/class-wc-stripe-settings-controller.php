09aecdbec1e467381752da173f1e3b05fce64994
Mayisha <mayisha007@gmail.com>
2024-06-26 00:42:08 +0600

    set stripe gateways in the gateway order list when gateways are reordered on the payment settings page

--- includes/admin/class-wc-stripe-settings-controller.php
+++ includes/admin/class-wc-stripe-settings-controller.php
@@ -40,6 +40,22 @@ class WC_Stripe_Settings_Controller {
 		add_action( 'woocommerce_admin_field_payment_gateways', [ $this, 'hide_gateways_on_settings_page' ], 5 );
 
 		add_action( 'admin_init', [ $this, 'maybe_update_account_data' ] );
+
+		add_action( 'update_option_woocommerce_gateway_order', [ $this, 'set_stripe_gateways_in_list' ] );
+	}
+
+	public function set_stripe_gateways_in_list( $ordering ) {
+		$is_upe_enabled  = WC_Stripe_Feature_Flags::is_upe_checkout_enabled();
+		$stripe_settings = get_option( 'woocommerce_stripe_settings', [] );
+
+		$ordered_payment_method_ids = $is_upe_enabled ? $stripe_settings['stripe_upe_payment_method_order'] : $stripe_settings['stripe_legacy_method_order'];
+
+		// Prevent unnecessary recursion, 'add_stripe_methods_in_woocommerce_gateway_order' saves the same option that triggers this callback.
+		remove_action( 'update_option_woocommerce_gateway_order', [ $this, 'set_stripe_gateways_in_list' ] );
+
+		WC_Stripe_Helper::add_stripe_methods_in_woocommerce_gateway_order( $ordered_payment_method_ids );
+
+		add_action( 'update_option_woocommerce_gateway_order', [ $this, 'set_stripe_gateways_in_list' ] );
 	}
 
 	/**
