1388d08b0741ebfec6ba6b3facf27535278745a2
Wesley Rosa <wesleyjrosa@gmail.com>
2024-07-09 18:51:57 -0300

    Update client/settings/general-settings-section/__tests__/general-settings-section.test.js
    Co-authored-by: Diego Curbelo <diego@curbelo.com>

--- client/settings/general-settings-section/__tests__/general-settings-section.test.js
+++ client/settings/general-settings-section/__tests__/general-settings-section.test.js
@@ -159,7 +159,7 @@ describe( 'GeneralSettingsSection', () => {
 		expect( updateEnabledMethodsMock ).not.toHaveBeenCalled();
 		expect( alipayCheckbox ).not.toBeChecked();
 
-		userEvent.click( sofortCheckbox );
+		userEvent.click( alipayCheckbox );
 
 		expect( updateEnabledMethodsMock ).toHaveBeenCalledWith( [
 			'card',
