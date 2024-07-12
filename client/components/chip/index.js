2d74c44bb108f3b4b27bfd6299aeca1374b5d784
James Allan <james.allan@automattic.com>
2024-07-12 10:23:43 +1000

    Fix order of params

--- client/components/chip/index.js
+++ client/components/chip/index.js
@@ -6,13 +6,14 @@ import './styles.scss';
  * The Chip component.
  *
  * @param {Object} props              The component props.
- * @param {string} props.color        The color of the chip. Can be 'gray', 'green', 'blue', 'red', 'yellow'.
  * @param {string} props.text         The text of the chip.
+ * @param {string} props.color        The color of the chip. Can be 'gray', 'green', 'blue', 'red', 'yellow'.
  * @param {JSX.Element} props.icon    Optional icon for the chip.
  * @param {string} props.iconPosition The position of the icon. Default is 'right'. Can be 'left' or 'right'.
- * @return {JSX.Element}              The rendered Chip component.
+ *
+ * @return {JSX.Element} The rendered Chip component.
  */
-const Chip = ( { text, icon, color = 'gray', iconPosition = 'right' } ) => {
+const Chip = ( { text, color = 'gray', icon, iconPosition = 'right' } ) => {
 	return (
 		<span
 			className={ classNames(
