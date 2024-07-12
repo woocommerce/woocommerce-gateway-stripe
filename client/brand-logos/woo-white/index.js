6ba299099f6f6de837a1a009a6cd580c4474c323
James Allan <james.allan@automattic.com>
2024-07-12 10:17:06 +1000

    Add comments and allow more classnames to stripemark and woologo

--- client/brand-logos/woo-white/index.js
+++ client/brand-logos/woo-white/index.js
@@ -1,14 +1,27 @@
 import { __ } from '@wordpress/i18n';
+import classNames from 'classnames';
 import logo from './logo.svg';
 
-const WooLogo = ( props ) => (
+/**
+ * WooLogo component.
+ *
+ * @param {Object} props             The component props.
+ * @param {string} [props.className] Additional classes to add to the WooLogo component.
+ * @param {any} props.restProps      Additional props to add to the WooLogo component.
+ *
+ * @return {JSX.Element} The rendered WooLogo component.
+ */
+const WooLogo = ( { className, ...restProps } ) => (
 	<img
-		className="woocommerce-stripe-woo-white-logo"
+		className={ classNames(
+			'woocommerce-stripe-woo-white-logo',
+			className
+		) }
 		src={ logo }
 		width="64"
 		height="64"
 		alt={ __( 'Woo logo', 'woocommerce-gateway-stripe' ) }
-		{ ...props }
+		{ ...restProps }
 	/>
 );
 
