361a22e841f666db3d4bea5cc9f3b8e61ce720a6
Wesley Rosa <wesleyjrosa@gmail.com>
2024-07-18 18:23:19 -0300

    Fix webhook button text

--- client/components/webhook-information/index.js
+++ client/components/webhook-information/index.js
@@ -2,27 +2,25 @@ import { __ } from '@wordpress/i18n';
 import { ExternalLink } from '@wordpress/components';
 import styled from '@emotion/styled';
 import interpolateComponents from 'interpolate-components';
-import { useAccount } from 'wcstripe/data/account';
 
-const WebhookEndpointText = styled.strong`
+const WebhookButtonText = styled.strong`
 	padding: 0 2px;
 	background-color: #f6f7f7; // $studio-gray-0
 `;
 
 export const WebhookInformation = () => {
-	const { data } = useAccount();
 	return (
 		<p data-testid="webhook-information">
 			{ interpolateComponents( {
 				mixedString: __(
-					'Click the Edit account keys button to {{settingsLink}}configure a webhook{{/settingsLink}}. This will complete your Stripe account connection process.',
+					'Click the {{webhookUrl/}} button to {{settingsLink}}configure a webhook{{/settingsLink}}. This will complete your Stripe account connection process.',
 					'woocommerce-gateway-stripe'
 				),
 				components: {
 					webhookUrl: (
-						<WebhookEndpointText>
-							{ data.webhook_url }
-						</WebhookEndpointText>
+						<WebhookButtonText>
+							Configure connection
+						</WebhookButtonText>
 					),
 					settingsLink: (
 						<ExternalLink href="https://woocommerce.com/document/stripe/setup-and-configuration/stripe-webhooks/" />
