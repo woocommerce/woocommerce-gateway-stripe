094a771570e95ad8454d0736e6104fc7cba33b69
James Allan <james.allan@automattic.com>
2024-07-25 17:12:00 +1000

    Introduce new ajax function to handle confirmed 3ds cards in a change payment method context

--- includes/class-wc-stripe-intent-controller.php
+++ includes/class-wc-stripe-intent-controller.php
@@ -34,6 +34,8 @@ class WC_Stripe_Intent_Controller {
 
 		add_action( 'wc_ajax_wc_stripe_update_order_status', [ $this, 'update_order_status_ajax' ] );
 		add_action( 'wc_ajax_wc_stripe_update_failed_order', [ $this, 'update_failed_order_ajax' ] );
+
+		add_action( 'wc_ajax_wc_stripe_confirm_change_payment', [ $this, 'confirm_change_payment_from_setup_intent_ajax' ] );
 	}
 
 	/**
@@ -563,22 +565,6 @@ class WC_Stripe_Intent_Controller {
 				throw new WC_Stripe_Exception( 'order_not_found', __( "We're not able to process this payment. Please try again later.", 'woocommerce-gateway-stripe' ) );
 			}
 
-			// If the order is a subscription, then this is the change payment method flow and it should be handled differently.
-			if ( function_exists( 'wcs_is_subscription' ) && wcs_is_subscription( $order->get_id() ) ) {
-				$subscription    = $order;
-				$setup_intent_id = isset( $_POST['intent_id'] ) ? wc_clean( wp_unslash( $_POST['intent_id'] ) ) : null;
-
-				$gateway = $this->get_upe_gateway();
-				$gateway->create_token_from_setup_intent( $setup_intent_id, $subscription->get_user() );
-
-				wp_send_json_success(
-					[
-						'return_url' => $subscription->get_view_order_url(),
-					],
-					200
-				);
-			}
-
 			$intent_id          = WC_Stripe_Helper::get_intent_id_from_order( $order );
 			$intent_id_received = isset( $_POST['intent_id'] ) ? wc_clean( wp_unslash( $_POST['intent_id'] ) ) : null;
 			if ( empty( $intent_id_received ) || $intent_id_received !== $intent_id ) {
@@ -1054,7 +1040,57 @@ class WC_Stripe_Intent_Controller {
 			wp_send_json_error(
 				[
 					'error' => [
-						'message' => $e->getLocalizedMessage(),
+						'message' => $e->getLocalizesdMessage(),
+					],
+				]
+			);
+		}
+	}
+
+	/**
+	 * Confirms the change payment method request for a subscription.
+	 *
+	 * This creates the payment method token from the setup intent.
+	 *
+	 * This function is used to confirm the change payment method request for a subscription after the user has been asked to authenticate their payment (eg 3D-Secure).
+	 * It is initiated from the subscription change payment method page.
+	 */
+	public function confirm_change_payment_from_setup_intent_ajax() {
+		try {
+			$is_nonce_valid = check_ajax_referer( 'wc_stripe_update_order_status_nonce', false, false );
+
+			if ( ! $is_nonce_valid ) {
+				throw new WC_Stripe_Exception( 'missing-nonce', __( 'CSRF verification failed.', 'woocommerce-gateway-stripe' ) );
+			}
+
+			if ( ! function_exists( 'wcs_is_subscription' ) ) {
+				throw new WC_Stripe_Exception( 'subscriptions_not_found', __( "We're not able to process this subscription change payment request payment. Please try again later.", 'woocommerce-gateway-stripe' ) );
+			}
+
+			$subscription_id = isset( $_POST['order_id'] ) ? absint( $_POST['order_id'] ) : false;
+			$subscription    = wcs_get_subscription( $subscription_id );
+
+			if ( ! $subscription ) {
+				throw new WC_Stripe_Exception( 'subscription_not_found', __( "We're not able to process this subscription change payment request payment. Please try again later.", 'woocommerce-gateway-stripe' ) );
+			}
+
+			$setup_intent_id = isset( $_POST['intent_id'] ) ? wc_clean( wp_unslash( $_POST['intent_id'] ) ) : null;
+
+			$gateway = $this->get_upe_gateway();
+			$gateway->create_token_from_setup_intent( $setup_intent_id, $subscription->get_user() );
+
+			wp_send_json_success(
+				[
+					'return_url' => $subscription->get_view_order_url(),
+				],
+				200
+			);
+		} catch ( WC_Stripe_Exception $e ) {
+			WC_Stripe_Logger::log( 'Change subscription payment method error: ' . $e->getMessage() );
+			wp_send_json_error(
+				[
+					'error' => [
+						'message' => $e->getMessage(),
 					],
 				]
 			);
