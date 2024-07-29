ebcd3b3604638c20673f6d59678159a0f780b86a
James Allan <james.allan@automattic.com>
2024-07-29 10:47:11 +1000

    Fall back to selected payment type if payment types aren't provided

--- includes/class-wc-stripe-intent-controller.php
+++ includes/class-wc-stripe-intent-controller.php
@@ -956,7 +956,7 @@ class WC_Stripe_Intent_Controller {
 	public function create_and_confirm_setup_intent( $payment_information ) {
 		$request = [
 			'payment_method'       => $payment_information['payment_method'],
-			'payment_method_types' => $payment_information['payment_method_types'],
+			'payment_method_types' => $payment_information['payment_method_types'] ?? [ $payment_information['selected_payment_type'] ],
 			'customer'             => $payment_information['customer'],
 			'confirm'              => 'true',
 			'return_url'           => $payment_information['return_url'],
