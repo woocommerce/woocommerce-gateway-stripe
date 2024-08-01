75c31b7b90e16e3fec57a196dfa554cd94a7a71a
Danae Millan <danae.millan@automattic.com>
2024-08-01 10:46:48 -0600

    Check the conditionals for logging in a separate method

--- includes/class-wc-stripe-logger.php
+++ includes/class-wc-stripe-logger.php
@@ -21,41 +21,56 @@ class WC_Stripe_Logger {
 	 * @version 4.0.0
 	 */
 	public static function log( $message, $start_time = null, $end_time = null ) {
-		if ( ! class_exists( 'WC_Logger' ) ) {
+		if ( ! self::can_log() ) {
 			return;
 		}
 
-		if ( apply_filters( 'wc_stripe_logging', true, $message ) ) {
-			if ( empty( self::$logger ) ) {
-				self::$logger = wc_get_logger();
-			}
-
-			$settings = get_option( 'woocommerce_stripe_settings' );
-
-			if ( empty( $settings ) || isset( $settings['logging'] ) && 'yes' !== $settings['logging'] ) {
-				return;
-			}
-
-			if ( ! is_null( $start_time ) ) {
-
-				$formatted_start_time = date_i18n( get_option( 'date_format' ) . ' g:ia', $start_time );
-				$end_time             = is_null( $end_time ) ? current_time( 'timestamp' ) : $end_time;
-				$formatted_end_time   = date_i18n( get_option( 'date_format' ) . ' g:ia', $end_time );
-				$elapsed_time         = round( abs( $end_time - $start_time ) / 60, 2 );
-
-				$log_entry  = "\n" . '====Stripe Version: ' . WC_STRIPE_VERSION . '====' . "\n";
-				$log_entry .= '====Stripe Plugin API Version: ' . WC_Stripe_API::STRIPE_API_VERSION . '====' . "\n";
-				$log_entry .= '====Start Log ' . $formatted_start_time . '====' . "\n" . $message . "\n";
-				$log_entry .= '====End Log ' . $formatted_end_time . ' (' . $elapsed_time . ')====' . "\n\n";
-
-			} else {
-				$log_entry  = "\n" . '====Stripe Version: ' . WC_STRIPE_VERSION . '====' . "\n";
-				$log_entry .= '====Stripe Plugin API Version: ' . WC_Stripe_API::STRIPE_API_VERSION . '====' . "\n";
-				$log_entry .= '====Start Log====' . "\n" . $message . "\n" . '====End Log====' . "\n\n";
-
-			}
-
-			self::$logger->debug( $log_entry, [ 'source' => self::WC_LOG_FILENAME ] );
+		if ( empty( self::$logger ) ) {
+			self::$logger = wc_get_logger();
 		}
+
+		if ( ! is_null( $start_time ) ) {
+
+			$formatted_start_time = date_i18n( get_option( 'date_format' ) . ' g:ia', $start_time );
+			$end_time             = is_null( $end_time ) ? current_time( 'timestamp' ) : $end_time;
+			$formatted_end_time   = date_i18n( get_option( 'date_format' ) . ' g:ia', $end_time );
+			$elapsed_time         = round( abs( $end_time - $start_time ) / 60, 2 );
+
+			$log_entry  = "\n" . '====Stripe Version: ' . WC_STRIPE_VERSION . '====' . "\n";
+			$log_entry .= '====Stripe Plugin API Version: ' . WC_Stripe_API::STRIPE_API_VERSION . '====' . "\n";
+			$log_entry .= '====Start Log ' . $formatted_start_time . '====' . "\n" . $message . "\n";
+			$log_entry .= '====End Log ' . $formatted_end_time . ' (' . $elapsed_time . ')====' . "\n\n";
+
+		} else {
+			$log_entry  = "\n" . '====Stripe Version: ' . WC_STRIPE_VERSION . '====' . "\n";
+			$log_entry .= '====Stripe Plugin API Version: ' . WC_Stripe_API::STRIPE_API_VERSION . '====' . "\n";
+			$log_entry .= '====Start Log====' . "\n" . $message . "\n" . '====End Log====' . "\n\n";
+
+		}
+
+		self::$logger->debug( $log_entry, [ 'source' => self::WC_LOG_FILENAME ] );
+	}
+
+	/**
+	 * Whether we can log based on settings and filters.
+	 *
+	 * @return boolean
+	 */
+	private static function can_log(): bool {
+		if ( ! class_exists( 'WC_Logger' ) ) {
+			return false;
+		}
+
+		if ( ! apply_filters( 'wc_stripe_logging', true, $message ) ) {
+			return false;
+		}
+
+		$settings = get_option( 'woocommerce_stripe_settings' );
+
+		if ( empty( $settings ) || isset( $settings['logging'] ) && 'yes' !== $settings['logging'] ) {
+			return false;
+		}
+
+		return true;
 	}
 }
