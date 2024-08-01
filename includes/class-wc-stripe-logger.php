62d6fdc5122ed75344eec43ba21356309e1d7315
Danae Millan <danae.millan@automattic.com>
2024-08-01 10:50:22 -0600

    Share the same variable for entries for both timestamped and not timestamped logs

--- includes/class-wc-stripe-logger.php
+++ includes/class-wc-stripe-logger.php
@@ -29,23 +29,20 @@ class WC_Stripe_Logger {
 			self::$logger = wc_get_logger();
 		}
 
-		if ( ! is_null( $start_time ) ) {
+		$log_entry  = "\n" . '====Stripe Version: ' . WC_STRIPE_VERSION . '====' . "\n";
+		$log_entry .= '====Stripe Plugin API Version: ' . WC_Stripe_API::STRIPE_API_VERSION . '====' . "\n";
 
+		if ( ! is_null( $start_time ) ) {
 			$formatted_start_time = date_i18n( get_option( 'date_format' ) . ' g:ia', $start_time );
 			$end_time             = is_null( $end_time ) ? current_time( 'timestamp' ) : $end_time;
 			$formatted_end_time   = date_i18n( get_option( 'date_format' ) . ' g:ia', $end_time );
 			$elapsed_time         = round( abs( $end_time - $start_time ) / 60, 2 );
 
-			$log_entry  = "\n" . '====Stripe Version: ' . WC_STRIPE_VERSION . '====' . "\n";
-			$log_entry .= '====Stripe Plugin API Version: ' . WC_Stripe_API::STRIPE_API_VERSION . '====' . "\n";
 			$log_entry .= '====Start Log ' . $formatted_start_time . '====' . "\n" . $message . "\n";
 			$log_entry .= '====End Log ' . $formatted_end_time . ' (' . $elapsed_time . ')====' . "\n\n";
 
 		} else {
-			$log_entry  = "\n" . '====Stripe Version: ' . WC_STRIPE_VERSION . '====' . "\n";
-			$log_entry .= '====Stripe Plugin API Version: ' . WC_Stripe_API::STRIPE_API_VERSION . '====' . "\n";
 			$log_entry .= '====Start Log====' . "\n" . $message . "\n" . '====End Log====' . "\n\n";
-
 		}
 
 		self::$logger->debug( $log_entry, [ 'source' => self::WC_LOG_FILENAME ] );
