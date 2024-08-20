8129e64a1c397e0c964b5c661d2dc5e024bfaa8c
root <root@pop-os.localdomain>
2024-08-20 14:26:42 -0500

    revert "wait for wc_stripe_upe_params before registering payment methods"

--- client/blocks/upe/index.js
+++ client/blocks/upe/index.js
@@ -22,87 +22,70 @@ const api = new WCStripeAPI(
 	}
 );
 
-const registerPaymentMethods = () => {
-	const upeMethods = getPaymentMethodsConstants();
-	Object.entries( getBlocksConfiguration()?.paymentMethodsConfig )
-		.filter( ( [ upeName ] ) => upeName !== 'link' )
-		.filter( ( [ upeName ] ) => upeName !== 'giropay' ) // Skip giropay as it was deprecated by Jun, 30th 2024.
-		.forEach( ( [ upeName, upeConfig ] ) => {
-			let iconName = upeName;
+const upeMethods = getPaymentMethodsConstants();
+Object.entries( getBlocksConfiguration()?.paymentMethodsConfig )
+	.filter( ( [ upeName ] ) => upeName !== 'link' )
+	.filter( ( [ upeName ] ) => upeName !== 'giropay' ) // Skip giropay as it was deprecated by Jun, 30th 2024.
+	.forEach( ( [ upeName, upeConfig ] ) => {
+		let iconName = upeName;
 
-			// Afterpay/Clearpay have different icons for UK merchants.
-			if ( upeName === 'afterpay_clearpay' ) {
-				iconName =
-					getBlocksConfiguration()?.accountCountry === 'GB'
-						? 'clearpay'
-						: 'afterpay';
-			}
+		// Afterpay/Clearpay have different icons for UK merchants.
+		if ( upeName === 'afterpay_clearpay' ) {
+			iconName =
+				getBlocksConfiguration()?.accountCountry === 'GB'
+					? 'clearpay'
+					: 'afterpay';
+		}
 
-			const Icon = Icons[ iconName ];
+		const Icon = Icons[ iconName ];
 
-			registerPaymentMethod( {
-				name: upeMethods[ upeName ],
-				content: getDeferredIntentCreationUPEFields(
-					upeName,
-					upeMethods,
-					api,
-					upeConfig.description,
-					upeConfig.testingInstructions,
-					upeConfig.showSaveOption ?? false
-				),
-				edit: getDeferredIntentCreationUPEFields(
-					upeName,
-					upeMethods,
-					api,
-					upeConfig.description,
-					upeConfig.testingInstructions,
-					upeConfig.showSaveOption ?? false
-				),
-				savedTokenComponent: <SavedTokenHandler api={ api } />,
-				canMakePayment: ( cartData ) => {
-					const billingCountry = cartData.billingAddress.country;
-					const isRestrictedInAnyCountry = !! upeConfig.countries.length;
-					const isAvailableInTheCountry =
-						! isRestrictedInAnyCountry ||
-						upeConfig.countries.includes( billingCountry );
+		registerPaymentMethod( {
+			name: upeMethods[ upeName ],
+			content: getDeferredIntentCreationUPEFields(
+				upeName,
+				upeMethods,
+				api,
+				upeConfig.description,
+				upeConfig.testingInstructions,
+				upeConfig.showSaveOption ?? false
+			),
+			edit: getDeferredIntentCreationUPEFields(
+				upeName,
+				upeMethods,
+				api,
+				upeConfig.description,
+				upeConfig.testingInstructions,
+				upeConfig.showSaveOption ?? false
+			),
+			savedTokenComponent: <SavedTokenHandler api={ api } />,
+			canMakePayment: ( cartData ) => {
+				const billingCountry = cartData.billingAddress.country;
+				const isRestrictedInAnyCountry = !! upeConfig.countries.length;
+				const isAvailableInTheCountry =
+					! isRestrictedInAnyCountry ||
+					upeConfig.countries.includes( billingCountry );
 
-					return isAvailableInTheCountry && !! api.getStripe();
-				},
-				// see .wc-block-checkout__payment-method styles in blocks/style.scss
-				label: (
-					<>
-						<span>
-							{ upeConfig.title }
-							<Icon alt={ upeConfig.title } />
-						</span>
-					</>
-				),
-				ariaLabel: 'Stripe',
-				supports: {
-					// Use `false` as fallback values in case server provided configuration is missing.
-					showSavedCards:
-						getBlocksConfiguration()?.showSavedCards ?? false,
-					showSaveOption: upeConfig.showSaveOption ?? false,
-					features: getBlocksConfiguration()?.supports ?? [],
-				},
-			} );
+				return isAvailableInTheCountry && !! api.getStripe();
+			},
+			// see .wc-block-checkout__payment-method styles in blocks/style.scss
+			label: (
+				<>
+					<span>
+						{ upeConfig.title }
+						<Icon alt={ upeConfig.title } />
+					</span>
+				</>
+			),
+			ariaLabel: 'Stripe',
+			supports: {
+				// Use `false` as fallback values in case server provided configuration is missing.
+				showSavedCards:
+					getBlocksConfiguration()?.showSavedCards ?? false,
+				showSaveOption: upeConfig.showSaveOption ?? false,
+				features: getBlocksConfiguration()?.supports ?? [],
+			},
 		} );
-};
-
-// If this script is running in the customizer preview pane, wait for wc_stripe_upe_params to be defined on the window before registering payment methods
-if ( wp.customize ) {
-	const upeParamsReady = new Promise( ( resolve ) => {
-		const interval = setInterval( () => {
-			if ( window.wc_stripe_upe_params !== undefined ) {
-				clearInterval( interval );
-				resolve();
-			}
-		}, 10 );
 	} );
-	upeParamsReady.then( registerPaymentMethods );
-} else {
-	registerPaymentMethods();
-}
 
 // Register Stripe Payment Request.
 registerExpressPaymentMethod( paymentRequestPaymentMethod );
