3cd9a465d0abc14f1cebfc46d1f39187888e0388
Wesley Rosa <wesleyjrosa@gmail.com>
2024-09-19 19:30:00 -0300

    Adding specific unit tests

--- tests/phpunit/test-wc-stripe-webhook-handler.php
+++ tests/phpunit/test-wc-stripe-webhook-handler.php
@@ -21,10 +21,10 @@ class WC_Stripe_Webhook_Handler_Test extends WP_UnitTestCase {
 	 * Mock card payment intent template.
 	 */
 	const MOCK_PAYMENT_INTENT = [
-		'id'                 => 'pi_mock',
-		'object'             => 'payment_intent',
-		'status'             => 'succeeded',
-		'charges'            => [
+		'id'      => 'pi_mock',
+		'object'  => 'payment_intent',
+		'status'  => 'succeeded',
+		'charges' => [
 			'total_count' => 1,
 			'data'        => [
 				[
@@ -96,7 +96,7 @@ class WC_Stripe_Webhook_Handler_Test extends WP_UnitTestCase {
 		$this->mock_webhook_handler->process_deferred_webhook( 'payment_intent.succeeded', $data );
 
 		// No payment intent.
-		$order = WC_Helper_Order::create_order();
+		$order            = WC_Helper_Order::create_order();
 		$data['order_id'] = $order->get_id();
 
 		$this->expectExceptionMessage( "Missing required data. 'intent_id' is missing for the deferred 'payment_intent.succeeded' event." );
@@ -110,7 +110,7 @@ class WC_Stripe_Webhook_Handler_Test extends WP_UnitTestCase {
 		$order     = WC_Helper_Order::create_order();
 		$intent_id = 'pi_mock_1234';
 		$data      = [
-			'order_id' => $order->get_id(),
+			'order_id'  => $order->get_id(),
 			'intent_id' => $intent_id,
 		];
 
@@ -134,7 +134,7 @@ class WC_Stripe_Webhook_Handler_Test extends WP_UnitTestCase {
 	public function test_mismatch_intent_id_process_deferred_webhook() {
 		$order = WC_Helper_Order::create_order();
 		$data  = [
-			'order_id' => $order->get_id(),
+			'order_id'  => $order->get_id(),
 			'intent_id' => 'pi_wrong_id',
 		];
 
@@ -168,7 +168,7 @@ class WC_Stripe_Webhook_Handler_Test extends WP_UnitTestCase {
 	public function test_process_of_successful_payment_intent_deferred_webhook() {
 		$order = WC_Helper_Order::create_order();
 		$data  = [
-			'order_id' => $order->get_id(),
+			'order_id'  => $order->get_id(),
 			'intent_id' => self::MOCK_PAYMENT_INTENT['id'],
 		];
 
@@ -197,5 +197,92 @@ class WC_Stripe_Webhook_Handler_Test extends WP_UnitTestCase {
 			);
 
 		$this->mock_webhook_handler->process_deferred_webhook( 'payment_intent.succeeded', $data );
+	}
+
+	/**
+	 * Test for `process_webhook_dispute_closed`
+	 *
+	 * @param string $charge_id       Charge ID.
+	 * @param string $dispute_status  Dispute status.
+	 * @param array  $expected_metas   Expected order metas.
+	 * @param string $expected_status Expected order status.
+	 * @return void
+	 * @dataProvider provide_test_process_webhook_dispute_closed
+	 * @throws WC_Data_Exception When order creation fails.
+	 */
+	public function test_process_webhook_dispute_closed( $charge_id, $dispute_status, $expected_metas, $expected_status ) {
+		$order = WC_Helper_Order::create_order();
+		$order->set_transaction_id( $charge_id );
+		$order->update_meta_data( '_stripe_status_before_hold', 'completed' );
+		$order->save();
+
+		$notification = (object) [
+			'data' => (object) [
+				'object' => (object) [
+					'charge' => $charge_id,
+					'status' => $dispute_status,
+				],
+			],
+		];
+
+		$this->mock_webhook_handler->process_webhook_dispute_closed( $notification );
+
+		// Reload the order.
+		$order = wc_get_order( $order->get_id() );
+
+		foreach ( $expected_metas as $meta_key => $meta_value ) {
+			$this->assertSame( $meta_value, get_post_meta( $order->get_id(), $meta_key, true ) );
+		}
+
+		$this->assertSame( $expected_status, $order->get_status() );
+	}
+
+	/**
+	 * Provider for `test_process_webhook_dispute_closed`
+	 *
+	 * @return array
+	 */
+	public function provide_test_process_webhook_dispute_closed() {
+		return [
+			'order not found' => [
+				'charge id'       => '',
+				'dispute status'  => '',
+				'expected metas'  => [],
+				'expected status' => 'pending',
+			],
+			'dispute lost'    => [
+				'charge id'       => '123',
+				'dispute status'  => 'lost',
+				'expected metas'  => [
+					'_stripe_status_final'   => '1',
+					'_dispute_closed_status' => 'lost',
+				],
+				'expected status' => 'failed',
+			],
+			'dispute won'     => [
+				'charge id'       => '123',
+				'dispute status'  => 'won',
+				'expected metas'  => [
+					'_stripe_status_final'   => '1',
+					'_dispute_closed_status' => 'won',
+				],
+				'expected status' => 'completed',
+			],
+			'inquiry closed'  => [
+				'charge id'       => '123',
+				'dispute status'  => 'warning_closed',
+				'expected metas'  => [
+					'_stripe_status_final'   => '1',
+					'_dispute_closed_status' => 'warning_closed',
+				],
+				'expected status' => 'completed',
+			],
+			'unknown status'  => [
+				'charge id'       => '123',
+				'dispute status'  => 'unknown',
+				'expected metas'  => [],
+				'expected status' => 'pending',
+			],
+		];
 	}
 }
