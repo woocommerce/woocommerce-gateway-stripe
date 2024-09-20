94b9c639aaa3fb510a377bef9b54c85e38705104
Wesley Rosa <wesleyjrosa@gmail.com>
2024-09-20 12:32:15 -0300

    Reverting unnecessary changes

--- tests/js/jest-setup.js
+++ tests/js/jest-setup.js
@@ -18,7 +18,6 @@ beforeEach( () => {
 
 afterEach( () => {
 	global.__PAYMENT_METHOD_FEES_ENABLED = false;
-	global.wc_stripe_express_checkout_params = {};
 	function cleanup() {
 		jest.clearAllTimers();
 		nock.cleanAll();
