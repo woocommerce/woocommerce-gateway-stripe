656d284c82ee4ca3446450f0c357e8c0e144428f
Wesley Rosa <wesleyjrosa@gmail.com>
2024-10-18 13:02:34 -0300

    Blocking enablement of APMs and adding a specific deprecation pill

---
+++ client/components/payment-method-deprecation-pill/index.js
@@ -0,0 +1,56 @@
+import { __ } from '@wordpress/i18n';
+import React from 'react';
+import styled from '@emotion/styled';
+import { Icon, info } from '@wordpress/icons';
+import Pill from 'wcstripe/components/pill';
+import Popover from 'wcstripe/components/popover';
+
+const StyledPill = styled( Pill )`
+	display: inline-flex;
+	align-items: center;
+	gap: 4px;
+	padding: 4px 8px;
+	border: 1px solid #fcf9e8;
+	border-radius: 2px;
+	background-color: #fcf9e8;
+	color: #674600;
+	font-size: 12px;
+	font-weight: 400;
+	line-height: 16px;
+	width: fit-content;
+`;
+
+const IconWrapper = styled.span`
+	height: 16px;
+	cursor: pointer;
+`;
+
+const AlertIcon = styled( Icon )`
+	fill: #674600;
+`;
+
+const IconComponent = ( { children, ...props } ) => (
+	<IconWrapper { ...props }>
+		<AlertIcon icon={ info } size="16" />
+		{ children }
+	</IconWrapper>
+);
+
+const PaymentMethodDeprecationPill = () => {
+	return (
+		<>
+			<StyledPill>
+				{ __( 'Deprecated', 'woocommerce-gateway-stripe' ) }
+
+				<Popover
+					BaseComponent={ IconComponent }
+					content={ __(
+						'This payment method is deprecated on the legacy checkout as of Oct, 31st 2024.'
+					) }
+				/>
+			</StyledPill>
+		</>
+	);
+};
+
+export default PaymentMethodDeprecationPill;
--- client/settings/general-settings-section/payment-methods-list.js
+++ client/settings/general-settings-section/payment-methods-list.js
@@ -288,16 +288,14 @@ const GeneralSettingsSection = ( {
 					return null;
 				}
 
-				// Remove APMs (legacy checkout) due deprecation by Stripe on Oct 31st, 2024.
-				if ( areAPMsDeprecated() && method !== 'card' ) {
-					return null;
-				}
-
 				const {
 					Icon,
 					label,
 					allows_manual_capture: isAllowingManualCapture,
 				} = PaymentMethodsMap[ method ];
+
+				// Remove APMs (legacy checkout) due deprecation by Stripe on Oct 31st, 2024.
+				const deprecated = areAPMsDeprecated() && method !== 'card';
 
 				return (
 					<div key={ method }>
@@ -316,6 +314,7 @@ const GeneralSettingsSection = ( {
 								isAllowingManualCapture={
 									isAllowingManualCapture
 								}
+								disabled={ deprecated }
 							/>
 							<PaymentMethodWrapper>
 								<PaymentMethodDescription
@@ -326,6 +325,7 @@ const GeneralSettingsSection = ( {
 										data.account?.default_currency
 									) }
 									label={ label }
+									deprecated={ deprecated }
 								/>
 								<StyledFees id={ method } />
 							</PaymentMethodWrapper>
