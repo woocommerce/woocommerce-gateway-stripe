2f4074a9937dfe45264ba957a51d0a156d83f67f
Wesley Rosa <wesleyjrosa@gmail.com>
2024-10-21 19:59:05 -0300

    Update includes/class-wc-stripe-helper.php
    Co-authored-by: Diego Curbelo <diego@curbelo.com>

--- includes/class-wc-stripe-helper.php
+++ includes/class-wc-stripe-helper.php
@@ -1612,6 +1612,6 @@ class WC_Stripe_Helper {
 	 * @return bool Whether the APMs are deprecated.
 	 */
 	public static function are_apms_deprecated() {
-		return ( new \DateTime() )->format( 'Y-m-d' ) > '2024-10-31' && ! WC_Stripe_Feature_Flags::is_upe_checkout_enabled();
+		return ( new \DateTime() )->format( 'Y-m-d' ) > '2024-10-29' && ! WC_Stripe_Feature_Flags::is_upe_checkout_enabled();
 	}
 }
