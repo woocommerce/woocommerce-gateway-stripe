83bf37e3f98d214855b1b6be1888456a1bbcd3d8
James Allan <james.allan@automattic.com>
2024-10-24 16:11:54 +1000

    Update express payment button previews

---
+++ client/blocks/express-checkout/apple-pay-preview/index.js
@@ -0,0 +1,10 @@
+import icon from '../../../payment-method-icons/apple-pay/white-icon.svg';
+import './style.scss';
+
+const ApplePayPreview = () => (
+	<div className="apple-pay-preview">
+		<img src={ icon } alt="" />
+	</div>
+);
+
+export default ApplePayPreview;
---
+++ client/blocks/express-checkout/google-pay-preview/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+import icon from '../../../payment-method-icons/google-pay/white-icon.svg';
+import './style.scss';
+
+const GooglePayPreview = () => (
+	<div className="google-pay-preview">
+		<img src={ icon } alt="" />
+	</div>
+);
+
+export default GooglePayPreview;
