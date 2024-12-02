5a61b18aea2a7a84aef4279a6075932eb478228f
Wesley Rosa <wesleyjrosa@gmail.com>
2024-12-02 13:06:57 -0300

    Fix custom product type price calculation

--- includes/payment-methods/class-wc-stripe-payment-request.php
+++ includes/payment-methods/class-wc-stripe-payment-request.php
@@ -1458,9 +1458,7 @@ class WC_Stripe_Payment_Request {
 			$variation_id = $data_store->find_matching_product_variation( $product, $attributes );
 
 			WC()->cart->add_to_cart( $product->get_id(), $qty, $variation_id, $attributes );
-		}
-
-		if ( in_array( $product_type, [ 'simple', 'variation', 'subscription', 'subscription_variation' ], true ) ) {
+		} else {
 			WC()->cart->add_to_cart( $product->get_id(), $qty );
 		}
 
