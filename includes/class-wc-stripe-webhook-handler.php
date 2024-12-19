0d66f934eda6925edef936e490cefd3249c5838d
nbloomf <nathan.bloomfield@automattic.com>
2024-12-19 11:06:06 -0600

    Lint fixes

--- includes/class-wc-stripe-webhook-handler.php
+++ includes/class-wc-stripe-webhook-handler.php
@@ -1200,7 +1200,6 @@ class WC_Stripe_Webhook_Handler extends WC_Stripe_Payment_Gateway {
 
 		do_action( 'wc_gateway_stripe_process_payment', $charge, $order );
 		$this->process_response( $charge, $order );
-
 	}
 
 	/**
