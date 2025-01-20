b6531d74f257bacb36a11a3d9970a2b6acc6458a
hsingyuc <hsingyuc.7@gmail.com>
2025-01-20 10:10:38 -0500

    Add Amazon Pay hook

--- client/settings/payment-request-section/index.js
+++ client/settings/payment-request-section/index.js
@@ -230,7 +230,6 @@ const PaymentRequestSection = () => {
 							</div>
 						</li>
 					) }
-<<<<<<< HEAD
 					{ isECEEnabled && (
 						<li className="express-checkout has-icon-border">
 							<div className="express-checkout__checkbox">
@@ -241,18 +240,6 @@ const PaymentRequestSection = () => {
 									) }
 									checked={ isAmazonPayEnabled }
 									onChange={ updateIsAmazonPayEnabled }
-=======
-					{ displayLinkPaymentMethod && (
-						<li className="express-checkout has-icon-border">
-							<div className="express-checkout__checkbox loadable-checkbox label-hidden">
-								<CheckboxControl
-									label={ __(
-										'Amazon Pay',
-										'woocommerce-gateway-stripe'
-									) }
-									checked={ isStripeLinkEnabled }
-									onChange={ updateStripeLinkCheckout }
->>>>>>> a40d9103 (Add the short blurb)
 								/>
 							</div>
 							<div className="express-checkout__icon">
@@ -296,11 +283,7 @@ const PaymentRequestSection = () => {
 									}
 								</div>
 							</div>
-<<<<<<< HEAD
 							<div className="express-checkout__amazon">
-=======
-							<div className="express-checkout__link">
->>>>>>> a40d9103 (Add the short blurb)
 								{ __(
 									'Customize',
 									'woocommerce-gateway-stripe'
