56dce4caf14fd0fa2b5dc2c8a9b7702596b88621
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2025-01-21 10:31:26 -0300

    Rename frontend flag

--- includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
@@ -414,7 +414,7 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 		$stripe_params['isECEEnabled'] = WC_Stripe_Feature_Flags::is_stripe_ece_enabled();
 
 		// ACH LPM Feature flag.
-		$stripe_params['isACHEnabled'] = WC_Stripe_Feature_Flags::is_ach_lpm_enabled();
+		$stripe_params['is_ach_enabled'] = WC_Stripe_Feature_Flags::is_ach_lpm_enabled();
 
 		$cart_total = ( WC()->cart ? WC()->cart->get_total( '' ) : 0 );
 		$currency   = get_woocommerce_currency();
