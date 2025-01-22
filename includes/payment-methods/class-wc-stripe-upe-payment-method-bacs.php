b6329b6efd4ce3157a950af763d9287a6c9e54f0
Alfredo Sumaran <a.sumaran@gmail.com>
2025-01-22 16:45:52 -0500

    Add doc blocks and remove unnecessary code

--- includes/payment-methods/class-wc-stripe-upe-payment-method-bacs.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-method-bacs.php
@@ -1,14 +1,10 @@
 <?php
-/**
- * Class UPE Cash App Pay Method
- *
- * @package WooCommerce_Stripe\Payment_Methods
- */
-
-defined( 'ABSPATH' ) || exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
- * The Cash App Pay Payment Method class extending UPE base class.
+ * The Bacs Direct Debit Payment Method class extending UPE base class.
  */
 class WC_Stripe_UPE_Payment_Method_Bacs extends WC_Stripe_UPE_Payment_Method {
 	/**
@@ -17,7 +13,7 @@ class WC_Stripe_UPE_Payment_Method_Bacs extends WC_Stripe_UPE_Payment_Method {
 	const STRIPE_ID = WC_Stripe_Payment_Methods::BACS_DEBIT;
 
 	/**
-	 * Constructor for Cash App payment method.
+	 * Constructor for Bacs Direct Debit payment method.
 	 */
 	public function __construct() {
 		parent::__construct();
@@ -32,14 +28,20 @@ class WC_Stripe_UPE_Payment_Method_Bacs extends WC_Stripe_UPE_Payment_Method {
 		$this->description                  = __( 'Bacs Direct Debit enables customers in the UK to pay by providing their bank account details.', 'woocommerce-gateway-stripe' );
 	}
 
+	/**
+	 * Determines if the Stripe Account country supports Bacs Direct Debit.
+	 *
+	 * @return bool
+	 */
 	public function is_available_for_account_country() {
 		return in_array( WC_Stripe::get_instance()->account->get_account_country(), $this->supported_countries, true );
 	}
 
-	public function get_retrievable_type() {
-		return $this->get_id();
-	}
-
+	/**
+	 * Returns true if Bacs Direct Debit is available for processing payments.
+	 *
+	 * @return bool
+	 */
 	public function is_enabled_at_checkout( $order_id = null, $account_domestic_currency = null ) {
 		if ( ! WC_Stripe_Feature_Flags::is_bacs_lpm_enabled() ) {
 			return false;
