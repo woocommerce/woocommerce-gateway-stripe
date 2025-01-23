2c5dcd045825ae54e20799c20e90a2c34b234a57
Anne Mirasol <anne.mirasol@automattic.com>
2025-01-23 16:07:16 -0600

    Add Google Pay to confirmation token flow for testing only

--- client/express-checkout/utils/index.js
+++ client/express-checkout/utils/index.js
@@ -349,5 +349,11 @@ export const isManualPaymentMethodCreation = ( expressPaymentType ) => {
 		return false;
 	}
 
+	// TODO: Uncomment me to test Google Pay on confirmation token flow.
+	// Remove me before merging to production.
+	// if ( [ 'googlePay', 'google_pay' ].includes( expressPaymentType ) ) {
+	// 	return false;
+	// }
+
 	return true;
 };
