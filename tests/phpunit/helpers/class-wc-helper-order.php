a6d732e552273225f38242837f7d4da46fd5e680
Alfredo Sumaran <a.sumaran@gmail.com>
2025-01-29 21:08:31 -0500

    Fix unit tests

--- tests/phpunit/helpers/class-wc-helper-order.php
+++ tests/phpunit/helpers/class-wc-helper-order.php
@@ -107,7 +107,7 @@ class WC_Helper_Order {
 
 		// Set payment gateway.
 		$payment_gateways = WC()->payment_gateways->payment_gateways();
-		$order->set_payment_method( $payment_gateways['bacs_debit'] );
+		$order->set_payment_method( $payment_gateways['bacs'] );
 
 		// Set totals.
 		$order->set_shipping_total( 10 );
