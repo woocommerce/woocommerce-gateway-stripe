bc723bc7a75a80d38cead9ccf2e23ec65b076426
Wesley Rosa <wesleyjrosa@gmail.com>
2025-01-30 11:06:19 -0300

    Fix rebase

--- client/settings/payment-request-section/index.js
+++ client/settings/payment-request-section/index.js
@@ -77,8 +77,6 @@ const PaymentRequestSection = () => {
 
 	const isECEEnabled = wc_stripe_settings_params.is_ece_enabled; // eslint-disable-line camelcase
 
-	const isECEEnabled = wc_stripe_settings_params.is_ece_enabled; // eslint-disable-line camelcase
-
 	return (
 		<Card className="express-checkouts">
 			<CardBody size={ 0 }>
