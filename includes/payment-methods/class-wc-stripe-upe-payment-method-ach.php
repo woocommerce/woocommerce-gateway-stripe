c23ed231c0b94647049228ca604d18f631501d1c
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2025-02-03 15:16:44 -0300

    Add method to create PM token for user.

--- includes/payment-methods/class-wc-stripe-upe-payment-method-ach.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-method-ach.php
@@ -24,10 +24,11 @@ class WC_Stripe_UPE_Payment_Method_ACH extends WC_Stripe_UPE_Payment_Method {
 		$this->stripe_id            = self::STRIPE_ID;
 		$this->title                = __( 'ACH Direct Debit', 'woocommerce-gateway-stripe' );
 		$this->is_reusable          = true;
-		$this->supported_currencies = [ 'USD' ];
-		$this->supported_countries  = [ 'US' ];
 		$this->label                = __( 'ACH Direct Debit', 'woocommerce-gateway-stripe' );
 		$this->description          = __( 'Pay directly from your US bank account via ACH.', 'woocommerce-gateway-stripe' );
+		$this->supported_currencies = [ WC_Stripe_Currency_Code::UNITED_STATES_DOLLAR ];
+		$this->supported_countries  = [ 'US' ];
+		$this->supports[]           = 'tokenization';
 	}
 
 	/**
@@ -46,4 +47,27 @@ class WC_Stripe_UPE_Payment_Method_ACH extends WC_Stripe_UPE_Payment_Method {
 	public function get_retrievable_type() {
 		return $this->get_id();
 	}
+
+	/**
+	 * Creates an ACH payment token for the customer.
+	 *
+	 * @param int      $user_id        The customer ID the payment token is associated with.
+	 * @param stdClass $payment_method The payment method object.
+	 *
+	 * @return WC_Payment_Token_ACH The payment token created.
+	 */
+	public function create_payment_token_for_user( $user_id, $payment_method ) {
+		$payment_token = new WC_Payment_Token_ACH();
+		$payment_token->set_gateway_id( $this->get_id() );
+		$payment_token->set_user_id( $user_id );
+		$payment_token->set_token( $payment_method->id );
+		$payment_token->set_last4( $payment_method->last4 );
+		$payment_token->set_bank_name( $payment_method->bank_name );
+		$payment_token->set_account_type( $payment_method->account_holder_type );
+		$payment_token->set_fingerprint( $payment_method->fingerprint );
+		$payment_token->save();
+
+		return $payment_token;
+	}
+
 }
