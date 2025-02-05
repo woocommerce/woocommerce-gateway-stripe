7fda5788a720c8b97ee1f999b18d51abf03ebad1
Malith Senaweera <malith.senaweera@automattic.com>
2025-02-05 15:17:26 -0600

    Move loading indicator into the primary button

--- client/settings/stripe-auth-account/stripe-auth-actions.js
+++ client/settings/stripe-auth-account/stripe-auth-actions.js
@@ -1,7 +1,7 @@
 /* global wc_stripe_settings_params, ajaxurl */
 import { __ } from '@wordpress/i18n';
 import { React, useState, useEffect } from 'react';
-import { Button, ExternalLink, Spinner } from '@wordpress/components';
+import { Button, ExternalLink } from '@wordpress/components';
 import interpolateComponents from 'interpolate-components';
 import ConfigureWebhookButton from './configure-webhook-button';
 import InlineNotice from 'wcstripe/components/inline-notice';
@@ -61,56 +61,41 @@ const StripeAuthActions = ( { testMode, displayWebhookConfigure } ) => {
 		fetchOAuthUrls();
 	}, [] );
 
-	if ( isLoading ) {
-		return <Spinner />;
-	}
-
-	if ( error ) {
-		return (
-			<InlineNotice isDismissible={ false } status="error">
-				{ error }
-			</InlineNotice>
-		);
-	}
-
 	const oauthUrl = testMode ? oauthUrls.test_oauth_url : oauthUrls.oauth_url;
+	const buttonText = testMode
+		? __( 'Create or connect a test account', 'woocommerce-gateway-stripe' )
+		: __( 'Create or connect an account', 'woocommerce-gateway-stripe' );
 
-	return oauthUrl ? (
+	return (
 		<div className="woocommerce-stripe-auth__actions">
-			<Button
-				variant="primary"
-				href={ oauthUrl }
-				text={
-					testMode
-						? __(
-								'Create or connect a test account',
-								'woocommerce-gateway-stripe'
-						  )
-						: __(
-								'Create or connect an account',
-								'woocommerce-gateway-stripe'
-						  )
-				}
-			/>
+			{ error ? (
+				<InlineNotice isDismissible={ false } status="error">
+					{ interpolateComponents( {
+						mixedString: __(
+							'An issue occurred generating a connection to Stripe, please ensure your server has a valid SSL certificate and try again.{{br /}}For assistance, refer to our {{Link}}documentation{{/Link}}.',
+							'woocommerce-gateway-stripe'
+						),
+						components: {
+							br: <br />,
+							Link: (
+								<ExternalLink href="https://woocommerce.com/document/stripe/setup-and-configuration/connecting-to-stripe/" />
+							),
+						},
+					} ) }
+				</InlineNotice>
+			) : (
+				<Button
+					variant="primary"
+					href={ oauthUrl }
+					text={ buttonText }
+					disabled={ isLoading }
+					isBusy={ isLoading }
+				/>
+			) }
 			{ displayWebhookConfigure && (
 				<ConfigureWebhookButton testMode={ testMode } />
 			) }
 		</div>
-	) : (
-		<InlineNotice isDismissible={ false } status="error">
-			{ interpolateComponents( {
-				mixedString: __(
-					'An issue occurred generating a connection to Stripe, please ensure your server has a valid SSL certificate and try again.{{br /}}For assistance, refer to our {{Link}}documentation{{/Link}}.',
-					'woocommerce-gateway-stripe'
-				),
-				components: {
-					br: <br />,
-					Link: (
-						<ExternalLink href="https://woocommerce.com/document/stripe/setup-and-configuration/connecting-to-stripe/" />
-					),
-				},
-			} ) }
-		</InlineNotice>
 	);
 };
 
