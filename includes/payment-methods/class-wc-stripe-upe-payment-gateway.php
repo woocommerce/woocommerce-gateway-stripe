5964a5c49664580f7d734c29765a5d547cbb2ca2
Wesley Rosa <wesleyjrosa@gmail.com>
2025-02-05 18:50:16 -0300

    Hiding actions for pending Amazon Pay orders

--- includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
@@ -246,6 +246,9 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 		// Add hooks for clearing appearance transients when theme is updated
 		add_action( 'customize_save_after', [ $this, 'clear_appearance_transients' ] );
 		add_action( 'save_post', [ $this, 'clear_appearance_transients_block_theme' ], 10, 2 );
+
+		// Hide action buttons for pending Amazon Pay orders (as they take a while to be confirmed).
+		add_filter( 'woocommerce_my_account_my_orders_actions', [ $this, 'filter_my_account_my_orders_actions' ], 10, 2 );
 	}
 
 	/**
@@ -2873,5 +2876,19 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 	 */
 	public function clear_appearance_transients() {
 		delete_transient( $this->get_appearance_transient_key() );
+	}
+
+	/**
+	 * Hide "Pay" and "Cancel" action buttons for pending Amazon Pay orders (as they take a while to be confirmed).
+	 *
+	 * @param $actions array An array with the default actions.
+	 * @param $order WC_Order The order.
+	 * @return array
+	 */
+	public function filter_my_account_my_orders_actions( $actions, $order ) {
+		if ( $order->get_payment_method_title() === 'Amazon Pay (Stripe)' && $order->has_status( 'pending' ) ) {
+			unset( $actions['pay'], $actions['cancel'] );
+		}
+		return $actions;
 	}
 }
