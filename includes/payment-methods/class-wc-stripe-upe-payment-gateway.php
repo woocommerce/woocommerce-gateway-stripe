0a476ddc8139fde98453310bdd2e389160d0499e
Peter Wilson <git-commit-by@peterwilson.cc>
2025-02-07 11:42:26 +1100

    UPE GW: Init forced tokenization.

--- includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
@@ -236,6 +236,9 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 		// Check if pre-orders are enabled and add support for them.
 		$this->maybe_init_pre_orders();
 
+		// Check if forced tokenization is enabled and add support for it.
+		$this->maybe_init_forced_tokenization();
+
 		$this->title                         = $this->payment_methods['card']->get_title();
 		$this->description                   = $this->payment_methods['card']->get_description();
 		$this->enabled                       = $this->get_option( 'enabled' );
