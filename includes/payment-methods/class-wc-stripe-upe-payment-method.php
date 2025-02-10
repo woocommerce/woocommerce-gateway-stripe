789862e1c8766536af5e7ac74e637fe5d8692d08
Peter Wilson <git-commit-by@peterwilson.cc>
2025-02-10 12:56:04 +1100

    Set tokens to reusable if the order requires a token.

--- includes/payment-methods/class-wc-stripe-upe-payment-method.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-method.php
@@ -273,6 +273,11 @@ abstract class WC_Stripe_UPE_Payment_Method extends WC_Payment_Gateway {
 			return $this->is_reusable();
 		}
 
+		// If cart or order contains a product that requires a payment token, enable payment method if it's reusable.
+		if ( $this->cart_requires_order_payment_token() || ( ! empty( $order_id ) && $this->order_requires_order_payment_token( $order_id ) ) ) {
+			return $this->is_reusable();
+		}
+
 		// Note: this $this->is_automatic_capture_enabled() call will be handled by $this->__call() and fall through to the UPE gateway class.
 		if ( $this->requires_automatic_capture() && ! $this->is_automatic_capture_enabled() ) {
 			return false;
