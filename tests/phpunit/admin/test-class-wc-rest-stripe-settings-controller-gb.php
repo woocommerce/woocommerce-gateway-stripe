5de2c17c6a73ce2182e5c9881a90b0d7a97605bb
Alfredo Sumaran <a.sumaran@gmail.com>
2025-02-11 00:43:30 -0500

    Fix tests for WC_REST_Stripe_Settings_Controller_Test_GB

--- tests/phpunit/admin/test-class-wc-rest-stripe-settings-controller-gb.php
+++ tests/phpunit/admin/test-class-wc-rest-stripe-settings-controller-gb.php
@@ -24,30 +24,6 @@ class WC_REST_Stripe_Settings_Controller_Test_GB extends WP_UnitTestCase {
 	private static $gateway;
 
 	/**
-	 * Enable UPE and store gateway instance.
-	 *
-	 * We are doing this here because if we did it in set_up(), the method body would get called before every single test
-	 * however the REST controller is instantiated only once. If we reloaded gateways then, WC()->payment_gateways()
-	 * would contain another gateway instance than the controller.
-	 *
-	 * @see UPE_Test_Utils::reload_payment_gateways()
-	 */
-	public static function set_up_before_class() {
-		parent::set_up_before_class();
-
-		$upe_helper = new UPE_Test_Helper();
-
-		// Enable Bacs for tests.
-		update_option( WC_Stripe_Feature_Flags::LPM_BACS_FEATURE_FLAG_NAME, 'yes' );
-
-		// All tests assume UPE is enabled.
-		update_option( '_wcstripe_feature_upe', 'yes' );
-		$upe_helper->enable_upe();
-		$upe_helper->reload_payment_gateways();
-		self::$gateway = WC()->payment_gateways()->payment_gateways()[ WC_Gateway_Stripe::ID ];
-	}
-
-	/**
 	 * Pre-test setup
 	 */
 	public function set_up() {
@@ -57,32 +33,22 @@ class WC_REST_Stripe_Settings_Controller_Test_GB extends WP_UnitTestCase {
 			$this->markTestSkipped( 'The controller is not compatible with older WC versions, due to the missing `update_option` method on the gateway.' );
 		}
 
+		// Enable Bacs for tests.
+		update_option( WC_Stripe_Feature_Flags::LPM_BACS_FEATURE_FLAG_NAME, 'yes' );
+
+		// All tests assume UPE feature is enabled.
+		update_option( '_wcstripe_feature_upe', 'yes' );
+
 		// Set the user so that we can pass the authentication.
 		wp_set_current_user( 1 );
 
-		$stripe_settings                                    = WC_Stripe_Helper::get_stripe_settings();
-		$stripe_settings['enabled']                         = 'yes';
-		$stripe_settings['testmode']                        = 'yes';
-		$stripe_settings['test_publishable_key']            = 'pk_test_key';
-		$stripe_settings['test_secret_key']                 = 'sk_test_key';
-		$stripe_settings['country']                         = 'GB';
-		$stripe_settings['stripe_upe_payment_method_order'] = [
-			'card',
-			'alipay',
-			'klarna',
-			'afterpay_clearpay',
-			'eps',
-			'bancontact',
-			'boleto',
-			'ideal',
-			'oxxo',
-			'sepa_debit',
-			'p24',
-			'multibanco',
-			'link',
-			'wechat_pay',
-			'bacs_debit',
-		];
+		$stripe_settings                         = WC_Stripe_Helper::get_stripe_settings();
+		$stripe_settings['enabled']              = 'yes';
+		$stripe_settings['testmode']             = 'yes';
+		$stripe_settings['test_publishable_key'] = 'pk_test_key';
+		$stripe_settings['test_secret_key']      = 'sk_test_key';
+		$stripe_settings['country']              = 'GB';
+
 		WC_Stripe_Helper::update_main_stripe_settings( $stripe_settings );
 
 		$account = [
@@ -99,24 +65,58 @@ class WC_REST_Stripe_Settings_Controller_Test_GB extends WP_UnitTestCase {
 	}
 
 	public function test_get_settings_returns_available_payment_method_ids_for_gb() {
+		$expected_method_ids = [
+			'card',
+			'alipay',
+			'klarna',
+			'afterpay_clearpay',
+			'eps',
+			'bancontact',
+			'boleto',
+			'ideal',
+			'oxxo',
+			'sepa_debit',
+			'p24',
+			'multibanco',
+			'link',
+			'wechat_pay',
+			'bacs_debit',
+		];
+
 		$response             = $this->rest_get_settings();
-		$expected_method_ids  = array_keys( self::$gateway->payment_methods );
 		$available_method_ids = $response->get_data()['available_payment_method_ids'];
 
-		$this->assertEquals( $expected_method_ids, $available_method_ids );
+		$this->assertEquals(
+			$expected_method_ids,
+			$available_method_ids,
+		);
+
 		$this->assertContains( WC_Stripe_Payment_Methods::BACS_DEBIT, $available_method_ids );
 	}
 
 	public function test_get_settings_returns_ordered_payment_method_ids_for_gb() {
+		$expected_ordered_method_ids = [
+			'card',
+			'alipay',
+			'klarna',
+			'afterpay_clearpay',
+			'eps',
+			'bancontact',
+			'boleto',
+			'ideal',
+			'oxxo',
+			'sepa_debit',
+			'p24',
+			'multibanco',
+			'wechat_pay',
+			'bacs_debit',
+		];
+
 		$response = $this->rest_get_settings();
 
-		$expected_methods = $this->get_gateway()->payment_methods;
-		unset( $expected_methods['link'] );
-		$expected_method_ids = array_keys( $expected_methods );
-		$ordered_method_ids  = $response->get_data()['ordered_payment_method_ids'];
-
+		$ordered_method_ids = $response->get_data()['ordered_payment_method_ids'];
 		$this->assertEquals(
-			$expected_method_ids,
+			$expected_ordered_method_ids,
 			$ordered_method_ids
 		);
 		$this->assertContains( WC_Stripe_Payment_Methods::BACS_DEBIT, $ordered_method_ids );
