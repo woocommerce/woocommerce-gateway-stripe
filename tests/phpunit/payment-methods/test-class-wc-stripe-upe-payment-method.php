35eb728e15763e46b581ffd411e9ca2d6b103646
Alfredo Sumaran <a.sumaran@gmail.com>
2025-02-12 17:21:50 -0500

    Fix syntax

--- tests/phpunit/payment-methods/test-class-wc-stripe-upe-payment-method.php
+++ tests/phpunit/payment-methods/test-class-wc-stripe-upe-payment-method.php
@@ -625,7 +625,7 @@ class WC_Stripe_UPE_Payment_Method_Test extends WP_UnitTestCase {
 
 		foreach ( $this->mock_payment_methods as $payment_method_id => $payment_method ) {
 			$store_currency = 'EUR';
-			if ( in_array( $payment_method_id, [ WC_Stripe_UPE_Payment_Method_Link::STRIPE_ID, WC_Stripe_UPE_Payment_Method_ACH::STRIPE_ID ] ) {
+			if ( in_array( $payment_method_id, [ WC_Stripe_UPE_Payment_Method_Link::STRIPE_ID, WC_Stripe_UPE_Payment_Method_ACH::STRIPE_ID ] ) ) {
 				$store_currency = WC_Stripe_Currency_Code::UNITED_STATES_DOLLAR;
 			} elseif ( WC_Stripe_UPE_Payment_Method_Bacs_Debit::STRIPE_ID === $payment_method_id ) {
 				$store_currency = WC_Stripe_Currency_Code::POUND_STERLING;
