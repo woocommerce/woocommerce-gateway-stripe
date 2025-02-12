c1e362dcd8424a45cd0ade320edd8caf8be954ea
Josh Wood <josh@thewateringcan.ca>
2025-02-12 10:19:44 -0500

    Update get_order_from_intent to use charges->data

--- includes/class-wc-stripe-webhook-handler.php
+++ includes/class-wc-stripe-webhook-handler.php
@@ -1330,8 +1330,13 @@ class WC_Stripe_Webhook_Handler extends WC_Stripe_Payment_Gateway {
 		}
 
 		// Try to retrieve from the charges array.
-		if ( ! empty( $intent->charges ) && is_array( $intent->charges ) ) {
-			$charge   = $intent->charges[0] ?? [];
+		if ( ! empty( $intent->charges ) ) {
+			if ( is_array( $intent->charges ) ) {
+				$charge   = $intent->charges[0] ?? [];
+			}
+			else {
+				$charge   = $intent->charges->data[0] ?? [];
+			}
 			$order_id = $charge->metadata->order_id ?? null;
 			return $order_id ? wc_get_order( $order_id ) : false;
 		}
