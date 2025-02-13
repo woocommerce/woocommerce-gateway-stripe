b4432229420ce2e7fe7168f4208a187a8d3e8a8e
Wesley Rosa <wesleyjrosa@gmail.com>
2025-02-13 10:14:24 -0300

    Making use of the new retrieval method in the helper class

--- includes/compat/class-wc-stripe-subscriptions-helper.php
+++ includes/compat/class-wc-stripe-subscriptions-helper.php
@@ -8,6 +8,13 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class WC_Stripe_Subscriptions_Helper {
 	/**
+	 * Transient key for detached subscriptions.
+	 *
+	 * @var string
+	 */
+	private const DETACHED_SUBSCRIPTIONS_TRANSIENT_KEY = 'wcstripe_detached_subscriptions';
+
+	/**
 	 * Checks if subscriptions are enabled on the site.
 	 *
 	 * @return bool Whether subscriptions is enabled or not.
@@ -22,6 +29,12 @@ class WC_Stripe_Subscriptions_Helper {
 	 * @return array
 	 */
 	public static function get_detached_subscriptions() {
+		// Check if we have a cached result.
+		$cached_subscriptions = get_transient( self::DETACHED_SUBSCRIPTIONS_TRANSIENT_KEY );
+		if ( ! empty( $cached_subscriptions ) ) {
+			return $cached_subscriptions;
+		}
+
 		$detached_subscriptions = [];
 		$subscriptions          = wcs_get_subscriptions(
 			[
@@ -36,10 +49,18 @@ class WC_Stripe_Subscriptions_Helper {
 			if ( $source_id ) {
 				$payment_method = WC_Stripe_API::get_payment_method( $source_id );
 				if ( ! $payment_method->customer ) {
-					$detached_subscriptions[] = $subscription;
+					$detached_subscriptions[] = [
+						'id'                        => $subscription->get_id(),
+						'customer_id'               => $subscription->get_meta( '_stripe_customer_id' ),
+						'change_payment_method_url' => $subscription->get_change_payment_method_url(),
+					];
 				}
 			}
 		}
+
+		// Cache the result for a day.
+		set_transient( self::DETACHED_SUBSCRIPTIONS_TRANSIENT_KEY, $detached_subscriptions, DAY_IN_SECONDS );
+
 		return $detached_subscriptions;
 	}
 }
