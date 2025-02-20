54793fea99f95d1220b842fdcd9a37fc09894f4c
Malith Senaweera <malith.senaweera@automattic.com>
2025-02-20 10:00:38 -0600

    Remove dark logo switching logic

--- client/blocks/upe/checkout-icons.js
+++ client/blocks/upe/checkout-icons.js
@@ -1,61 +1,14 @@
-import { useEffect, useState } from 'react';
-import tinycolor from 'tinycolor2';
 import BaseIcon from '../../payment-method-icons/styles/base-icon';
 import { getStripeImageUrl } from '../utils';
-import { getBackgroundColor } from '../../styles/upe/utils';
-import { callWhenElementIsAvailable } from './call-when-element-is-available';
 
 /**
- * List of payment method icons that have dark variants
- */
-const PAYMENT_METHODS_WITH_DARK_ICONS = [ 'affirm', 'afterpay' ];
-
-/**
- * List of selectors to check for background color
- */
-const BACKGROUND_SELECTORS = [
-	'#payment-method .wc-block-components-radio-control-accordion-option',
-	'#payment-method',
-	'form.wc-block-checkout__form',
-	'.wc-block-checkout',
-	'body',
-];
-
-/**
- * Determines whether background color is light or dark.
- *
- * @param {string} color CSS color value.
- * @return {boolean} True, if background is light; false, if background is dark.
- */
-const isColorDark = ( color ) => {
-	return tinycolor( color ).isDark();
-};
-
-/**
- * Creates an icon component that switches between light/dark variants based on background
+ * Creates an icon component that.
  *
  * @param {string} iconName The base name of the icon file without extension
  * @return {Function} A React component that renders the appropriate icon
  */
 const createIconComponent = ( iconName ) => ( props ) => {
-	const [ isDark, setIsDark ] = useState( false );
-	const hasDarkVariant = PAYMENT_METHODS_WITH_DARK_ICONS.includes( iconName );
-
-	useEffect( () => {
-		const checkBackground = () => {
-			const bg = getBackgroundColor( BACKGROUND_SELECTORS );
-			setIsDark( isColorDark( bg ) );
-		};
-
-		// Wait for the payment method container to be available
-		callWhenElementIsAvailable( '#payment-method', checkBackground );
-	}, [] );
-
-	// Use dark variant only if it exists and background is dark
-	const shouldUseDarkVariant = isDark && hasDarkVariant;
-	const iconSrc = shouldUseDarkVariant
-		? getStripeImageUrl( `${ iconName }-dark` )
-		: getStripeImageUrl( iconName );
+	const iconSrc = getStripeImageUrl( iconName );
 
 	return <BaseIcon { ...props } size="medium" src={ iconSrc } />;
 };
