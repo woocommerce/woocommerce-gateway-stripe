6a20b2a6ee47a3b4f1a8f88a72059837ca4c303d
Wesley Rosa <wesleyjrosa@gmail.com>
2025-02-20 16:24:28 -0500

    Ignore deprecation warnings

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -5,6 +5,8 @@
  * @package WooCommerce\Stripe
  */
 
+error_reporting( E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED );
+
 require_once __DIR__ . '/../../vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
