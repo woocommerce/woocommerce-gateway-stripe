012600f12c302d9db4298ccad479345de89565c3
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2025-02-22 11:33:58 -0500

    Use ACH utils in blocks tests

--- tests/e2e/tests/checkout/blocks/lpm/ach.spec.js
+++ tests/e2e/tests/checkout/blocks/lpm/ach.spec.js
@@ -1,8 +1,15 @@
 import { test, expect } from '@playwright/test';
 import config from 'config';
 import { payments, api, user } from '../../../../utils';
+import { admin } from '../../../../utils';
 
-const { emptyCart, setupCart, setupBlocksCheckout } = payments;
+const {
+	emptyCart,
+	setupCart,
+	setupBlocksCheckout,
+	fillACHBankDetails,
+	setupACHCheckout,
+} = payments;
 
 test.describe( 'ACH payment tests @blocks', () => {
 	let username, userEmail;
@@ -29,18 +36,7 @@ test.describe( 'ACH payment tests @blocks', () => {
 				storageState: process.env.ADMINSTATE,
 			} );
 			const adminPage = await adminContext.newPage();
-
-			await adminPage.goto(
-				'/wp-admin/admin.php?page=wc-settings&tab=checkout&section=stripe&panel=methods'
-			);
-			await adminPage
-				.getByRole( 'checkbox', { name: 'ACH Direct Debit' } )
-				.check();
-			await adminPage.click( 'text=Save changes' );
-
-			await expect(
-				adminPage.getByText( 'Settings saved.' )
-			).toBeDefined();
+			await admin.toggleACHPaymentMethod( adminPage, true );
 			await adminContext.close();
 		} );
 	} );
@@ -51,71 +47,16 @@ test.describe( 'ACH payment tests @blocks', () => {
 				storageState: process.env.ADMINSTATE,
 			} );
 			const page = await adminContext.newPage();
-
-			await page.goto(
-				'/wp-admin/admin.php?page=wc-settings&tab=checkout&section=stripe&panel=methods'
-			);
-			await page
-				.getByRole( 'checkbox', { name: 'ACH Direct Debit' } )
-				.click();
-			await page.getByRole( 'button', { name: 'Remove' } ).click();
-			await page.click( 'text=Save changes' );
-
-			await expect( page.getByText( 'Settings saved.' ) ).toBeDefined();
+			await admin.toggleACHPaymentMethod( page, false );
 			await adminContext.close();
 		} );
 	} );
 
-	const fillBankDetails = async ( page ) => {
-		const frame = page
-			.frameLocator( 'iframe[name^="__privateStripeFrame"]' )
-			.first();
-
-		// Agree and Continue
-		await frame.getByTestId( 'agree-button' ).click();
-
-		// Click "Success ••••" button
-		await frame.getByRole( 'button', { name: 'Success ••••' } ).click();
-
-		// Click "Connect Account" button.
-		await frame.getByTestId( 'select-button' ).click();
-
-		// Skip link registration
-		await frame.getByTestId( 'link-not-now-button' ).click();
-
-		// Click "Done" button.
-		await frame.getByTestId( 'done-button' ).click();
-	};
-
-	const setupACHCheckout = async ( page ) => {
-		await emptyCart( page );
-		await setupCart( page );
-
-		await setupBlocksCheckout(
-			page,
-			config.get( 'addresses.customer.billing' )
-		);
-
-		// Select ACH payment method
-		await page
-			.locator( 'label' )
-			.filter( { hasText: 'ACH Direct Debit' } )
-			.click();
-
-		// Click "Test Institution"
-		await page
-			.frameLocator(
-				'#radio-control-wc-payment-method-options-stripe_us_bank_account__content iframe[src*="elements-inner-payment"]'
-			)
-			.getByText( 'Test Institution' )
-			.click();
-	};
-
 	test( 'customer can pay with ACH using valid bank details @smoke', async ( {
 		page,
 	} ) => {
-		await setupACHCheckout( page );
-		await fillBankDetails( page );
+		await setupACHCheckout( page, 'blocks' );
+		await fillACHBankDetails( page );
 		await page.locator( 'text=Place order' ).click();
 		await page.waitForURL( '**/checkout/order-received/**' );
 		await expect( page.locator( 'h1.entry-title' ) ).toHaveText(
@@ -132,11 +73,11 @@ test.describe( 'ACH payment tests @blocks', () => {
 				username,
 				config.get( 'users.customer.password' )
 			);
-			await setupACHCheckout( page );
+			await setupACHCheckout( page, 'blocks' );
 		} );
 
 		await test.step( 'Connect bank account', async () => {
-			await fillBankDetails( page );
+			await fillACHBankDetails( page );
 			await page
 				.locator(
 					'.wc-block-components-payment-methods__save-card-info'
