bd2e091717cc00c9aa55db1091ba2eaa4345e6fd
Hector Lovo <lovohh@gmail.com>
2025-02-25 14:55:15 -0500

    Add additional logging for Pre-Orders installation steps

--- tests/e2e/bin/setup.sh
+++ tests/e2e/bin/setup.sh
@@ -158,9 +158,12 @@ redirect_output curl -sLJ \
 	--output $E2E_ROOT/woocommerce-pre-orders.zip \
 	https://api.github.com/repos/woocommerce/woocommerce-pre-orders/releases/assets/"$LATEST_RELEASE_ASSET_ID"
 
+echo " - Installing"
 redirect_output cli wp plugin install /var/www/html/wp-content/plugins/woocommerce-gateway-stripe/tests/e2e/woocommerce-pre-orders.zip --force
+echo " - Removing lingering zip"
 rm -rf $E2E_ROOT/woocommerce-pre-orders.zip
 
+echo " - Activating"
 redirect_output cli wp plugin activate woocommerce-pre-orders
 
 echo
