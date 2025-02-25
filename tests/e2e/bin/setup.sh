8df527cd20b3c68e0ca8778482d51690ef22d97b
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2025-02-25 11:27:05 -0300

    Fix project reference in E2E commands

--- tests/e2e/bin/setup.sh
+++ tests/e2e/bin/setup.sh
@@ -34,7 +34,7 @@ if ! docker info > /dev/null 2>&1; then
 fi
 
 step "Starting E2E docker containers"
-CWD="$CWD" E2E_ROOT="$E2E_ROOT" redirect_output docker compose -p wcstripe-e2e -f "$E2E_ROOT"/env/docker-compose.yml up --build --force-recreate -d wordpress
+CWD="$CWD" E2E_ROOT="$E2E_ROOT" redirect_output docker compose -p wcstripe-e2e --env-file "$E2E_ROOT"/config/local.env -f "$E2E_ROOT"/env/docker-compose.yml up --build --force-recreate
 
 step "Configuring WordPress"
 # Wait for containers to be started up before setup.
