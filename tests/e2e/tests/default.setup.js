71c6c49e088466015e1b41026080d85a8798fe8d
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2025-02-25 11:26:22 -0300

    Revert change from e2e setup

--- tests/e2e/tests/default.setup.js
+++ tests/e2e/tests/default.setup.js
@@ -4,7 +4,7 @@
 
 import { expect, test as setup } from '@playwright/test';
 
-setup( 'Disable legacy checkout experience', async ( { browser } ) => {
+setup.skip( 'Disable legacy checkout experience', async ( { browser } ) => {
 	const adminContext = await browser.newContext( {
 		storageState: process.env.ADMINSTATE,
 	} );
@@ -24,7 +24,7 @@ setup( 'Disable legacy checkout experience', async ( { browser } ) => {
 	await adminContext.close();
 } );
 
-setup( 'enable Link', async ( { browser } ) => {
+setup.skip( 'enable Link', async ( { browser } ) => {
 	const adminContext = await browser.newContext( {
 		storageState: process.env.ADMINSTATE,
 	} );
