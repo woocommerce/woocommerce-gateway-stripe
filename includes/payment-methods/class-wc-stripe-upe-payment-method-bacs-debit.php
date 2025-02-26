1ffd47b6b99d9cc6b082fb73a439a3a8f8009c45
Alfredo Sumaran <a.sumaran@gmail.com>
2025-02-26 00:28:56 -0500

    Add subscriptions support to Bacs

--- includes/payment-methods/class-wc-stripe-upe-payment-method-bacs-debit.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-method-bacs-debit.php
@@ -28,6 +28,7 @@ class WC_Stripe_UPE_Payment_Method_Bacs_Debit extends WC_Stripe_UPE_Payment_Meth
 		$this->accept_only_domestic_payment = true;
 		$this->label                        = __( 'Bacs Direct Debit', 'woocommerce-gateway-stripe' );
 		$this->description                  = __( 'Bacs Direct Debit enables customers in the UK to pay by providing their bank account details.', 'woocommerce-gateway-stripe' );
+		$this->supports[]                   = 'subscriptions';
 
 		// Remove Bacs from the “Add Payment Method” page for now, as its implementation will be handled later.
 		if ( ! is_wc_endpoint_url( 'add-payment-method' ) ) {
