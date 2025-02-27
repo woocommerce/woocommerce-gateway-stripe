d83809168818aa2d304437e0e488372d4fd0be05
Hector Lovo <lovohh@gmail.com>
2025-02-27 12:42:07 -0500

    Only run Pre-Order E2E tests if the plugin is installed

--- tests/e2e/tests/checkout/blocks/pre-order-product.spec.js
+++ tests/e2e/tests/checkout/blocks/pre-order-product.spec.js
@@ -1,16 +1,25 @@
 import { test, expect } from '@playwright/test';
 import config from 'config';
 import { api, payments, products } from '../../../utils';
+import { isPluginInstalled } from '../../../utils/plugin-utils';
 
 const { setupBlocksCheckout, fillCreditCardDetails } = payments;
 
 let productId;
+
+test.skip( async () => {
+	return ! ( await isPluginInstalled( 'woocommerce-pre-orders' ) );
+}, 'Woo Pre-Orders plugin is not active. Skipping tests.' );
 
 test.beforeAll( async () => {
 	productId = await api.create.product( products.preOrderData() );
 } );
 
 test.afterAll( async () => {
+	if ( ! productId ) {
+		return;
+	}
+
 	await api.deletePost.product( productId );
 } );
 
--- tests/e2e/tests/checkout/shortcode/pre-order-product.spec.js
+++ tests/e2e/tests/checkout/shortcode/pre-order-product.spec.js
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import config from 'config';
 import { api, payments, products } from '../../../utils';
+import { isPluginInstalled } from '../../../utils/plugin-utils';
 
 const {
 	setupShortcodeCheckout,
@@ -9,11 +10,19 @@ const {
 
 let productId;
 
+test.skip( async () => {
+	return ! ( await isPluginInstalled( 'woocommerce-pre-orders' ) );
+}, 'Woo Pre-Orders plugin is not active. Skipping tests.' );
+
 test.beforeAll( async () => {
 	productId = await api.create.product( products.preOrderData() );
 } );
 
 test.afterAll( async () => {
+	if ( ! productId ) {
+		return;
+	}
+
 	await api.deletePost.product( productId );
 } );
 
