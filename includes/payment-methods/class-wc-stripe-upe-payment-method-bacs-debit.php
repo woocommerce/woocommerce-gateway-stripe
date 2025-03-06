bc91fbeb0104c1ca4e29d7cca906411f35536cf9
Alfredo Sumaran <a.sumaran@gmail.com>
2025-03-06 17:50:10 -0500

    Put reference in a variable before passing it to reset

--- includes/payment-methods/class-wc-stripe-upe-payment-method-bacs-debit.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-method-bacs-debit.php
@@ -113,7 +113,8 @@ class WC_Stripe_UPE_Payment_Method_Bacs_Debit extends WC_Stripe_UPE_Payment_Meth
 			function ( $available_gateways ) {
 				if ( is_checkout() && class_exists( 'WC_Pre_Orders_Cart' ) && WC_Pre_Orders_Cart::cart_contains_pre_order() ) {
 					// Iteration is unnecessary since only one pre-order product can be in the cart.
-					$product_id = reset( WC()->cart->get_cart() )['product_id'];
+					$cart = WC()->cart->get_cart();
+					$product_id = reset( $cart )['product_id'];
 					if ( WC_Pre_Orders_Product::product_is_charged_upon_release( $product_id ) ) {
 						unset( $available_gateways['stripe_bacs_debit'] );
 					}
