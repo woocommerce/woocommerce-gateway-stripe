398219894beb72a02ea3c974e38f5acae497ef59
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2025-03-12 12:17:19 -0300

    Make code php 7 compatible

--- includes/abstracts/abstract-wc-stripe-payment-gateway.php
+++ includes/abstracts/abstract-wc-stripe-payment-gateway.php
@@ -602,7 +602,11 @@ abstract class WC_Stripe_Payment_Gateway extends WC_Payment_Gateway_CC {
 
 			if ( 'failed' === $response->status ) {
 				$localized_message = __( 'Payment processing failed. Please retry.', 'woocommerce-gateway-stripe' );
-				$order->add_order_note( $response->outcome->seller_message ?? $localized_message );
+				$order->add_order_note(
+					property_exists( $response, 'outcome' ) && isset( $response->outcome->seller_message )
+						? $response->outcome->seller_message
+						: $localized_message
+				);
 
 				throw new WC_Stripe_Exception( $localized_message, $localized_message );
 			}
