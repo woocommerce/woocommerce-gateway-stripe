e29479512f4f4834de90a237ad1ab44d6ad494ba
Wesley Rosa <wesleyjrosa@gmail.com>
2025-03-13 09:39:19 -0300

    Putting back the legacy normalize methods

--- client/express-checkout/utils/normalize.js
+++ client/express-checkout/utils/normalize.js
@@ -1,4 +1,5 @@
 import { applyFilters } from '@wordpress/hooks';
+import { extractOrderAttributionData } from 'wcstripe/blocks/utils';
 
 /**
  * Normalizes incoming cart total items for use as a displayItems with the Stripe api.
@@ -86,6 +87,80 @@ export const normalizeOrderData = ( {
 			'wcstripe.express-checkout.cart-place-order-extension-data',
 			{}
 		),
+	};
+};
+
+/**
+ * Normalize order data from Stripe's object to the expected format for WC (legacy version, non-StoreAPI).
+ *
+ * @param {Object} event Stripe's event object.
+ * @param {string} paymentMethodId Stripe's payment method id.
+ *
+ * @return {Object} Order object in the format WooCommerce expects.
+ */
+export const normalizeOrderDataLegacy = ( event, paymentMethodId ) => {
+	const name = event?.billingDetails?.name;
+	const email = event?.billingDetails?.email ?? '';
+	const billing = event?.billingDetails?.address ?? {};
+	const shipping = event?.shippingAddress ?? {};
+
+	const phone =
+		event?.billingDetails?.phone?.replace( /[() -]/g, '' ) ??
+		event?.payerPhone?.replace( /[() -]/g, '' ) ??
+		'';
+
+	return {
+		billing_first_name:
+			name?.split( ' ' )?.slice( 0, 1 )?.join( ' ' ) ?? '',
+		billing_last_name: name?.split( ' ' )?.slice( 1 )?.join( ' ' ) ?? '-',
+		billing_company: billing?.organization ?? '',
+		billing_email: email ?? event?.payerEmail ?? '',
+		billing_phone: phone,
+		billing_country: billing?.country ?? '',
+		billing_address_1: billing?.line1 ?? '',
+		billing_address_2: billing?.line2 ?? '',
+		billing_city: billing?.city ?? '',
+		billing_state: billing?.state ?? '',
+		billing_postcode: billing?.postal_code ?? '',
+		shipping_first_name:
+			shipping?.name?.split( ' ' )?.slice( 0, 1 )?.join( ' ' ) ?? '',
+		shipping_last_name:
+			shipping?.name?.split( ' ' )?.slice( 1 )?.join( ' ' ) ?? '',
+		shipping_company: shipping?.organization ?? '',
+		shipping_phone: phone,
+		shipping_country: shipping?.address?.country ?? '',
+		shipping_address_1: shipping?.address?.line1 ?? '',
+		shipping_address_2: shipping?.address?.line2 ?? '',
+		shipping_city: shipping?.address?.city ?? '',
+		shipping_state: shipping?.address?.state ?? '',
+		shipping_postcode: shipping?.address?.postal_code ?? '',
+		shipping_method: [ event?.shippingRate?.id ?? null ],
+		order_comments: '',
+		payment_method: 'stripe',
+		ship_to_different_address: 1,
+		terms: 1,
+		'wc-stripe-payment-method': paymentMethodId,
+		express_checkout_type: event?.expressPaymentType,
+		express_payment_type: event?.expressPaymentType,
+		'wc-stripe-is-deferred-intent': true,
+		...extractOrderAttributionData(),
+	};
+};
+
+/**
+ * Normalize Pay for Order data from Stripe's object to the expected format for WC (legacy version, non-StoreAPI).
+ *
+ * @param {Object} event Stripe's event object.
+ * @param {string} paymentMethodId Stripe's payment method id.
+ *
+ * @return {Object} Order object in the format WooCommerce expects.
+ */
+export const normalizePayForOrderDataLegacy = ( event, paymentMethodId ) => {
+	return {
+		payment_method: 'stripe',
+		'wc-stripe-is-deferred-intent': true, // Set the deferred intent flag, so the deferred intent flow is used.
+		'wc-stripe-payment-method': paymentMethodId,
+		express_payment_type: event?.expressPaymentType,
 	};
 };
 
