73a19b8a4d668cf9c1b1d490323207f968889fef
Wesley Rosa <wesleyjrosa@gmail.com>
2025-03-18 09:24:33 -0300

    Simplifying the style applycation logic

--- client/blocks/upe/apply-single-payment-element-styles.js
+++ client/blocks/upe/apply-single-payment-element-styles.js
@@ -1,16 +1,28 @@
 export const applySinglePaymentElementStyles = () => {
 	// Add the single payment element class to the Stripe payment method elements.
-	const stripePaymentMethodContent = document.getElementById(
-		'radio-control-wc-payment-method-options-stripe__content'
-	);
-	stripePaymentMethodContent.classList.add( 'single-payment-element' );
-	const stripePaymentMethodLabel = document.getElementById(
-		'radio-control-wc-payment-method-options-stripe__label'
-	);
-	stripePaymentMethodLabel.classList.add( 'single-payment-element' );
+	document
+		.getElementById(
+			'radio-control-wc-payment-method-options-stripe__content'
+		)
+		.classList.add( 'single-payment-element' );
+	document
+		.getElementById(
+			'radio-control-wc-payment-method-options-stripe__label'
+		)
+		.classList.add( 'single-payment-element' );
+
 	// Style the Stripe iframe to remove the margin.
-	const stripeIFrame = document.querySelector(
+	document.querySelector(
 		'.wcstripe-payment-element iframe'
+	).style.margin = 0;
+
+	// Hide the Stripe radio button when it is the only available payment method.
+	const paymentMethodOptions = document.querySelectorAll(
+		'.wc-payment-method-options__option'
 	);
-	stripeIFrame.style.margin = 0;
+	if ( paymentMethodOptions.length === 1 ) {
+		document.querySelector(
+			'.wc-payment-method-options__option input[type="radio"]'
+		).style.display = 'none';
+	}
 };
