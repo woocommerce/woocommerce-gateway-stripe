de0fa9236f1197f22700bf0f902d5657b3831939
Wesley Rosa <wesleyjrosa@gmail.com>
2025-03-18 09:32:32 -0300

    Hiding the Stripe label when it is the only option available

--- client/blocks/upe/apply-single-payment-element-styles.js
+++ client/blocks/upe/apply-single-payment-element-styles.js
@@ -1,4 +1,14 @@
 export const applySinglePaymentElementStyles = () => {
+	// Hide the Stripe radio button when it is the only available payment method.
+	const paymentMethodOptions = document.querySelectorAll(
+		'input[name=radio-control-wc-payment-method-options]'
+	);
+	if ( paymentMethodOptions.length === 1 ) {
+		document.querySelector(
+			'.wc-block-components-radio-control__option'
+		).style.display = 'none';
+	}
+
 	// Add the single payment element class to the Stripe payment method elements.
 	document
 		.getElementById(
@@ -15,14 +25,4 @@ export const applySinglePaymentElementStyles = () => {
 	document.querySelector(
 		'.wcstripe-payment-element iframe'
 	).style.margin = 0;
-
-	// Hide the Stripe radio button when it is the only available payment method.
-	const paymentMethodOptions = document.querySelectorAll(
-		'.wc-payment-method-options__option'
-	);
-	if ( paymentMethodOptions.length === 1 ) {
-		document.querySelector(
-			'.wc-payment-method-options__option input[type="radio"]'
-		).style.display = 'none';
-	}
 };
