7fa8def4175e40d2e16599a13ad0b6c1db3c84b0
Malith Senaweera <malith.senaweera@automattic.com>
2025-03-19 10:42:36 -0500

    Avoid retry_time redefinition in subject

--- includes/compat/class-wc-stripe-email-failed-authentication-retry.php
+++ includes/compat/class-wc-stripe-email-failed-authentication-retry.php
@@ -73,8 +73,7 @@ class WC_Stripe_Email_Failed_Authentication_Retry extends WC_Email_Failed_Order 
 	 */
 	public function get_default_subject() {
 		if ( apply_filters( 'woocommerce_is_email_preview', false ) ) {
-			$interval = 12 * HOUR_IN_SECONDS;
-			$retry_time = human_time_diff( time(), time() + ( $interval ) );
+			$retry_time = $this->get_retry_time();
 			return str_replace( '{retry_time}', $retry_time, $this->subject );
 		}
 		return $this->subject;
