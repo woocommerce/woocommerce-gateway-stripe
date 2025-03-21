dd043fb82c0e5a4704a417aea09f9e64b457d4bb
Wesley Rosa <wesleyjrosa@gmail.com>
2025-03-21 12:14:03 -0300

    Adding constant + renaming selector

--- client/blocks/upe/handle-display-of-saving-checkbox-for-spe.js
+++ client/blocks/upe/handle-display-of-saving-checkbox-for-spe.js
@@ -1,10 +1,10 @@
 import { NON_RECURRING_METHODS } from 'wcstripe/stripe-utils/constants';
 
 export const handleDisplayOfSavingCheckboxForSpe = ( method ) => {
-	const $saveCardInfoContainer = document.querySelector(
+	const saveCardInfoContainer = document.querySelector(
 		'.wc-block-components-payment-methods__save-card-info'
 	);
 	if ( NON_RECURRING_METHODS.includes( method ) ) {
-		$saveCardInfoContainer.style.display = 'none';
+		saveCardInfoContainer.style.display = 'none';
 	}
 };
