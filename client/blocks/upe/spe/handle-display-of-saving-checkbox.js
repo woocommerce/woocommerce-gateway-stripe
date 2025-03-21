059d02e0ba4f22d8cde7ae8dc87834217f73070a
Wesley Rosa <wesleyjrosa@gmail.com>
2025-03-21 17:20:05 -0300

    Renaming constant

--- client/blocks/upe/spe/handle-display-of-saving-checkbox.js
+++ client/blocks/upe/spe/handle-display-of-saving-checkbox.js
@@ -1,11 +1,11 @@
-import { NON_RECURRING_METHODS } from 'wcstripe/stripe-utils/constants';
+import { NON_REUSABLE_METHODS } from 'wcstripe/stripe-utils/constants';
 
 export const handleDisplayOfSavingCheckbox = ( method ) => {
 	const saveCardInfoContainer = document.querySelector(
 		'.wc-block-components-payment-methods__save-card-info'
 	);
 	if ( saveCardInfoContainer ) {
-		saveCardInfoContainer.style.display = NON_RECURRING_METHODS.includes(
+		saveCardInfoContainer.style.display = NON_REUSABLE_METHODS.includes(
 			method
 		)
 			? 'none'
