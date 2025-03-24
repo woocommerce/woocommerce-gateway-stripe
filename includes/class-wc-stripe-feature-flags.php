351e95f72343ba8adeb57009c02c406b4219f77f
César Costa <cesar.costa@automattic.com>
2025-03-24 13:24:41 -0300

    Add BLIK feature flag to dev tools mapping

--- includes/class-wc-stripe-feature-flags.php
+++ includes/class-wc-stripe-feature-flags.php
@@ -29,6 +29,7 @@ class WC_Stripe_Feature_Flags {
 		self::LPM_ACSS_FEATURE_FLAG_NAME       => 'no',
 		self::LPM_BACS_FEATURE_FLAG_NAME       => 'yes',
 		self::LPM_BECS_DEBIT_FEATURE_FLAG_NAME => 'no',
+		self::LPM_BLIK_FEATURE_FLAG_NAME       => 'no',
 	];
 
 	/**
