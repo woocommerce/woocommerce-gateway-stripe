e82be6fbe11db04d17fb894360ebdbd65d05443a
Wesley Rosa <wesleyjrosa@gmail.com>
2025-03-27 17:23:30 -0300

    Fix code coverage weorkflow

--- includes/class-wc-stripe-mode.php
+++ includes/class-wc-stripe-mode.php
@@ -26,4 +26,13 @@ class WC_Stripe_Mode {
 		$settings = WC_Stripe_Helper::get_stripe_settings();
 		return 'yes' === ( $settings['testmode'] ?? 'no' );
 	}
+
+	/**
+	 * Random method to test code coverage.
+	 *
+	 * @return true
+	 */
+	public function random_method() {
+		return true;
+	}
 }
