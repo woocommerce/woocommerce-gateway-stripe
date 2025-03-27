e8b8d081014118068c36a373c9caba5e1c1edfc9
Wesley Rosa <wesleyjrosa@gmail.com>
2025-03-27 18:18:46 -0300

    Reverse logic for checkbox display

--- client/classic/upe/spe/handle-display-of-saving-checkbox.js
+++ client/classic/upe/spe/handle-display-of-saving-checkbox.js
@@ -11,7 +11,7 @@ export const handleDisplayOfSavingCheckbox = ( method ) => {
 	const createAccountCheckbox = document.getElementById( 'createaccount' );
 	if (
 		( ! createAccountCheckbox || createAccountCheckbox?.checked ) &&
-		NON_REUSABLE_METHODS.includes( method )
+		! NON_REUSABLE_METHODS.includes( method )
 	) {
 		saveCardInfoContainer.style.display = 'block';
 	} else {
