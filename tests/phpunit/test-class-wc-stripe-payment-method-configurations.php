dcac28b1ac2d91aa217b7529ecb8d4378d4f0518
Wesley Rosa <wesleyjrosa@gmail.com>
2025-04-15 10:09:18 -0300

    Unit test

---
+++ tests/phpunit/test-class-wc-stripe-payment-method-configurations.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Class WC_Stripe_Payment_Method_Configurations tests.
+ */
+class WC_Stripe_Payment_Method_Configurations_Test extends WP_UnitTestCase {
+	/**
+	 * Tests for `get_parent_configuration_id`.
+	 *
+	 * @return void
+	 */
+	public function test_get_parent_configuration_id() {
+		$this->assertSame(
+			WC_Stripe_Payment_Method_Configurations::TEST_MODE_CONFIGURATION_PARENT_ID,
+			WC_Stripe_Payment_Method_Configurations::get_parent_configuration_id()
+		);
+	}
+}
