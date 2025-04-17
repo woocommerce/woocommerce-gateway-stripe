9c9597782fa842e03b561c89e80e97fa19bfba35
Dale du Preez <dale@automattic.com>
2025-04-17 15:40:05 +0200

    Fix the correct method signature

--- includes/abstracts/abstract-wc-stripe-payment-gateway.php
+++ includes/abstracts/abstract-wc-stripe-payment-gateway.php
@@ -33,7 +33,7 @@ abstract class WC_Stripe_Payment_Gateway extends WC_Payment_Gateway_CC {
 	 *
 	 * @return string[]
 	 */
-	public function get_upe_enabled_payment_method_ids() {
+	public function get_upe_enabled_payment_method_ids( $force_refresh = false ) {
 		return [ WC_Stripe_Payment_Methods::CARD ];
 	}
 
@@ -42,7 +42,7 @@ abstract class WC_Stripe_Payment_Gateway extends WC_Payment_Gateway_CC {
 	 *
 	 * @return string[]
 	 */
-	public function get_upe_available_payment_methods( $force_refresh = false ) {
+	public function get_upe_available_payment_methods() {
 		return [ WC_Stripe_Payment_Methods::CARD ];
 	}
 
