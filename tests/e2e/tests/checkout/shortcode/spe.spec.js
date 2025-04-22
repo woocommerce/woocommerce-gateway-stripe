80cc59b7217ef0bea411306a9da56998d04ca8c9
Anne Mirasol <anne.mirasol@automattic.com>
2025-04-22 11:29:46 -0500

    e2e: Use check() instead of click() for checkbox

--- tests/e2e/tests/checkout/shortcode/spe.spec.js
+++ tests/e2e/tests/checkout/shortcode/spe.spec.js
@@ -67,7 +67,7 @@ test.describe( 'SPE payment tests @shortcode', () => {
 					.getByRole( 'checkbox', {
 						name: 'Save payment information to',
 					} )
-					.click();
+					.check();
 				await page.locator( 'text=Place order' ).click();
 				await fillSPEDetails(
 					page,
