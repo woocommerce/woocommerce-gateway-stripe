8c334e6fd9dd502f585a581162b7af4953b5966c
Wesley Rosa <wesleyjrosa@gmail.com>
2025-04-29 22:33:11 -0300

    Fix tests

--- tests/phpunit/WC_Stripe_Helper_Test.php
+++ tests/phpunit/WC_Stripe_Helper_Test.php
@@ -394,10 +394,9 @@ class WC_Stripe_Helper_Test extends WP_UnitTestCase {
 	 * @return void
 	 */
 	public function test_handle_main_stripe_settings() {
-		$this->markTestSkipped( 'WIP' );
-
-		WC_Stripe_Helper::update_main_stripe_settings( [ 'test' => 'test' ] );
-		$current_settings = WC_Stripe_Helper::get_stripe_settings();
+		$current_settings         = WC_Stripe_Helper::get_stripe_settings();
+		$current_settings['test'] = 'test';
+		WC_Stripe_Helper::update_main_stripe_settings( $current_settings );
 		$this->assertSame( [ 'test' => 'test' ], $current_settings );
 
 		WC_Stripe_Helper::delete_main_stripe_settings();
