d683c5fec8bc8efdc756c1b97b1c485a190112f7
Malith Senaweera <malith.senaweera@automattic.com>
2025-05-07 14:28:19 -0500

    Yoda condition

--- includes/admin/class-wc-stripe-settings-controller.php
+++ includes/admin/class-wc-stripe-settings-controller.php
@@ -273,7 +273,7 @@ class WC_Stripe_Settings_Controller {
 	 */
 	public static function hide_gateways_on_settings_page() {
 		// Prevent hiding gateways in the new payments settings experience (React-based UI).
-		if ( get_option( 'woocommerce_feature_reactify-classic-payments-settings_enabled' ) === 'yes' ) {
+		if ( 'yes' === get_option( 'woocommerce_feature_reactify-classic-payments-settings_enabled' ) ) {
 			return;
 		}
 
