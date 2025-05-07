8f586b821da2e71c255391ee10f75e55c889cc5f
Mayisha <mayisha.alam@automattic.com>
2025-05-07 17:00:18 +0600

    add comments and refactor

--- includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
@@ -2637,25 +2637,28 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 			return false;
 		}
 
-		// Save it when 'wc_stripe_force_save_payment_method' filter is true
+		/**
+		 * Filters the flag that decides if the payment method should be saved.
+		 *
+		 * @since 9.6.0
+		 *
+		 * @param bool   $force_save Whether the payment method should be saved.
+		 * @param string $order_id   Order ID.
+		 *
+		 * @return bool Whether the payment method should be saved.
+		 */
 		$force_save_payment_method = apply_filters( 'wc_stripe_force_save_payment_method', false, $order_id );
 
 		// Backward compatibility for deprecated 'wc_stripe_force_save_source' filter.
-		if ( has_filter( 'wc_stripe_force_save_source' ) ) {
-			_doing_it_wrong(
-				'wc_stripe_force_save_source',
-				'The wc_stripe_force_save_source filter is deprecated since WooCommerce Stripe Gateway 9.0.0. Use wc_stripe_force_save_payment_method instead.',
-				'9.6.0'
-			);
-			$force_save_payment_method = apply_filters_deprecated(
-				'wc_stripe_force_save_source',
-				[ false, $order_id ],
-				'9.6.0',
-				'wc_stripe_force_save_payment_method',
-				'The wc_stripe_force_save_source filter is deprecated since WooCommerce Stripe Gateway 9.6.0. Use wc_stripe_force_save_payment_method instead.'
-			) || $force_save_payment_method;
-		}
+		$force_save_payment_method = apply_filters_deprecated(
+			'wc_stripe_force_save_source',
+			[ $force_save_payment_method, $order_id ],
+			'9.6.0',
+			'wc_stripe_force_save_payment_method',
+			'The wc_stripe_force_save_source filter is deprecated since WooCommerce Stripe Gateway 9.6.0. Use wc_stripe_force_save_payment_method instead.'
+		);
 
+		// Save the payment method when forced by the filter.
 		if ( $force_save_payment_method ) {
 			return true;
 		}
