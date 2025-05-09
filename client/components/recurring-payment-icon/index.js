1f345bd3f1ede6e9dad81bea8378003d881b65fc
Mayisha <mayisha.alam@automattic.com>
2025-05-09 15:30:07 +0600

    add popover

--- client/components/recurring-payment-icon/index.js
+++ client/components/recurring-payment-icon/index.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import { __ } from '@wordpress/i18n';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import { Popover as PopoverComponent } from '@wordpress/components';
 import icon from './icon.svg';
 
-const IconWrapper = styled.img`
+const Icon = styled.img`
 	height: 12px;
 	width: 12px;
+	cursor: pointer;
 `;
 
-const RecurringPaymentIcon = () => <IconWrapper src={ icon } alt="" />;
+const StyledPopover = styled( PopoverComponent )`
+	top: -11px !important;
+
+	.components-popover__content {
+		border-radius: 4px;
+		box-shadow: 0px 2px 6px 0px rgba( 0, 0, 0, 0.05 );
+		padding: 5px 10px;
+		background-color: #000000;
+		color: #ffffff;
+	}
+
+	@media ( min-width: 660px ) {
+		.components-popover__content {
+			width: auto;
+		}
+	}
+`;
+
+const IconComponent = ( { children, ...props } ) => (
+	<span { ...props }>
+		<Icon src={ icon } alt="" />
+		{ children }
+	</span>
+);
+
+const RecurringPaymentIcon = () => {
+	const [ isVisible, setIsVisible ] = useState( false );
+
+	const toggleVisible = () => {
+		setIsVisible( ( state ) => ! state );
+	};
+
+	return (
+		<IconComponent onClick={ toggleVisible }>
+			{ isVisible && (
+				<StyledPopover
+					animate={ true }
+					placement="top"
+					variant="toolbar"
+					onFocusOutside={ () => setIsVisible( false ) }
+				>
+					{ __(
+						'Supports recurring payments',
+						'woocommerce-gateway-stripe'
+					) }
+				</StyledPopover>
+			) }
+		</IconComponent>
+	);
+};
 
 export default RecurringPaymentIcon;
