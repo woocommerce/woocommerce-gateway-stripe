20cfc63fafc0fdb559a07a01a175f018031c58db
Malith Senaweera <malith.senaweera@automattic.com>
2025-05-16 13:15:18 -0500

    Set keys during env setup

--- tests/e2e/tests/blik.setup.js
+++ tests/e2e/tests/blik.setup.js
@@ -2,15 +2,12 @@ import { test as setup } from '@playwright/test';
 import { admin } from '../utils';
 import { execSync } from 'child_process';
 
-setup( 'Configure store for BLIK tests', async ( { browser } ) => {
+const setKeysForLocalEnv = async ( adminContext ) => {
 	execSync(
 		`WP_PATH="${ process.env.WP_PATH }" STRIPE_PUB_KEY="${ process.env.STRIPE_PUB_KEY_PL }" STRIPE_SECRET_KEY="${ process.env.STRIPE_SECRET_KEY_PL }" ./tests/e2e/bin/set-keys.sh`,
 		{ stdio: 'inherit' }
 	);
 
-	const adminContext = await browser.newContext( {
-		storageState: process.env.ADMINSTATE,
-	} );
 	const page = await adminContext.newPage();
 
 	// Refresh account data in Stripe settings.
@@ -21,11 +18,16 @@ setup( 'Configure store for BLIK tests', async ( { browser } ) => {
 	await page
 		.getByRole( 'menuitem', { name: 'Refresh account details' } )
 		.click();
+};
 
-	await page.goto(
-		'/wp-admin/admin.php?page=wc-settings&tab=checkout&section=stripe&panel=settings'
-	);
-	await page.waitForTimeout( 1000 );
+setup( 'Configure store for BLIK tests', async ( { browser } ) => {
+	const adminContext = await browser.newContext( {
+		storageState: process.env.ADMINSTATE,
+	} );
+
+	if ( ! process.env.CI ) {
+		await setKeysForLocalEnv( adminContext );
+	}
 
 	// Change store currency to PLN.
 	await admin.updateStoreCurrency( browser, 'PLN' );
