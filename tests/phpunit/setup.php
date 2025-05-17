a4fb1c469e5090e88c44ef62a9e5a64fb0f9f391
Wesley Rosa <wesleyjrosa@gmail.com>
2025-05-17 11:49:17 -0300

    Removing unnecessary hook in unit tests

--- tests/phpunit/setup.php
+++ tests/phpunit/setup.php
@@ -3,13 +3,3 @@
  * Set up shared by all tests.
  */
 update_option( 'woocommerce_default_country', 'US:CA' );
-
-/**
- * Disables the transient cooldown for the payment method configuration fetch.
- */
-add_filter(
-	'transient_wcstripe_payment_method_config_fetch_cooldown',
-	function () {
-		return 0;
-	}
-);
