3643a38106407c7cf970a408516774fd7c85fa84
Wesley Rosa <wesleyjrosa@gmail.com>
2025-05-26 14:24:14 -0300

    Random method to test the action

--- includes/class-wc-stripe-mode.php
+++ includes/class-wc-stripe-mode.php
@@ -26,4 +26,13 @@ class WC_Stripe_Mode {
 		$settings = WC_Stripe_Helper::get_stripe_settings();
 		return 'yes' === ( $settings['testmode'] ?? 'no' );
 	}
+
+	/**
+	 * Random method to test the code coverage action.
+	 *
+	 * @return bool Whether the method executed successfully.
+	 */
+	public static function random_method() {
+		return true;
+	}
 }
