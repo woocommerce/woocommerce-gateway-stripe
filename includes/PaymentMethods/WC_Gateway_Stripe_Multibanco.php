b9c68fdcdfc978d817068f925fdfdd98bbd616e4
Wesley Rosa <wesleyjrosa@gmail.com>
2025-05-29 09:19:08 -0300

    Reverting unnecessary changes

--- includes/PaymentMethods/WC_Gateway_Stripe_Multibanco.php
+++ includes/PaymentMethods/WC_Gateway_Stripe_Multibanco.php
@@ -169,7 +169,7 @@ class WC_Gateway_Stripe_Multibanco extends WC_Stripe_Payment_Gateway {
 	 * Initialize Gateway Settings Form Fields.
 	 */
 	public function init_form_fields() {
-		$this->form_fields = require WC_STRIPE_PLUGIN_PATH . '/includes/Admin/WC_Stripe_Settings_Multibanco.php';
+		$this->form_fields = require WC_STRIPE_PLUGIN_PATH . '/includes/Admin/stripe-multibanco-settings.php';
 	}
 
 	/**
--- includes/PaymentMethods/WC_Gateway_Stripe_P24.php
+++ includes/PaymentMethods/WC_Gateway_Stripe_P24.php
@@ -166,7 +166,7 @@ class WC_Gateway_Stripe_P24 extends WC_Stripe_Payment_Gateway {
 	 * Initialize Gateway Settings Form Fields.
 	 */
 	public function init_form_fields() {
-		$this->form_fields = require WC_STRIPE_PLUGIN_PATH . '/includes/Admin/WC_Stripe_Settings_P24.php';
+		$this->form_fields = require WC_STRIPE_PLUGIN_PATH . '/includes/Admin/stripe-p24-settings.php';
 	}
 
 	/**
