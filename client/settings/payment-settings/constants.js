6df34f00a810c5ef61e705eae17e58b63dc0648f
Wesley Rosa <wesleyjrosa@gmail.com>
2025-06-03 10:55:01 -0300

    BNPL banner constant identifier

--- client/settings/payment-settings/constants.js
+++ client/settings/payment-settings/constants.js
@@ -1,2 +1,3 @@
 export const RECONNECT_BANNER = 'stripe-reconnect';
 export const NEW_CHECKOUT_EXPERIENCE_BANNER = 'new-checkout-experience';
+export const BNPL_PROMOTION_BANNER = 'bnpl_promotion_banner';
