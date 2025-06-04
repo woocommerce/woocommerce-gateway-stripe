e4d84b295aba4edf2044e686205cab497d1ae8f9
Wesley Rosa <wesleyjrosa@gmail.com>
2025-06-04 18:53:57 -0300

    Unit tests

--- tests/phpunit/WC_Stripe_Page_Helper_Test.php
+++ tests/phpunit/WC_Stripe_Page_Helper_Test.php
@@ -2,22 +2,20 @@
 
 namespace WooCommerce\Stripe\Tests;
 
+use WC_Stripe_Page_Helper;
+use WP_UnitTestCase;
+
 /**
- * Tests for the WC_Stripe_Page_Helper class.
  */
-class WC_Stripe_Page_Helper_Test extends \WP_UnitTestCase {
+class WC_Stripe_Page_Helper_Test extends WP_UnitTestCase {
 	/**
 	 * Test if the subscription edit page is correctly identified.
 	 */
 	public function test_is_subscription_edit_page() {
-		// Simulate a subscription edit page query parameters.
-		$_GET = [
-			'post'   => 123,
-			'action' => 'edit',
-			'page'   => 'wc-orders--shop_subscription',
-			'id'     => 456,
-		];
+		add_filter( 'wc_stripe_is_subscription_edit_page', '__return_true' );
+		$this->assertTrue( WC_Stripe_Page_Helper::is_subscription_edit_page() );
 
-		$this->assertTrue( \WC_Stripe_Page_Helper::is_subscription_edit_page() );
+		remove_filter( 'wc_stripe_is_subscription_edit_page', '__return_true' );
+		$this->assertFalse( WC_Stripe_Page_Helper::is_subscription_edit_page() );
 	}
 }
