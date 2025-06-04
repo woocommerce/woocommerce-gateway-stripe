0dc037e9d69e7a447b1f8392e3d53ba4ef06b7fe
Wesley Rosa <wesleyjrosa@gmail.com>
2025-06-04 18:54:17 -0300

    Unit tests

--- includes/class-wc-stripe-page-helper.php
+++ includes/class-wc-stripe-page-helper.php
@@ -15,13 +15,23 @@ class WC_Stripe_Page_Helper {
 	 * @return bool
 	 */
 	public static function is_subscription_edit_page() {
+		if ( apply_filters( 'wc_stripe_is_subscription_edit_page', false ) ) {
+			return true;
+		}
+
 		$query_params = wp_unslash( $_GET ); // phpcs:ignore WordPress.Security.NonceVerification.Missing
 		if ( WC_Stripe_Woo_Compat_Utils::is_custom_orders_table_enabled() ) { // If custom order tables are enabled, we need to check the page query param.
-			return isset( $query_params['page'] ) && 'wc-orders--shop_subscription' === $query_params['page'] && isset( $query_params['id'] );
+			return apply_filters(
+				'wc_stripe_is_subscription_edit_page',
+				isset( $query_params['page'] ) && 'wc-orders--shop_subscription' === $query_params['page'] && isset( $query_params['id'] )
+			);
 		}
 
 		// If custom order tables are not enabled, we need to check the post type and action query params.
 		$is_shop_subscription_post_type = isset( $query_params['post'] ) && 'shop_subscription' === get_post_type( $query_params['post'] );
-		return isset( $query_params['action'] ) && 'edit' === $query_params['action'] && $is_shop_subscription_post_type;
+		return apply_filters(
+			'wc_stripe_is_subscription_edit_page',
+			isset( $query_params['action'] ) && 'edit' === $query_params['action'] && $is_shop_subscription_post_type
+		);
 	}
 }
