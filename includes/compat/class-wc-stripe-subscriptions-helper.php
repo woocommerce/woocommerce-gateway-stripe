8ae96ebffbc75e7ee0bab52110552a73a84bd14d
Wesley Rosa <wesleyjrosa@gmail.com>
2025-06-06 10:13:06 -0300

    Using double-quotes and removing espcaping for single quotes

--- includes/compat/class-wc-stripe-subscriptions-helper.php
+++ includes/compat/class-wc-stripe-subscriptions-helper.php
@@ -145,7 +145,7 @@ class WC_Stripe_Subscriptions_Helper {
 			count( $subscriptions )
 		); /* phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped */
 		// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
-		$intro_message .= __( 'To fix this, either: <br />1) Share the payment method page link with the customer to update it, or <br />2) Manually update the payment method in the subscription\'s billing details using a valid payment method from the customer\'s Stripe account. ', 'woocommerce-gateway-stripe' );
+		$intro_message .= __( "To fix this, either: <br />1) Share the payment method page link with the customer to update it, or <br />2) Manually update the payment method in the subscription's billing details using a valid payment method from the customer's Stripe account. ", 'woocommerce-gateway-stripe' );
 		$intro_message .= __( 'Below are the affected subscriptions and their update links:<br />', 'woocommerce-gateway-stripe' );
 		return $intro_message . $detached_messages;
 	}
