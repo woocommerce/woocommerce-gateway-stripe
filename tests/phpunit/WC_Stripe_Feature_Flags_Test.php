9d2f31c67343305b3fe1f1e8f771be271bd7b8c7
Wesley Rosa <wesleyjrosa@gmail.com>
2025-06-16 18:57:03 -0300

    Unit test

--- tests/phpunit/WC_Stripe_Feature_Flags_Test.php
+++ tests/phpunit/WC_Stripe_Feature_Flags_Test.php
@@ -17,13 +17,20 @@ class WC_Stripe_Feature_Flags_Test extends WP_UnitTestCase {
 	 * Test for `is_oc_available`.
 	 *
 	 * @param string $option_value The value of the feature flag option.
+	 * @param string $filter_function The filter function to apply.
 	 * @param bool   $expected     The expected result.
 	 * @return void
 	 * @dataProvider provide_test_is_oc_available
 	 */
-	public function test_is_oc_available( $option_value, $expected ) {
+	public function test_is_oc_available( $option_value, $filter_function, $expected ) {
+		if ( ! empty( $filter_function ) ) {
+			add_filter( 'wc_stripe_is_optimized_checkout_available', '__return_true' );
+		}
+
 		update_option( WC_Stripe_Feature_Flags::OC_FEATURE_FLAG_NAME, $option_value );
 		$this->assertSame( $expected, WC_Stripe_Feature_Flags::is_oc_available() );
+
+		remove_filter( 'wc_stripe_is_optimized_checkout_available', '__return_true' );
 	}
 
 	/**
@@ -33,13 +40,17 @@ class WC_Stripe_Feature_Flags_Test extends WP_UnitTestCase {
 	 */
 	public function provide_test_is_oc_available() {
 		return [
-			'available'     => [
+			'available'          => [
 				'option value' => 'yes',
 				'expected'     => true,
 			],
-			'not available' => [
+			'not available'      => [
 				'option value' => 'no',
 				'expected'     => false,
+			],
+			'filter set to true' => [
+				'option value' => 'no',
+				'expected'     => true,
 			],
 		];
 	}
