f580da74e9fd3628e96bee02c7817c8a404cd55c
Wesley Rosa <wesleyjrosa@gmail.com>
2025-06-18 18:59:01 -0300

    Update includes/class-wc-stripe-webhook-handler.php
    Co-authored-by: daledupreez <dale.du.preez@automattic.com>

--- includes/class-wc-stripe-webhook-handler.php
+++ includes/class-wc-stripe-webhook-handler.php
@@ -826,8 +826,11 @@ class WC_Stripe_Webhook_Handler extends WC_Stripe_Payment_Gateway {
 					}
 
 					// Store the failure reason
+					$order->update_meta_data( '_stripe_refund_status', $refund_object->status );
 					if ( isset( $refund_object->failure_reason ) ) {
 						$order->update_meta_data( '_stripe_refund_failure_reason', $refund_object->failure_reason );
+					} else {
+						$order->delete_meta_data( '_stripe_refund_failure_reason' );
 					}
 
 					// Revert to previous status
