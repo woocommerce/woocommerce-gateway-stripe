ed58908934861e104d3cbee06c1c098bd3c53b62
Anne Mirasol <anne.mirasol@automattic.com>
2025-06-19 11:08:39 -0500

    Add the custom data under extensions

---
+++ client/express-checkout/compatibility/classic-checkout-custom-fields.js
@@ -0,0 +1,45 @@
+/**
+ * This file is for adding custom checkout field data when using express checkout
+ * with classic checkout.
+ *
+ * It adds the data under extensions, using the wc-stripe/ece-custom-checkout-data namespace.
+ */
+import { addFilter } from '@wordpress/hooks';
+import { getExpressCheckoutData } from 'wcstripe/express-checkout/utils';
+
+addFilter(
+	'wcstripe.express-checkout.cart-place-order-extension-data',
+	'automattic/wcstripe/express-checkout',
+	( extensionData ) => {
+		const customCheckoutFields = getExpressCheckoutData(
+			'custom_checkout_fields'
+		);
+
+		if ( ! customCheckoutFields ) {
+			return extensionData;
+		}
+
+		const customCheckoutFieldsData = {};
+		Object.keys( customCheckoutFields ).forEach( ( field ) => {
+			const formElement = document.querySelector(
+				`form [name="${ field }"]`
+			);
+			if ( ! formElement ) {
+				return;
+			}
+
+			if ( formElement.type === 'checkbox' ) {
+				if ( formElement.checked ) {
+					customCheckoutFieldsData[ field ] = 1;
+				}
+			} else {
+				customCheckoutFieldsData[ field ] = formElement.value;
+			}
+		} );
+
+		return {
+			...extensionData,
+			'wc-stripe/ece-custom-checkout-data': customCheckoutFieldsData,
+		};
+	}
+);
