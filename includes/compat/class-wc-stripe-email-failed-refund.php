f623d1f1a6ca4c889a4094f485cd32b9abd54201
Wesley Rosa <wesleyjrosa@gmail.com>
2025-06-23 15:18:04 -0300

    Update includes/compat/class-wc-stripe-email-failed-refund.php
    Co-authored-by: Mayisha <33387139+Mayisha@users.noreply.github.com>

--- includes/compat/class-wc-stripe-email-failed-refund.php
+++ includes/compat/class-wc-stripe-email-failed-refund.php
@@ -23,7 +23,7 @@ abstract class WC_Stripe_Email_Failed_Refund extends WC_Email_Failed_Order {
 	public function __construct() {
 		$this->title   = __( 'Stripe refund failed', 'woocommerce-gateway-stripe' );
 		$this->heading = __( 'Refund failed', 'woocommerce-gateway-stripe' );
-		$this->subject = __( '[{site_title}] Refund failed for #{order_number}.', 'woocommerce-gateway-stripe' );
+		$this->subject = __( '[{site_title}]: Refund failed for #{order_number}', 'woocommerce-gateway-stripe' );
 	}
 
 	/**
