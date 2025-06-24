451c3b5294a5d77861a5c3e7dd5b0229c7eb45ec
Wesley Rosa <wesleyjrosa@gmail.com>
2025-06-24 08:25:00 -0300

    Revert "Using placeholders instead of find and replace"
    This reverts commit acf140727a91e3cf21674faa8fd86353e2c0c42e.

--- includes/compat/class-wc-stripe-email-failed-authentication-retry.php
+++ includes/compat/class-wc-stripe-email-failed-authentication-retry.php
@@ -106,7 +106,8 @@ class WC_Stripe_Email_Failed_Authentication_Retry extends WC_Email_Failed_Order 
 			return;
 		}
 
-		$this->placeholders['{order_number}'] = $this->object->get_order_number();
+		$this->find['order-number']    = '{order_number}';
+		$this->replace['order-number'] = $this->object->get_order_number();
 
 		if ( ! $this->is_enabled() || ! $this->get_recipient() ) {
 			return;
