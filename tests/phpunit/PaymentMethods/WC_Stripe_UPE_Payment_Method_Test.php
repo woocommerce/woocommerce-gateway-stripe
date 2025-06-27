678356cd9a364547fc12785f9c0bd5e26eef7cce
Wesley Rosa <wesleyjrosa@gmail.com>
2025-06-27 16:17:12 -0300

    Fix tests

--- tests/phpunit/PaymentMethods/WC_Stripe_UPE_Payment_Method_Test.php
+++ tests/phpunit/PaymentMethods/WC_Stripe_UPE_Payment_Method_Test.php
@@ -856,11 +856,14 @@ class WC_Stripe_UPE_Payment_Method_Test extends WC_Mock_Stripe_API_Unit_Test_Cas
 			update_option( 'woocommerce_stripe_' . $payment_method_id . '_settings', $original_payment_settings );
 		}
 
-		// Test custom description when SPE is enabled. Should be always empty.
+		// Test custom description when OC is enabled. Should be always empty.
 		update_option( WC_Stripe_Feature_Flags::OC_FEATURE_FLAG_NAME, 'yes' );
 
+		// Enable PMC.
 		$stripe_settings                               = WC_Stripe_Helper::get_stripe_settings();
 		$stripe_settings['optimized_checkout_element'] = 'yes';
+		$stripe_settings['test_connection_type']       = 'connect';
+		$stripe_settings['pmc_enabled']                = 'yes';
 		WC_Stripe_Helper::update_main_stripe_settings( $stripe_settings );
 
 		$payment_method_id                       = WC_Stripe_Payment_Methods::CARD;
@@ -883,6 +886,9 @@ class WC_Stripe_UPE_Payment_Method_Test extends WC_Mock_Stripe_API_Unit_Test_Cas
 			->getMock();
 
 		$this->assertEmpty( $mocked_payment_method->get_description() );
+
+		// Clean up
+		WC_Stripe_Helper::update_main_stripe_settings( [] );
 	}
 
 	/**
