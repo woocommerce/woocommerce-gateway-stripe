113026a1c4460a62ad8fb9129ec1c5dcafa3b30f
Wesley Rosa <wesleyjrosa@gmail.com>
2025-06-27 11:00:04 -0300

    Unit test

--- tests/phpunit/WC_Stripe_Feature_Flags_Test.php
+++ tests/phpunit/WC_Stripe_Feature_Flags_Test.php
@@ -3,6 +3,7 @@
 namespace WooCommerce\Stripe\Tests;
 
 use WC_Stripe_Feature_Flags;
+use WC_Stripe_Helper;
 use WP_UnitTestCase;
 
 /**
@@ -22,7 +23,15 @@ class WC_Stripe_Feature_Flags_Test extends WP_UnitTestCase {
 	 * @return void
 	 * @dataProvider provide_test_is_oc_available
 	 */
-	public function test_is_oc_available( $option_value, $filter_function, $expected ) {
+	public function test_is_oc_available( $settings_values, $option_value, $filter_function, $expected ) {
+		// Merge the provided settings values with the default settings.
+		WC_Stripe_Helper::update_main_stripe_settings(
+			array_merge(
+				WC_Stripe_Helper::get_stripe_settings(),
+				$settings_values
+			)
+		);
+
 		if ( ! empty( $filter_function ) ) {
 			add_filter( 'wc_stripe_is_optimized_checkout_available', $filter_function );
 		}
@@ -33,6 +42,13 @@ class WC_Stripe_Feature_Flags_Test extends WP_UnitTestCase {
 		if ( ! empty( $filter_function ) ) {
 			remove_filter( 'wc_stripe_is_optimized_checkout_available', $filter_function );
 		}
+
+		// Reset settings to the default values.
+		$settings                         = WC_Stripe_Helper::get_stripe_settings();
+		$settings['connection_type']      = 'connect';
+		$settings['test_connection_type'] = 'connect';
+		$settings['pmc_enabled']          = 'yes';
+		WC_Stripe_Helper::update_main_stripe_settings( $settings );
 	}
 
 	/**
@@ -42,22 +58,52 @@ class WC_Stripe_Feature_Flags_Test extends WP_UnitTestCase {
 	 */
 	public function provide_test_is_oc_available() {
 		return [
-			'available'           => [
+			'available'                                 => [
+				'settings values' => [
+					'connection_type'      => 'connect',
+					'test_connection_type' => 'connect',
+					'pmc_enabled'          => 'yes',
+				],
 				'option value'    => 'yes',
 				'filter function' => '',
 				'expected'        => true,
 			],
-			'not available'       => [
+			'not available indirectly due PMC disabled' => [
+				'settings values' => [
+					'connection_type'      => 'connect',
+					'test_connection_type' => 'connect',
+					'pmc_enabled'          => 'no',
+				],
+				'option value'    => 'yes',
+				'filter function' => '',
+				'expected'        => false,
+			],
+			'not available directly'                    => [
+				'settings values' => [
+					'connection_type'      => 'connect',
+					'test_connection_type' => 'connect',
+					'pmc_enabled'          => 'yes',
+				],
 				'option value'    => 'no',
 				'filter function' => '',
 				'expected'        => false,
 			],
-			'filter set to true'  => [
+			'filter set to true'                        => [
+				'settings values' => [
+					'connection_type'      => 'connect',
+					'test_connection_type' => 'connect',
+					'pmc_enabled'          => 'yes',
+				],
 				'option value'    => 'no',
 				'filter function' => '__return_true',
 				'expected'        => true,
 			],
-			'filter set to false' => [
+			'filter set to false'                       => [
+				'settings values' => [
+					'connection_type'      => 'connect',
+					'test_connection_type' => 'connect',
+					'pmc_enabled'          => 'yes',
+				],
 				'option value'    => 'yes',
 				'filter function' => '__return_false',
 				'expected'        => false,
