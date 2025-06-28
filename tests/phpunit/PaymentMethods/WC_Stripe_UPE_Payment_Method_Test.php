dc962611e5864cbf55237fb49515e636981a6d44
Wesley Rosa <wesleyjrosa@gmail.com>
2025-06-28 10:50:54 -0300

    Improving tests

--- tests/phpunit/PaymentMethods/WC_Stripe_UPE_Payment_Method_Test.php
+++ tests/phpunit/PaymentMethods/WC_Stripe_UPE_Payment_Method_Test.php
@@ -856,9 +856,12 @@ class WC_Stripe_UPE_Payment_Method_Test extends WC_Mock_Stripe_API_Unit_Test_Cas
 			update_option( 'woocommerce_stripe_' . $payment_method_id . '_settings', $original_payment_settings );
 		}
 
-		// Test custom description when SPE is enabled. Should be always empty.
+		// Test custom description when OC is enabled. Should be always empty.
 		update_option( WC_Stripe_Feature_Flags::OC_FEATURE_FLAG_NAME, 'yes' );
-		update_option( 'optimized_checkout_element', 'yes' );
+
+		$stripe_settings                               = WC_Stripe_Helper::get_stripe_settings();
+		$stripe_settings['optimized_checkout_element'] = 'yes';
+		WC_Stripe_Helper::update_main_stripe_settings( $stripe_settings );
 
 		$payment_method_id                       = WC_Stripe_Payment_Methods::CARD;
 		$custom_description                      = 'Custom description for ' . $payment_method_id;
@@ -880,6 +883,15 @@ class WC_Stripe_UPE_Payment_Method_Test extends WC_Mock_Stripe_API_Unit_Test_Cas
 			->getMock();
 
 		$this->assertEmpty( $mocked_payment_method->get_description() );
+
+		// Clean up.
+		update_option( WC_Stripe_Feature_Flags::OC_FEATURE_FLAG_NAME, 'yes' );
+
+		$stripe_settings                               = WC_Stripe_Helper::get_stripe_settings();
+		$stripe_settings['optimized_checkout_element'] = 'yes';
+		WC_Stripe_Helper::update_main_stripe_settings( $stripe_settings );
+
+		update_option( 'woocommerce_stripe_' . $payment_method_id . '_settings', $original_payment_settings );
 	}
 
 	/**
