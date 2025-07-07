b59b6b8ffc62f6b85e1b42eea55df5bc83af6a1a
Diego Curbelo <diego@curbelo.com>
2025-07-07 15:06:24 -0300

    Add shortcut methods for each log severity level

--- includes/class-wc-stripe-logger.php
+++ includes/class-wc-stripe-logger.php
@@ -11,20 +11,30 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class WC_Stripe_Logger {
 
-	public static $logger;
 	const WC_LOG_FILENAME = 'woocommerce-gateway-stripe';
 
-	protected const LOG_CONTEXT = [
+	const LOG_CONTEXT = [
 		'source'             => self::WC_LOG_FILENAME,
 		'stripe_version'     => WC_STRIPE_VERSION,
 		'stripe_api_version' => WC_Stripe_API::STRIPE_API_VERSION,
 	];
 
 	/**
+	 * Log handler instance.
+	 *
+	 * @see https://developer.wordpress.org/reference/classes/wp_logger/
+	 * @see https://developer.woocommerce.com/docs/best-practices/data-management/logging/#log-handlers
+	 *
+	 * @var WC_Logger
+	 */
+	public static $logger;
+
+	/**
 	 * Utilize WC logger class
 	 *
+	 * @deprecated 9.7.0 Use the shortcut methods for each log severity level: info(), error(), etc. instead.
+	 *
 	 * @since 4.0.0
-	 * @version 4.0.0
 	 */
 	public static function log( $message, $start_time = null, $end_time = null ) {
 		if ( ! self::can_log() ) {
@@ -58,15 +68,51 @@ class WC_Stripe_Logger {
 		self::$logger->debug( $log_entry, [ 'source' => self::WC_LOG_FILENAME ] );
 	}
 
+
+	// Logs have eight different severity levels:
+	// - emergency
+	// - alert
+	// - critical
+	// - error
+	// - warning
+	// - notice
+	// - info
+	// - debug
+
+
 	/**
-	 * Creates a log entry of type error.
+	 * Creates a log entry of type critical.
+	 *
+	 * @since 9.7.0
 	 *
 	 * @param string $message To send to the log file.
 	 * @param array $context Additional context to add to the log.
 	 *
 	 * @return void
 	 */
-	public static function error( $log_entry, $context = [] ) {
+	public static function critical( $message, $context = [] ) {
+		if ( ! self::can_log() ) {
+			return;
+		}
+
+		if ( empty( self::$logger ) ) {
+			self::$logger = wc_get_logger();
+		}
+
+		self::$logger->critical( $message, array_merge( self::LOG_CONTEXT, $context, [ 'backtrace' => true ] ) );
+	}
+
+	/**
+	 * Creates a log entry of type error.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @param string $message To send to the log file.
+	 * @param array $context Additional context to add to the log.
+	 *
+	 * @return void
+	 */
+	public static function error( $message, $context = [] ) {
 		if ( ! self::can_log() ) {
 			return;
 		}
@@ -79,7 +125,53 @@ class WC_Stripe_Logger {
 	}
 
 	/**
+	 * Creates a log entry of type warning.
+	 *
+	 * @since 9.7.0
+	 *
+	 * @param string $message To send to the log file.
+	 * @param array $context Additional context to add to the log.
+	 *
+	 * @return void
+	 */
+	public static function warning( $message, $context = [] ) {
+		if ( ! self::can_log() ) {
+			return;
+		}
+
+		if ( empty( self::$logger ) ) {
+			self::$logger = wc_get_logger();
+		}
+
+		self::$logger->warning( $message, array_merge( self::LOG_CONTEXT, $context, [ 'backtrace' => true ] ) );
+	}
+
+	/**
+	 * Creates a log entry of type info.
+	 *
+	 * @since 9.7.0
+	 *
+	 * @param string $message To send to the log file.
+	 * @param array $context Additional context to add to the log.
+	 *
+	 * @return void
+	 */
+	public static function info( $message, $context = [] ) {
+		if ( ! self::can_log() ) {
+			return;
+		}
+
+		if ( empty( self::$logger ) ) {
+			self::$logger = wc_get_logger();
+		}
+
+		self::$logger->info( $message, array_merge( self::LOG_CONTEXT, $context ) );
+	}
+
+	/**
 	 * Creates a log entry of type debug.
+	 *
+	 * @since 4.0.0
 	 *
 	 * @param string $message To send to the log file.
 	 * @param array $context Additional context to add to the log.
