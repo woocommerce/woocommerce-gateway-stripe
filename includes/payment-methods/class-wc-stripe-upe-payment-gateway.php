df3087af70026918b977766e892d2afce06c906e
Wesley Rosa <wesleyjrosa@gmail.com>
2025-07-07 11:09:23 -0300

    Removing unlock call from finally and reverting to original positions

--- includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
@@ -1184,6 +1184,8 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 				$redirect = $this->get_return_url( $order );
 			}
 
+			$this->unlock_order_payment( $order );
+
 			return array_merge(
 				[
 					'result'   => 'success',
@@ -1192,10 +1194,9 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 				$response_args
 			);
 		} catch ( WC_Stripe_Exception $e ) {
-			return $this->handle_process_payment_error( $e, $order );
-		} finally {
 			// Ensure the order is unlocked in case of an exception.
 			$this->unlock_order_payment( $order );
+			return $this->handle_process_payment_error( $e, $order );
 		}
 	}
 
