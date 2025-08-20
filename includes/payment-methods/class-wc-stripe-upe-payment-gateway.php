6cc5629b4b6d7388cb29f4a1237f33a804ad5071
Wesley Rosa <wesleyjrosa@gmail.com>
2025-08-20 16:01:53 -0300

    Update includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
    Co-authored-by: Mayisha <33387139+Mayisha@users.noreply.github.com>

--- includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
@@ -533,9 +533,7 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Gateway_Stripe {
 
 		// Optimized Checkout feature flag + setting.
 		$stripe_params['isOCEnabled'] = $this->oc_enabled;
-		$stripe_params['OCLayout']    = $this->get_option( 'optimized_checkout_layout' )
-			? $this->get_option( 'optimized_checkout_layout' )
-			: self::OPTIMIZED_CHECKOUT_DEFAULT_LAYOUT;
+		$stripe_params['OCLayout']    = $this->get_option( 'optimized_checkout_layout', self::OPTIMIZED_CHECKOUT_DEFAULT_LAYOUT );
 
 		// Single Payment Element payment method parent configuration ID
 		$stripe_params['paymentMethodConfigurationParentId'] = WC_Stripe_Payment_Method_Configurations::get_parent_configuration_id();
