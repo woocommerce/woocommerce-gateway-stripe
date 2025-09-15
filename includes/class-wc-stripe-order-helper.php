d750e3b1f2a67cde0c4a65dfbd7748302a11478a
Wesley Rosa <wesleyjrosa@gmail.com>
2025-09-15 17:00:52 -0300

    Revert unnecessary changes

--- includes/class-wc-stripe-order-helper.php
+++ includes/class-wc-stripe-order-helper.php
@@ -238,63 +238,6 @@ class WC_Stripe_Order_Helper {
 	}
 
 	/**
-	 * Gets the order by Stripe source ID.
-	 *
-	 * @since 9.9.0
-	 *
-	 * @param string $source_id
-	 */
-	public static function get_order_by_source_id( $source_id ) {
-		return self::get_by_meta( self::META_STRIPE_SOURCE_ID, $source_id );
-	}
-
-	/**
-	 * Gets the order by Stripe charge ID.
-	 *
-	 * @since 9.9.0
-	 *
-	 * @param string $charge_id
-	 */
-	public static function get_order_by_charge_id( $charge_id ) {
-		return self::get_by_meta( self::META_STRIPE_CHARGE_ID, $charge_id );
-	}
-
-	/**
-	 * Gets the order by Stripe refund ID.
-	 *
-	 * @since 9.9.0
-	 *
-	 * @param string $refund_id
-	 */
-	public static function get_order_by_refund_id( $refund_id ) {
-		return self::get_by_meta( self::META_STRIPE_REFUND_ID, $refund_id );
-	}
-
-	/**
-	 * Gets the order by Stripe PaymentIntent ID.
-	 *
-	 * @since 9.9.0
-	 *
-	 * @param string $intent_id The ID of the intent.
-	 * @return WC_Order|bool Either an order or false when not found.
-	 */
-	public static function get_order_by_intent_id( $intent_id ) {
-		return self::get_by_meta( self::META_STRIPE_INTENT_ID, $intent_id );
-	}
-
-	/**
-	 * Gets the order by Stripe SetupIntent ID.
-	 *
-	 * @since 9.9.0
-	 *
-	 * @param string $intent_id The ID of the intent.
-	 * @return WC_Order|bool Either an order or false when not found.
-	 */
-	public static function get_order_by_setup_intent_id( $intent_id ) {
-		return self::get_by_meta( self::META_STRIPE_SETUP_INTENT, $intent_id );
-	}
-
-	/**
 	 * Adds payment intent id and order note to order if payment intent is not already saved
 	 *
 	 * @since 9.9.0
@@ -666,46 +609,5 @@ class WC_Stripe_Order_Helper {
 	public static function unlock_order_refund( $order ) {
 		$order->delete_meta_data( '_stripe_lock_refund' );
 		$order->save_meta_data();
-	}
-
-	/**
-	 * Queries for an order by a specific meta key and value.
-	 *
-	 * @param $meta_key string The meta key to search for.
-	 * @param $meta_value string The meta value to search for.
-	 * @return bool|WC_Order
-	 */
-	private static function get_by_meta( $meta_key, $meta_value ) {
-		global $wpdb;
-
-		if ( WC_Stripe_Woo_Compat_Utils::is_custom_orders_table_enabled() ) {
-			$params = [ 'limit' => 1 ];
-			// Check if the meta key is a transaction ID. If so, use the transaction ID to query the order, instead of the meta when HPOS is enabled.
-			if ( self::META_STRIPE_CHARGE_ID === $meta_key ) {
-				$params['transaction_id'] = $meta_value;
-			} else {
-				$params['meta_query'] = [ // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query
-					[
-						'key'   => $meta_key,
-						'value' => $meta_value,
-					],
-				];
-			}
-
-			$orders   = wc_get_orders( $params );
-			$order_id = current( $orders ) ? current( $orders )->get_id() : false;
-		} else {
-			$order_id = $wpdb->get_var( $wpdb->prepare( "SELECT DISTINCT ID FROM $wpdb->posts as posts LEFT JOIN $wpdb->postmeta as meta ON posts.ID = meta.post_id WHERE meta.meta_value = %s AND meta.meta_key = %s", $meta_value, $meta_key ) );
-		}
-
-		if ( ! empty( $order_id ) ) {
-			$order = wc_get_order( $order_id );
-		}
-
-		if ( ! empty( $order ) && $order->get_status() !== OrderStatus::TRASH ) {
-			return $order;
-		}
-
-		return false;
 	}
 }
