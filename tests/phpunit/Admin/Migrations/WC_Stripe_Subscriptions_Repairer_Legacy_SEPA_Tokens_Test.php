2ca2c04c0264510740f2ee1f5e3b34baf7d0b1e5
Wesley Rosa <wesleyjrosa@gmail.com>
2025-09-19 11:56:08 -0300

    Fix tests

--- tests/phpunit/Admin/Migrations/WC_Stripe_Subscriptions_Repairer_Legacy_SEPA_Tokens_Test.php
+++ tests/phpunit/Admin/Migrations/WC_Stripe_Subscriptions_Repairer_Legacy_SEPA_Tokens_Test.php
@@ -115,17 +115,6 @@ class WC_Stripe_Subscriptions_Repairer_Legacy_SEPA_Tokens_Test extends WP_UnitTe
 		$this->updater->maybe_update();
 	}
 
-	public function test_updater_doesn_not_get_scheduled_when_legacy_is_enabled() {
-		add_filter( 'wc_stripe_is_upe_checkout_enabled', '__return_false' );
-		delete_option( 'woocommerce_stripe_subscriptions_legacy_sepa_tokens_updated' );
-
-		$this->updater
-			 ->expects( $this->never() )
-			 ->method( 'schedule_repair' );
-
-		$this->updater->maybe_update();
-	}
-
 	public function test_updater_doesn_not_get_scheduled_when_already_done() {
 		WC_Stripe_Helper::update_main_stripe_settings( [ 'upe_checkout_experience_enabled' => 'yes' ] );
 		update_option( 'woocommerce_stripe_subscriptions_legacy_sepa_tokens_updated', 'yes' );
@@ -202,29 +191,6 @@ class WC_Stripe_Subscriptions_Repairer_Legacy_SEPA_Tokens_Test extends WP_UnitTe
 			);
 
 		$this->updater->repair_item( $ids_to_migrate[0] );
-	}
-
-	public function test_maybe_update_subscription_legacy_payment_method_bails_when_the_legacy_experience_is_enabled() {
-		add_filter( 'wc_stripe_is_upe_checkout_enabled', '__return_false' );
-		$ids_to_migrate  = $this->get_subs_ids_to_migrate();
-		$subscription_id = $ids_to_migrate[0];
-
-		// We didn't set upe_checkout_experience_enabled to 'yes', which means the Legacy experience is enabled.
-		$this->logger_mock
-			->expects( $this->exactly( 2 ) )
-			->method( 'add' )
-			->withConsecutive(
-				[
-					$this->anything(),
-					$this->anything(),
-				],
-				[
-					$this->equalTo( 'woocommerce-gateway-stripe-subscriptions-legacy-sepa-tokens-repairs' ),
-					$this->equalTo( sprintf( '---- Skipping migration of subscription #%d. The Legacy experience is enabled.', $subscription_id ) ),
-				],
-			);
-
-		$this->updater->repair_item( $subscription_id );
 	}
 
 	public function test_maybe_update_subscription_legacy_payment_method_bails_when_the_subscription_is_not_found() {
