5aeb66959332fffa56f4fddf6c640e6149b88510
Wesley Rosa <wesleyjrosa@gmail.com>
2025-11-14 08:22:30 -0300

    Update includes/class-wc-stripe-helper.php
    Co-authored-by: daledupreez <dale.du.preez@automattic.com>

--- includes/class-wc-stripe-helper.php
+++ includes/class-wc-stripe-helper.php
@@ -440,7 +440,7 @@ class WC_Stripe_Helper {
 	 *
 	 * @return array
 	 *
-	 * @deprecated 10.1.0 This method will be removed in future versions.
+	 * @deprecated 10.2.0 This method will be removed in future versions.
 	 */
 	public static function get_legacy_payment_method_classes() {
 		return [];
