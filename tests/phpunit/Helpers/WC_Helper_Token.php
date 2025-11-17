1bd15d5d90e384b7e9661873829596cd32879610
Wesley Rosa <wesleyjrosa@gmail.com>
2025-11-17 08:41:11 -0300

    Removing unused import

--- tests/phpunit/Helpers/WC_Helper_Token.php
+++ tests/phpunit/Helpers/WC_Helper_Token.php
@@ -3,7 +3,6 @@
 namespace WooCommerce\Stripe\Tests\Helpers;
 
 use WC_Stripe_UPE_Payment_Gateway;
-use WC_Gateway_Stripe_Sepa;
 use WC_Payment_Token_CC;
 use WC_Payment_Token_SEPA;
 use WC_Payment_Tokens;
