cc1222c2abb6aec6c383bf7497ca38679227bea4
Wesley Rosa <wesleyjrosa@gmail.com>
2025-12-23 11:10:17 -0300

    Requiring the new file in the legacy one

--- includes/constants/class-wc-stripe-payment-request-button-states.php
+++ includes/constants/class-wc-stripe-payment-request-button-states.php
@@ -11,6 +11,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+require_once WC_STRIPE_PLUGIN_PATH . '/includes/constants/wc-stripe-express-checkout-button-states.php';
+
 /**
  * Known issues/inconsistencies:
  *    1. WC provides a dropdown list of states, but there's no state field in Chrome for the following countries:
