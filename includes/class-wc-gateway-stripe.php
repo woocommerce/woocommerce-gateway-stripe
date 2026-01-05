95afcd9053120563cc9f4467408e2c676d0c16db
Wesley Rosa <wesleyjrosa@gmail.com>
2026-01-05 16:43:58 -0300

    Emptying class and temporarily extending the UPE class for backwards compatibility

---
+++ includes/class-wc-gateway-stripe.php
@@ -0,0 +1,15 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * WC_Gateway_Stripe class. Temporarily extends WC_Stripe_UPE_Payment_Gateway for backward compatibility.
+ *
+ * @extends WC_Stripe_UPE_Payment_Gateway
+ *
+ * @deprecated Deprecated in favor of WC_Stripe_UPE_Payment_Gateway. To be removed in a future version.
+ */
+class WC_Gateway_Stripe extends WC_Stripe_UPE_Payment_Gateway {
+}
