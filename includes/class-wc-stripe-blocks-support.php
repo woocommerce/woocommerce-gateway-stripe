52e3b5c7d660df8a734de2053c6f4be3b30de5a6
Wesley Rosa <wesleyjrosa@gmail.com>
2026-01-13 10:09:28 -0300

    Updating PHPStan baseline file

--- includes/class-wc-stripe-blocks-support.php
+++ includes/class-wc-stripe-blocks-support.php
@@ -29,7 +29,7 @@ final class WC_Stripe_Blocks_Support extends AbstractPaymentMethodType {
 	/**
 	 * Constructor
 	 *
-	 * @param mixed                              $payment_request_configuration The Stripe Payment Request configuration used for Payment Request buttons (removed).
+	 * @param mixed                                   $payment_request_configuration The Stripe Payment Request configuration used for Payment Request buttons (removed).
 	 * @param WC_Stripe_Express_Checkout_Element|null $express_checkout_configuration The Stripe Express Checkout configuration used for Express Checkout buttons.
 	 *
 	 * @deprecated Parameter $payment_request_configuration is deprecated since version 10.3.0 and will be removed in later versions.
