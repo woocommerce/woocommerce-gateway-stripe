4c4c2ec73adc8e5799e248857464c79471dc8e06
Wesley Rosa <wesleyjrosa@gmail.com>
2026-02-03 11:42:05 -0300

    Catching exceptions and calling the json error method

--- includes/class-wc-stripe-checkout-sessions-controller.php
+++ includes/class-wc-stripe-checkout-sessions-controller.php
@@ -24,67 +24,72 @@ class WC_Stripe_Checkout_Sessions_Controller {
 	 * @throws WC_Stripe_Exception When unable to create the Checkout Session.
 	 */
 	public function create_checkout_session(): void {
-		$is_nonce_valid = check_ajax_referer( 'wc_stripe_create_checkout_session_nonce', false, false );
-		if ( ! $is_nonce_valid ) {
-			throw new Exception( __( "We're not able to process this payment. Please refresh the page and try again.", 'woocommerce-gateway-stripe' ) );
-		}
-
-		if ( ! defined( 'WOOCOMMERCE_CART' ) ) {
-			define( 'WOOCOMMERCE_CART', true );
-		}
-
-		$payment_method_type     = isset( $_POST['payment_method_type'] ) ? wc_clean( wp_unslash( $_POST['payment_method_type'] ) ) : '';
-		$enabled_payment_methods = $payment_method_type ? [ $payment_method_type ] : [];
-
 		try {
-			$customer = new WC_Stripe_Customer( wp_get_current_user()->ID );
-			$customer->maybe_create_customer();
-		} catch ( Exception $e ) {
-			throw new Exception( __( 'Unable to create or retrieve Stripe customer.', 'woocommerce-gateway-stripe' ) );
-		}
+			$is_nonce_valid = check_ajax_referer( 'wc_stripe_create_checkout_session_nonce', false, false );
+			if ( ! $is_nonce_valid ) {
+				throw new Exception( __( "We're not able to process this payment. Please refresh the page and try again.", 'woocommerce-gateway-stripe' ) );
+			}
 
-		if ( is_null( WC()->cart ) || WC()->cart->is_empty() ) {
-			throw new Exception( __( 'Your cart is currently empty.', 'woocommerce-gateway-stripe' ) );
-		}
+			if ( ! defined( 'WOOCOMMERCE_CART' ) ) {
+				define( 'WOOCOMMERCE_CART', true );
+			}
 
-		$currency   = get_woocommerce_currency();
-		$line_items = [];
-		foreach ( WC()->cart->get_cart() as $cart_item ) {
-			$product_name   = $cart_item['data']->get_name();
-			$line_items[] = [
-				'price_data' => [
-					'currency' => strtolower( $currency ),
-					'product_data' => [
-						'name' => $product_name,
+			$payment_method_type     = isset( $_POST['payment_method_type'] ) ? wc_clean( wp_unslash( $_POST['payment_method_type'] ) ) : '';
+			$enabled_payment_methods = $payment_method_type ? [ $payment_method_type ] : [];
+
+			try {
+				$customer = new WC_Stripe_Customer( wp_get_current_user()->ID );
+				$customer->maybe_create_customer();
+			} catch ( Exception $e ) {
+				throw new Exception( __( 'Unable to create or retrieve Stripe customer.', 'woocommerce-gateway-stripe' ) );
+			}
+
+			if ( is_null( WC()->cart ) || WC()->cart->is_empty() ) {
+				throw new Exception( __( 'Your cart is currently empty.', 'woocommerce-gateway-stripe' ) );
+			}
+
+			$currency   = get_woocommerce_currency();
+			$line_items = [];
+			foreach ( WC()->cart->get_cart() as $cart_item ) {
+				$product_name   = $cart_item['data']->get_name();
+				$line_items[] = [
+					'price_data' => [
+						'currency' => strtolower( $currency ),
+						'product_data' => [
+							'name' => $product_name,
+						],
+						'unit_amount' => WC_Stripe_Helper::get_stripe_amount( $cart_item['line_subtotal'] / $cart_item['quantity'], $currency ),
 					],
-					'unit_amount' => WC_Stripe_Helper::get_stripe_amount( $cart_item['line_subtotal'] / $cart_item['quantity'], $currency ),
+					'quantity' => $cart_item['quantity'],
+				];
+			}
+
+			$request = [
+				'ui_mode'              => 'custom',
+				'customer'             => $customer->get_id(),
+				'line_items'           => $line_items,
+				'payment_method_types' => $enabled_payment_methods,
+				'payment_intent_data'  => [], // @todo Pass additional data if needed.
+				'mode'                 => 'payment',
+				'adaptive_pricing'     => [
+					'enabled' => 'true',
 				],
-				'quantity' => $cart_item['quantity'],
 			];
+
+			$checkout_session = WC_Stripe_API::request( $request, 'checkout/sessions' );
+
+			if ( ! empty( $checkout_session->error ) ) {
+				throw new Exception( $checkout_session->error->message );
+			}
+
+			if ( empty( $checkout_session->client_secret ) ) {
+				throw new Exception( __( 'Unable to create Stripe Checkout Session.', 'woocommerce-gateway-stripe' ) );
+			}
+
+			wp_send_json_success( [ 'client_secret' => $checkout_session->client_secret ] );
+		} catch ( Exception $e ) {
+			WC_Stripe_Logger::error( 'Create checkout session error.', [ 'error_message' => $e->getMessage() ] );
+			wp_send_json_error( [ 'message' => $e->getMessage() ] );
 		}
-
-		$request = [
-			'ui_mode'              => 'custom',
-			'customer'             => $customer->get_id(),
-			'line_items'           => $line_items,
-			'payment_method_types' => $enabled_payment_methods,
-			'payment_intent_data'  => [], // @todo Pass additional data if needed.
-			'mode'                 => 'payment',
-			'adaptive_pricing'     => [
-				'enabled' => 'true',
-			],
-		];
-
-		$checkout_session = WC_Stripe_API::request( $request, 'checkout/sessions' );
-
-		if ( ! empty( $checkout_session->error ) ) {
-			throw new Exception( $checkout_session->error->message );
-		}
-
-		if ( empty( $checkout_session->client_secret ) ) {
-			throw new Exception( __( 'Unable to create Stripe Checkout Session.', 'woocommerce-gateway-stripe' ) );
-		}
-
-		wp_send_json_success( [ 'client_secret' => $checkout_session->client_secret ] );
 	}
 }
