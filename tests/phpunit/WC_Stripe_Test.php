682b87fef97663e017de6a8b8a25e61083b45be4
Dale du Preez <dale@automattic.com>
2026-02-04 11:17:47 +0200

    Skip unit test

--- tests/phpunit/WC_Stripe_Test.php
+++ tests/phpunit/WC_Stripe_Test.php
@@ -235,6 +235,9 @@ class WC_Stripe_Test extends WC_Mock_Stripe_API_Unit_Test_Case {
 	 * @return void
 	 */
 	public function test_install_sets_fresh_install_flag(): void {
+		// @see https://github.com/woocommerce/woocommerce-gateway-stripe/issues/4979
+		$this->markTestSkipped( 'We are pausing OCS default on, so the flag is not being set on new installs' );
+
 		update_option( 'active_plugins', [ plugin_basename( WC_STRIPE_MAIN_FILE ) ] );
 
 		// Ensure the flag is not set.
