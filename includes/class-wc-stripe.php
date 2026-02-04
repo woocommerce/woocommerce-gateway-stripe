c840573bad371f91cad10744b9f0d588029ab1bf
Mayisha <mayisha.alam@automattic.com>
2026-02-04 20:50:17 +0600

    store boolean in 'is_oc_enabled'

--- includes/class-wc-stripe.php
+++ includes/class-wc-stripe.php
@@ -500,7 +500,7 @@ class WC_Stripe {
 	public function add_gateways( $methods ) {
 		$main_gateway  = $this->get_main_stripe_gateway();
 		$methods[]     = $main_gateway;
-		$is_oc_enabled = $main_gateway->get_option( 'optimized_checkout_element', 'no' );
+		$is_oc_enabled = 'yes' === $main_gateway->get_option( 'optimized_checkout_element', 'no' );
 
 		// The $main_gateway represents the card gateway so we don't want to include it in the list of UPE gateways.
 		$upe_payment_methods = $main_gateway->payment_methods;
@@ -527,7 +527,7 @@ class WC_Stripe {
 					}
 
 					if ( $method instanceof WC_Stripe_UPE_Payment_Method ) {
-						if ( 'yes' === $is_oc_enabled ) {
+						if ( $is_oc_enabled ) {
 							return false;
 						}
 
