5ab67735d5d1acc098eb60c5a34893790a48917a
Mayisha <mayisha.alam@automattic.com>
2026-02-11 14:34:24 +0600

    use clover version

--- includes/class-wc-stripe-api.php
+++ includes/class-wc-stripe-api.php
@@ -14,7 +14,7 @@ class WC_Stripe_API {
 	 * Stripe API Endpoint
 	 */
 	const ENDPOINT           = 'https://api.stripe.com/v1/';
-	const STRIPE_API_VERSION = '2025-03-31.basil';
+	const STRIPE_API_VERSION = '2025-09-30.clover';
 
 	/**
 	 * The invalid API key error count cache key.
