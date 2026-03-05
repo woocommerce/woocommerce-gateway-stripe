8149e0ac779d06e171188294c001713959be3055
Mayisha <mayisha.alam@automattic.com>
2026-03-05 13:06:56 +0600

    remove translation from title and label

--- includes/payment-methods/class-wc-stripe-upe-payment-method-ideal.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-method-ideal.php
@@ -21,10 +21,10 @@ class WC_Stripe_UPE_Payment_Method_Ideal extends WC_Stripe_UPE_Payment_Method {
 		parent::__construct();
 		$is_sepa_tokens_for_ideal_enabled         = $this->is_sepa_tokens_for_ideal_enabled();
 		$this->stripe_id                          = self::STRIPE_ID;
-		$this->title                              = __( 'iDEAL | Wero', 'woocommerce-gateway-stripe' );
+		$this->title                              = 'iDEAL | Wero';
 		$this->is_reusable                        = $is_sepa_tokens_for_ideal_enabled;
 		$this->supported_currencies               = [ WC_Stripe_Currency_Code::EURO ];
-		$this->label                              = __( 'iDEAL | Wero', 'woocommerce-gateway-stripe' );
+		$this->label                              = 'iDEAL | Wero';
 		$this->description                        = __(
 			'iDEAL | Wero is a Netherlands-based payment method that allows customers to complete transactions online using their bank credentials.',
 			'woocommerce-gateway-stripe'
