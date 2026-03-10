c8ea7fc47508d9bf006b990871857b57ecea928d
Mayisha <mayisha.alam@automattic.com>
2026-03-10 13:43:44 +0600

    clear cache only if setting default is successful

--- includes/class-wc-stripe-customer.php
+++ includes/class-wc-stripe-customer.php
@@ -962,10 +962,10 @@ class WC_Stripe_Customer {
 			'POST'
 		);
 
-		// Clear cache so that the payment methods list from Stripe is refreshed to have the correct default payment method.
-		$this->clear_cache();
-
 		if ( empty( $response->error ) ) {
+			// Clear cache so that the payment methods list from Stripe is refreshed to have the correct default payment method.
+			$this->clear_cache();
+
 			do_action( 'wc_stripe_set_default_source', $this->get_id(), $response );
 
 			return true;
@@ -992,10 +992,10 @@ class WC_Stripe_Customer {
 			'POST'
 		);
 
-		// Clear cache so that the payment methods list from Stripe is refreshed to have the correct default payment method.
-		$this->clear_cache();
-
 		if ( empty( $response->error ) ) {
+			// Clear cache so that the payment methods list from Stripe is refreshed to have the correct default payment method.
+			$this->clear_cache();
+
 			do_action( 'wc_stripe_set_default_payment_method', $this->get_id(), $response );
 
 			return true;
