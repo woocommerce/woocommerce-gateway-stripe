fa53f6e79c266563c265f4aede4ceab40603d549
Dale du Preez <dale@automattic.com>
2026-03-11 10:56:06 +0200

    Refactor CSS detection to check only valid fields via camelCase field names

--- client/styles/upe/index.js
+++ client/styles/upe/index.js
@@ -141,12 +141,6 @@ const appearanceSelectors = {
 	},
 };
 
-const dashedToCamelCase = ( string ) => {
-	return string.replace( /-([a-z])/g, function ( g ) {
-		return g[ 1 ].toUpperCase();
-	} );
-};
-
 const hiddenElementsForUPE = {
 	/**
 	 * Create hidden container for generating UPE styles.
@@ -306,12 +300,9 @@ export const getFieldStyles = ( selector, upeElement ) => {
 
 	const filteredStyles = {};
 
-	for ( let i = 0; i < styles.length; i++ ) {
-		const camelCase = dashedToCamelCase( styles[ i ] );
-		if ( validProperties.includes( camelCase ) ) {
-			filteredStyles[ camelCase ] = styles.getPropertyValue(
-				styles[ i ]
-			);
+	for ( const property of validProperties ) {
+		if ( typeof styles[ property ] !== 'undefined' ) {
+			filteredStyles[ property ] = styles[ property ];
 		}
 	}
 
