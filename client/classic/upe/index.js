73bb67e76289cd2290962c55afbcbcd652c21cc1
Dale du Preez <dale@automattic.com>
2026-03-18 22:27:57 +0200

    Extract main logic into dedicated file; disable when saved tokens are shown

--- client/classic/upe/index.js
+++ client/classic/upe/index.js
@@ -5,10 +5,10 @@ import {
 	getUPETerms,
 	maybeClearBlikCodeValidation,
 } from '../../stripe-utils';
+import { initializeAlwaysExpandedOptimizedCheckout } from './always-expanded-optimized-checkout';
 import { legacyHashchangeHandler } from './legacy-support';
 import './style.scss';
 import './deferred-intent.js';
-import { getStripePaymentElement } from './payment-processing';
 import {
 	maybeShowCashAppLimitNotice,
 	removeCashAppLimitNotice,
@@ -17,7 +17,6 @@ import {
 	PAYMENT_METHOD_BOLETO,
 	PAYMENT_METHOD_MULTIBANCO,
 	PAYMENT_METHOD_OXXO,
-	PAYMENT_METHOD_CARD,
 } from 'wcstripe/stripe-utils/constants';
 
 jQuery( function ( $ ) {
@@ -343,131 +342,7 @@ jQuery( function ( $ ) {
 		stripeServerData?.shouldShowOptimizedCheckout &&
 		stripeServerData?.shouldExpandOptimizedCheckout
 	) {
-		const getStripePaymentBox = () =>
-			$(
-				'.woocommerce-checkout-payment .payment_methods .payment_box.payment_method_stripe'
-			);
-		const keepPaymentBoxVisible = async () => {
-			const $stripePaymentMethod = getStripePaymentBox();
-			if ( ! $stripePaymentMethod.length ) {
-				return;
-			}
-			// Stop animation - clear the animation queue and stop jumpToEnd.
-			// Then immediately (re)show the element.
-			$stripePaymentMethod.stop( true, false ).show();
-
-			// Prevent any manual manipulation of the payment method box's size.
-			// The animations seem to try, but that generates incorrect fixed sizes.
-			const stripePaymentMethodBox = $stripePaymentMethod.get( 0 );
-			if ( stripePaymentMethodBox ) {
-				stripePaymentMethodBox.style.height = '';
-				stripePaymentMethodBox.style.marginTop = '';
-				stripePaymentMethodBox.style.marginBottom = '';
-				stripePaymentMethodBox.style.paddingTop = '';
-				stripePaymentMethodBox.style.paddingBottom = '';
-			}
-
-			// If we selected a different payment method, collapse the Stripe payment element.
-			const selectedPaymentMethod = document.querySelector(
-				'.woocommerce-checkout-payment input[name="payment_method"]:checked'
-			);
-			if ( selectedPaymentMethod?.id !== 'payment_method_stripe' ) {
-				try {
-					const paymentElement =
-						await getStripePaymentElement( PAYMENT_METHOD_CARD );
-					paymentElement?.collapse();
-				} catch {
-					// If we don't have a payment element, no need to collapse.
-				}
-			}
-		};
-
-		$( document.body ).on(
-			'payment_method_selected',
-			keepPaymentBoxVisible
-		);
-		$( document.body ).on( 'updated_checkout', keepPaymentBoxVisible );
-		$( document.body ).on( 'init_checkout', keepPaymentBoxVisible );
-
-		const contractOtherPaymentMethods = () => {
-			const paymentMethodsRoot = document.querySelector(
-				'.woocommerce-checkout-payment .payment_methods'
-			);
-			const allPaymentMethodBoxes = Array.from(
-				paymentMethodsRoot?.getElementsByClassName( 'payment_box' ) ??
-					[]
-			);
-			const otherPaymentMethodBoxes = allPaymentMethodBoxes.filter(
-				( paymentMethodBox ) =>
-					! paymentMethodBox.classList.contains(
-						'payment_method_stripe'
-					)
-			);
-			const $wrappedPaymentMethodBoxes = $( otherPaymentMethodBoxes );
-			$wrappedPaymentMethodBoxes.filter( ':visible' ).slideUp( 230 );
-		};
-
-		const selectStripePaymentMethod = () => {
-			const stripeRadioButton = document.getElementById(
-				'payment_method_stripe'
-			);
-			if ( stripeRadioButton && ! stripeRadioButton.checked ) {
-				stripeRadioButton.click();
-			}
-			// Now contract the other payment methods.
-			contractOtherPaymentMethods();
-		};
-
-		/**
-		 * Ensure that the Stripe payment method is marked as selected when the following events occur:
-		 * - User clicks on the Stripe payment box - this only includes content outside the Stripe iframe.
-		 * - User focuses on the Stripe payment element within the iframe.
-		 */
-		const reconfigureStripePaymentSelection = () => {
-			const stripePaymentBox = getStripePaymentBox();
-			// Use a namespaced event to ensure we remove existing event listeners before (re)adding the listener.
-			stripePaymentBox
-				.off( 'click.wc-stripe-expanded-ocs' )
-				.on(
-					'click.wc-stripe-expanded-ocs',
-					selectStripePaymentMethod
-				);
-
-			getStripePaymentElement( PAYMENT_METHOD_CARD )
-				.then( ( paymentElement ) => {
-					if ( paymentElement ) {
-						paymentElement.off(
-							'focus',
-							selectStripePaymentMethod
-						);
-						paymentElement.on( 'focus', selectStripePaymentMethod );
-					}
-				} )
-				.catch( () => {
-					// If we fail to get the payment element, no further action needed.
-				} );
-		};
-
-		$( document.body ).on(
-			'init_checkout',
-			reconfigureStripePaymentSelection
-		);
-		$( document.body ).on(
-			'updated_checkout',
-			reconfigureStripePaymentSelection
-		);
-
-		const addOcsRootClass = () => {
-			const stripePaymentMethodRoot = document.querySelector(
-				'.woocommerce-checkout-payment .payment_methods .wc_payment_method.payment_method_stripe'
-			);
-			stripePaymentMethodRoot?.classList.add(
-				'wc-stripe-optimized-checkout'
-			);
-		};
-
-		$( document.body ).on( 'init_checkout', addOcsRootClass );
-		$( document.body ).on( 'updated_checkout', addOcsRootClass );
+		initializeAlwaysExpandedOptimizedCheckout( $ );
 	}
 
 	// On every page load, check to see whether we should display the authentication
