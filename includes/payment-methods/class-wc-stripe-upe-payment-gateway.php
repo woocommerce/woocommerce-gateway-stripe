ec283c8ded7826dd2b7dafd89c5f61ec876783a5
Wesley Rosa <wesleyjrosa@gmail.com>
2026-03-23 11:21:23 -0300

    Removing unnecessary early return

--- includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-gateway.php
@@ -1071,11 +1071,6 @@ class WC_Stripe_UPE_Payment_Gateway extends WC_Stripe_Payment_Gateway {
 	 * @param WC_Order $order            Order data.
 	 */
 	public function add_converted_currency_information( string $formatted_total, WC_Order $order ): string {
-		$is_email_context = did_action( 'woocommerce_email_header' ) && ! did_action( 'woocommerce_email_footer' );
-		if ( ! is_order_received_page() && ! is_account_page() && ! $is_email_context ) {
-			return $formatted_total;
-		}
-
 		$presentment_data = $this->get_presentment_data_from_order( $order );
 		if ( null === $presentment_data ) {
 			return $formatted_total;
