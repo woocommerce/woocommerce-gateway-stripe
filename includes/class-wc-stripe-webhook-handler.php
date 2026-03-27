f5be280845a10f99b6224ae9843f87ca56247d58
Mayisha <33387139+Mayisha@users.noreply.github.com>
2026-03-27 20:45:35 +0600

    fix spacing
    Co-authored-by: Wesley Rosa <wesleyjrosa@gmail.com>

--- includes/class-wc-stripe-webhook-handler.php
+++ includes/class-wc-stripe-webhook-handler.php
@@ -1620,7 +1620,7 @@ class WC_Stripe_Webhook_Handler extends WC_Stripe_Payment_Gateway {
 			return;
 		}
 
-		$order            = WC_Stripe_Helper::get_order_by_checkout_session_id( $checkout_session->id );
+		$order = WC_Stripe_Helper::get_order_by_checkout_session_id( $checkout_session->id );
 
 		if ( ! $order instanceof \WC_Order ) {
 			WC_Stripe_Logger::debug( 'Could not find order via checkout session ID: ' . $checkout_session->id );
