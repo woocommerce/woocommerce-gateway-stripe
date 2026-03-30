99eb3f7e0e11199035711c8ef59d1cc910754481
Mayisha <mayisha.alam@automattic.com>
2026-03-30 17:24:15 +0600

    map properties

--- client/styles/upe/index.js
+++ client/styles/upe/index.js
@@ -1,6 +1,10 @@
 /* global wc_stripe_upe_params */
 
-import { upeRestrictedProperties } from './upe-styles';
+import {
+	computedStylePropertyMaps,
+	fontSmoothingProps,
+	upeRestrictedProperties,
+} from './upe-styles';
 import {
 	generateHoverRules,
 	generateOutlineStyle,
@@ -315,6 +319,14 @@ export const getFieldStyles = ( selector, upeElement ) => {
 	const filteredStyles = {};
 
 	for ( const property of validProperties ) {
+		if ( fontSmoothingProps.includes( property ) ) {
+			const mappedProperty = computedStylePropertyMaps[ property ];
+			if ( typeof styles[ mappedProperty ] !== 'undefined' ) {
+				filteredStyles[ property ] = styles[ mappedProperty ];
+			}
+			continue;
+		}
+
 		if ( typeof styles[ property ] !== 'undefined' ) {
 			filteredStyles[ property ] = styles[ property ];
 		}
