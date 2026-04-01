49b8bf213080ad58bbc382b1030f4f001a40e2eb
Mayisha <mayisha.alam@automattic.com>
2026-04-01 14:08:30 +0600

    update payment method title for OC in block checkout

--- includes/payment-methods/class-wc-stripe-upe-payment-method-oc.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-method-oc.php
@@ -27,7 +27,7 @@ class WC_Stripe_UPE_Payment_Method_OC extends WC_Stripe_UPE_Payment_Method {
 		$this->enabled     = $is_stripe_enabled && $this->oc_enabled ? 'yes' : 'no';
 		$this->id          = WC_Stripe_UPE_Payment_Gateway::ID; // Force the ID to be the same as the main payment gateway.
 		$this->stripe_id   = self::STRIPE_ID;
-		$this->title       = 'Stripe';
+		$this->title       = 'TBD';
 		$this->is_reusable = true;
 		$this->supports[]  = PaymentGatewayFeature::TOKENIZATION;
 
@@ -58,7 +58,7 @@ class WC_Stripe_UPE_Payment_Method_OC extends WC_Stripe_UPE_Payment_Method {
 
 		// Block checkout and pay for order (checkout) page.
 		if ( ( has_block( 'woocommerce/checkout' ) || ! empty( $_GET['pay_for_order'] ) ) && ! is_wc_endpoint_url( 'order-received' ) ) { // phpcs:ignore WordPress.Security.NonceVerification
-			return 'Stripe';
+			return 'TBD';
 		}
 
 		return parent::get_title();
