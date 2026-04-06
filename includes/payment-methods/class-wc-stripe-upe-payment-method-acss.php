84dd4fe8d26a4f782b4441a5841a08002ccff0ea
Diego Curbelo <diego@curbelo.com>
2026-04-06 15:49:44 -0300

    Fix cloned Stripe styles

--- includes/payment-methods/class-wc-stripe-upe-payment-method-acss.php
+++ includes/payment-methods/class-wc-stripe-upe-payment-method-acss.php
@@ -69,11 +69,11 @@ class WC_Stripe_UPE_Payment_Method_ACSS extends WC_Stripe_UPE_Payment_Method {
 				<div class="wc-stripe-upe-element" data-payment-method-type="<?php echo esc_attr( $this->stripe_id ); ?>"></div>
 				<div id="wc-<?php echo esc_attr( $this->id ); ?>-upe-errors" role="alert"></div>
 			</fieldset>
-			<div style="display: flex; align-items: center; gap: 12px; margin: 0; padding: 0; color: #6d6e78;">
-				<svg xmlns="http://www.w3.org/2000/svg" width="32" height="27" viewBox="0 0 48 40" fill="#9a9a9e" role="presentation" style="flex-shrink: 0;">
+			<div style="display: flex; align-items: center; gap: 12px; margin: 0; padding-left: 9.88875px;">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 40" fill="rgb(43, 45, 47)" role="presentation" style="flex-shrink: 0; width: 3em; height: 3em;">
 					<path opacity=".6" fill-rule="evenodd" clip-rule="evenodd" d="M0 8a4 4 0 014-4h30a4 4 0 014 4v8a1 1 0 11-2 0v-4a2 2 0 00-2-2H4a2 2 0 00-2 2v20a2 2 0 002 2h30a2 2 0 002-2v-6a1 1 0 112 0v6a4 4 0 01-4 4H4a4 4 0 01-4-4V8zm4 0a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm4-1a1 1 0 11-2 0 1 1 0 012 0zm29.992 9.409L44.583 20H29a1 1 0 100 2h15.583l-3.591 3.591a1 1 0 101.415 1.416l5.3-5.3a1 1 0 000-1.414l-5.3-5.3a1 1 0 10-1.415 1.416z"/>
 				</svg>
-				<span style="margin: 0; padding: 0; font-size: 14px; line-height: 1.4;">
+				<span style="margin: 0; padding: 0; font-size: 16px; line-height: 1.6;">
 					<?php echo esc_html__( 'After submission, you will need to authorize the payment with your bank.', 'woocommerce-gateway-stripe' ); ?>
 				</span>
 			</div>
