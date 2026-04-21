4f5743adc4865c5f84942a060f938c9d9f04d2d7
Mayisha <mayisha.alam@automattic.com>
2026-04-21 23:42:47 +0600

    fix phpstan

--- includes/admin/class-wc-stripe-settings-controller.php
+++ includes/admin/class-wc-stripe-settings-controller.php
@@ -234,7 +234,7 @@ class WC_Stripe_Settings_Controller {
 			'is_amazon_pay_available'               => WC_Stripe_Feature_Flags::is_amazon_pay_available(),
 			'is_oc_available'                       => WC_Stripe_Feature_Flags::is_oc_available(),
 			'is_oc_enabled'                         => $is_oc_enabled,
-			'is_cs_available'                       => 'yes' === $this->gateway->get_option( 'pmc_enabled', 'no' ) && $is_ap_available_for_account,
+			'is_cs_available'                       => 'yes' === $this->get_gateway()->get_option( 'pmc_enabled', 'no' ) && $is_ap_available_for_account,
 			'oc_layout'                             => $this->get_gateway()->get_validated_option( 'optimized_checkout_layout' ),
 			'oauth_nonce'                           => wp_create_nonce( 'wc_stripe_get_oauth_url' ),
 			'is_sepa_tokens_for_ideal_enabled'      => 'yes' === $this->gateway->get_option( 'sepa_tokens_for_ideal', 'no' ),
