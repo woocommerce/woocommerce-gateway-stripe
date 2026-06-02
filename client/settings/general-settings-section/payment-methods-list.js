37b5985cf2ab4e7efbbee6e1aeae1dc3dc8a0ad9
Dale du Preez <dale@automattic.com>
2026-06-02 11:23:02 +0200

    Use clsx instead of classnames

--- client/settings/general-settings-section/payment-methods-list.js
+++ client/settings/general-settings-section/payment-methods-list.js
@@ -1,7 +1,7 @@
 import { getSetting } from '@woocommerce/settings';
 import React, { useMemo } from 'react';
 import styled from '@emotion/styled';
-import classnames from 'classnames';
+import clsx from 'clsx';
 import { Icon as IconComponent, dragHandle } from '@wordpress/icons';
 import { Reorder } from 'framer-motion';
 import PaymentMethodsMap from '../../payment-methods-map';
@@ -227,7 +227,7 @@ const GeneralSettingsSection = ( { isChangingDisplayOrder } ) => {
 					<DraggableListElement
 						key={ method }
 						value={ method }
-						className={ classnames( {
+						className={ clsx( {
 							'has-overlay':
 								! isAllowingManualCapture &&
 								isManualCaptureEnabled,
