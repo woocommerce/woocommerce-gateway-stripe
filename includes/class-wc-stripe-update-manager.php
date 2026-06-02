e8cfeb621cde604cbea2209b8c677c444ab29693
Malith Senaweera <malith.senaweera@automattic.com>
2026-06-02 15:10:57 -0500

    Remove redundant require_once

--- includes/class-wc-stripe-update-manager.php
+++ includes/class-wc-stripe-update-manager.php
@@ -79,10 +79,8 @@ class WC_Stripe_Update_Manager {
 			[ new Migrate_Payment_Request_Data_To_Express_Checkout_Data(), 'maybe_migrate' ],
 			[ new Sepa_Tokens_For_Other_Methods_Settings_Update(), 'maybe_migrate' ],
 			[ new WC_Stripe_Express_Checkout_Add_Change_Payment_Method_Location_Update(), 'maybe_migrate' ],
+			[ new WC_Stripe_OCS_AP_Default_On_Update(), 'maybe_migrate' ],
 		];
-
-		require_once WC_STRIPE_PLUGIN_PATH . '/includes/migrations/class-wc-stripe-ocs-ap-default-on-update.php';
-		$functions[] = [ new WC_Stripe_OCS_AP_Default_On_Update(), 'maybe_migrate' ];
 
 		return $functions;
 	}
