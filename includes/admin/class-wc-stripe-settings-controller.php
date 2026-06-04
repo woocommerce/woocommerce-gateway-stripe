664c72d9be5a977607787829bdebc24ede1bd404
Mayisha <mayisha.alam@automattic.com>
2026-06-04 17:10:29 +0600

    allow enabling Adaptive Pricing checkbox immediately after turning on OCS

--- includes/admin/class-wc-stripe-settings-controller.php
+++ includes/admin/class-wc-stripe-settings-controller.php
@@ -215,14 +215,8 @@ class WC_Stripe_Settings_Controller {
 			// Show the Stripe Tax banner only if OC is enabled
 			&& $is_oc_enabled;
 
-		$is_checkout_sessions_available      = false;
-		$adaptive_pricing_unavailable_reason = 'disabled';
-		if ( WC_Stripe_Feature_Flags::is_checkout_sessions_available() ) {
-			$adaptive_pricing_unavailable_reason = WC_Stripe_Helper::get_adaptive_pricing_account_unavailable_reason();
-			if ( null === $adaptive_pricing_unavailable_reason ) {
-				$is_checkout_sessions_available = true;
-			}
-		}
+		$adaptive_pricing_unavailable_reason = WC_Stripe_Helper::get_adaptive_pricing_account_unavailable_reason();
+		$is_checkout_sessions_available      = null === $adaptive_pricing_unavailable_reason;
 
 		$params = [
 			'time'                                  => time(),
