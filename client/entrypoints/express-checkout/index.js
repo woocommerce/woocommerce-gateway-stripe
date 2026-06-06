d632707555d57c819e970fed0f660d462e4d75af
Chris Huber <chubes@extrachill.com>
2026-06-06 11:43:02 -0400

    Defer product-page Express Checkout initialization

--- client/entrypoints/express-checkout/index.js
+++ client/entrypoints/express-checkout/index.js
@@ -70,6 +70,8 @@ jQuery( function ( $ ) {
 	);
 
 	let wcStripeECEError = '';
+	let isProductPageInitialInitScheduled = false;
+	let isProductPageInitialInitStarted = false;
 	const defaultErrorMessage = __(
 		'There was an error getting the product information.',
 		'woocommerce-gateway-stripe'
@@ -980,9 +982,66 @@ jQuery( function ( $ ) {
 		},
 	};
 
+	const initProductPageExpressCheckout = () => {
+		if ( isProductPageInitialInitStarted ) {
+			return;
+		}
+
+		isProductPageInitialInitStarted = true;
+		wcStripeECE.init();
+	};
+
+	const scheduleProductPageExpressCheckout = () => {
+		if ( isProductPageInitialInitScheduled ) {
+			return;
+		}
+
+		isProductPageInitialInitScheduled = true;
+
+		const container = document.getElementById(
+			'wc-stripe-express-checkout-element'
+		);
+		if ( ! container ) {
+			return;
+		}
+
+		$( 'form.cart' ).one(
+			'change input click',
+			'input, select, textarea, button',
+			initProductPageExpressCheckout
+		);
+
+		const observerTarget =
+			container.closest( 'form.cart' ) ||
+			container.closest( '.summary' ) ||
+			container.parentElement;
+		if ( ! observerTarget ) {
+			initProductPageExpressCheckout();
+			return;
+		}
+
+		if ( 'IntersectionObserver' in window ) {
+			const observer = new IntersectionObserver(
+				( entries ) => {
+					if ( entries.some( ( entry ) => entry.isIntersecting ) ) {
+						observer.disconnect();
+						initProductPageExpressCheckout();
+					}
+				},
+				{ rootMargin: '300px 0px' }
+			);
+
+			observer.observe( observerTarget );
+			return;
+		}
+
+		initProductPageExpressCheckout();
+	};
+
 	// We don't need to initialize ECE on the checkout page now because it will be initialized by updated_checkout event.
-	if (
-		getExpressCheckoutData( 'is_product_page' ) ||
+	if ( getExpressCheckoutData( 'is_product_page' ) ) {
+		scheduleProductPageExpressCheckout();
+	} else if (
 		getExpressCheckoutData( 'is_pay_for_order' ) ||
 		getExpressCheckoutData( 'is_cart_page' ) ||
 		getExpressCheckoutData( 'is_change_payment_method' )
