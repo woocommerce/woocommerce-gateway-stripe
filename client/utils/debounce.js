f284ef95fcdcf0f7e1a9d427862480ff99491fd8
Dale du Preez <dale@automattic.com>
2026-06-12 10:58:34 +0200

    Include JSDoc for local debounce utility

--- client/utils/debounce.js
+++ client/utils/debounce.js
@@ -1,10 +1,17 @@
-const debounce = ( func, delay ) => {
+/**
+ * Debounces a function by delaying its execution until after a specified delay.
+ *
+ * @param {Function} callback The function to debounce.
+ * @param {number}   delay    The delay in milliseconds
+ * @return {Function} The debounced function.
+ */
+const debounce = ( callback, delay ) => {
 	let timeoutId;
 
 	return function ( ...args ) {
 		clearTimeout( timeoutId );
 		timeoutId = setTimeout( () => {
-			func.apply( this, args );
+			callback.apply( this, args );
 		}, delay );
 	};
 };
