deb8f029a771fdb8d6583712f61374a7be779941
Adrian Dobrescu <adrian.dobrescu@endava.com>
2026-06-15 16:00:40 +0300

    Apply suggested changes

--- includes/admin/class-wc-stripe-rest-payment-intents-controller.php
+++ includes/admin/class-wc-stripe-rest-payment-intents-controller.php
@@ -106,6 +106,12 @@ class WC_Stripe_REST_Payment_Intents_Controller extends WC_Stripe_REST_Base_Cont
 		$search_params = [];
 
 		foreach ( $rest_args as $search_param_name => $search_param_definition ) {
+			/**
+			 * Search parameter value.
+			 *
+			 * @var string $search_param_value
+			*/
+
 			$search_param_value = $request->get_param( $search_param_name );
 
 			if ( '' === $search_param_value || is_null( $search_param_value ) ) {
