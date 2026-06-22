235ddcd23b23f3ff131498c773ca538cb47dcc6d
Malith Senaweera <malith.senaweera@automattic.com>
2026-06-22 08:56:42 -0500

    Fix error when saving with no subscriptions plugin

--- includes/admin/stripe-settings.php
+++ includes/admin/stripe-settings.php
@@ -6,16 +6,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 $is_gte_wc6_6 = defined( WC_VERSION ) && version_compare( WC_VERSION, '6.6', '>=' );
 
 $wc_stripe_express_checkout_location_options = [
-	'product'  => __( 'Product', 'woocommerce-gateway-stripe' ),
-	'cart'     => __( 'Cart', 'woocommerce-gateway-stripe' ),
-	'checkout' => __( 'Checkout', 'woocommerce-gateway-stripe' ),
+	'product'               => __( 'Product', 'woocommerce-gateway-stripe' ),
+	'cart'                  => __( 'Cart', 'woocommerce-gateway-stripe' ),
+	'checkout'              => __( 'Checkout', 'woocommerce-gateway-stripe' ),
+	'change_payment_method' => __( 'Change payment method (subscriptions)', 'woocommerce-gateway-stripe' ),
 ];
 
 $wc_stripe_default_express_checkout_locations = [ 'product', 'cart', 'checkout' ];
 
 if ( WC_Stripe_Subscriptions_Helper::is_subscriptions_enabled() ) {
-	$wc_stripe_express_checkout_location_options['change_payment_method'] = __( 'Change payment method (subscriptions)', 'woocommerce-gateway-stripe' );
-	$wc_stripe_default_express_checkout_locations[]                       = 'change_payment_method';
+	$wc_stripe_default_express_checkout_locations[] = 'change_payment_method';
 }
 
 return apply_filters(
