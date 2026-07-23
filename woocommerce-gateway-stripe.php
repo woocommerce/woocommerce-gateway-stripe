f9c6b5731f6677b3f0bb6c61265fa64f889b59af
Diego Curbelo <diego@curbelo.com>
2026-07-23 12:31:37 -0300

    Update the tested up to and requries at least versions

--- woocommerce-gateway-stripe.php
+++ woocommerce-gateway-stripe.php
@@ -9,8 +9,8 @@
  * Requires Plugins: woocommerce
  * Requires at least: 6.8
  * Tested up to: 7.0
- * WC requires at least: 10.6
- * WC tested up to: 10.8
+ * WC requires at least: 10.7
+ * WC tested up to: 10.9
  * Text Domain: woocommerce-gateway-stripe
  * Domain Path: /languages
  */
