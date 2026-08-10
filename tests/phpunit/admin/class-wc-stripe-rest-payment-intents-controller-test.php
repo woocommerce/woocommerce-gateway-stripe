cb839b9e1477ce77a0673af23bfaf6c4e2696b1b
Adrian Dobrescu <adrian.dobrescu@endava.com>
2026-08-10 13:27:15 +0300

    Add test cases for 'created' timestamp of 0

--- tests/phpunit/admin/class-wc-stripe-rest-payment-intents-controller-test.php
+++ tests/phpunit/admin/class-wc-stripe-rest-payment-intents-controller-test.php
@@ -351,6 +351,20 @@ class WC_Stripe_REST_Payment_Intents_Controller_Test extends WP_UnitTestCase {
 							'gte' => '1779802569',
 						],
 				],
+				[
+					'created' =>
+						[
+							'lte' => '1779802821',
+							'gte' => '0',
+						],
+				],
+				[
+					'created' =>
+						[
+							'lte' => '0',
+							'gte' => '0',
+						],
+				],
 			],
 		];
 	}
@@ -417,6 +431,7 @@ class WC_Stripe_REST_Payment_Intents_Controller_Test extends WP_UnitTestCase {
 		if ( array_key_exists( 'limit', $rest_params ) ) {
 			$this->assertEmpty( array_diff_key( $pre_http_request_params['search_params'], $rest_params ) );
 		} else {
+			// We default the `limit` argument when not supplied by the caller.
 			$this->assertEquals(
 				[ 'limit' => 25 ],
 				array_diff_key( $pre_http_request_params['search_params'], $rest_params )
