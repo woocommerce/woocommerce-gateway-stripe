0c833cf1647e621d9e3026d01b619ffa3b2e4c9d
Dale du Preez <dale@automattic.com>
2026-09-21 18:31:08 +0200

    Revert "Split out shell CSS as it is unused in checkout blocks"
    Reinstate the central style.scss file as splitting out the CSS increased all bundle sizes.
    
    This reverts commit 02a3fc0ee46d65e99e12c8c241f30804a1657899.

--- client/payment-method-icons/styles/icon-with-shell.js
+++ client/payment-method-icons/styles/icon-with-shell.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
 import BaseIcon from './base-icon';
-import './icon-with-shell.scss';
 
 /**
  * A payment method icon inside a bordered, padded white "shell".
