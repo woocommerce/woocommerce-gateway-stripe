26d3781d1887f9fbbca4e4d0113ac8a5bd464572
Adrian Dobrescu <adrian.dobrescu@endava.com>
2026-09-23 19:52:24 +0300

    Fix as requested

--- includes/admin/class-wc-stripe-rest-api-abstract-client.php
+++ includes/admin/class-wc-stripe-rest-api-abstract-client.php
@@ -12,15 +12,16 @@ defined( 'ABSPATH' ) || exit;
  */
 abstract class WC_Stripe_REST_API_Abstract_Client {
 	/**
-	 * Builds an array of parameters to forward to Stripe API.
+	 * Builds an array of parameters to forward to Stripe API
+	 * by selecting only the parameters that are present in incoming request from a given list of parameter names.
 	 *
 	 * @param WP_REST_Request<array<string, mixed>> $request An incoming REST request.
-	 * @param array $params_to_forward Names of params to forward.
-	 * @param array $expand_param Array of value to populate the 'expand' Stripe AAPI param.
+	 * @param array $params_to_forward                       Names of params to forward.
+	 * @param array $expand_param                            Array of value to populate the 'expand' Stripe API param.
 	 *
 	 * @return array
 	 */
-	public static function build_params_to_forward( $request, $params_to_forward, $expand_param ) {
+	public static function build_params_to_forward( WP_REST_Request $request, array $params_to_forward, array $expand_param ) {
 		$stripe_params = array_intersect_key(
 			$request->get_params(),
 			array_flip( $params_to_forward )
@@ -35,11 +36,11 @@ abstract class WC_Stripe_REST_API_Abstract_Client {
 	 * Fetch data from an Stripe API endpoint and returns its raw data or a WP_Error if an error occurs.
 	 *
 	 * @param string $endpoint The Stripe endpoint.
-	 * @param array $params Parameters to pass to the endpoint.
+	 * @param array $params    Parameters to pass to the endpoint.
 	 *
-	 * @return StdClass|WP_Error
+	 * @return stdClass|WP_Error
 	 */
-	public static function fetch_from_stripe( $endpoint, $params = [] ) {
+	public static function fetch_from_stripe( string $endpoint, array $params = [] ) {
 		$query_string = 0 === count( $params ) ? '' : http_build_query( $params, '', '&', PHP_QUERY_RFC3986 );
 
 		$stripe_resource_url = $endpoint . ( '' === $query_string ? '' : '?' . $query_string );
